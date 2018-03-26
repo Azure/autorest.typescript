19da61c1c6a4930f6da71e2b6cb317ce517c5064
Rikki Gibson <rigibson@microsoft.com>
2018-03-25 19:05:00 -0700

    Move test files to new path

---
+++ test/azure/head.ts
@@ -0,0 +1,86 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+'use strict';
+
+import * as should from 'should';
+import * as assert from 'assert';
+import * as msRest from 'ms-rest-js';
+import * as msRestAzure from 'ms-rest-azure-js';
+
+import { AutoRestHeadTestService } from './generated/Head/autoRestHeadTestService';
+import { AutoRestHeadExceptionTestService } from './generated/HeadExceptions/autoRestHeadExceptionTestService';
+
+var dummySubscriptionId = 'a878ae02-6106-429z-9397-58091ee45g98';
+var dummyToken = 'dummy12321343423';
+var credentials = new msRest.TokenCredentials(dummyToken);
+
+var clientOptions: any = {};
+var baseUri = 'http://localhost:3000';
+
+describe('typescript', function () {
+
+  describe('Swagger Head BAT', function () {
+
+    describe('Head Operations', function () {
+      var testOptions = clientOptions;
+      testOptions.requestOptions = { jar: true };
+      testOptions.filters = [new msRest.ExponentialRetryPolicyFilter(3, 0, 0, 0)];
+      testOptions.noRetryPolicy = true;
+      var testClient = new AutoRestHeadTestService(credentials, baseUri, clientOptions);
+
+      it('should return true for 200 status code', function (done) {
+        testClient.httpSuccess.head200(function (error, result) {
+          should.not.exist(error);
+          result.should.be.exactly(true);
+          done();
+        });
+      });
+
+      it('should return true for 204 status code', function (done) {
+        testClient.httpSuccess.head204(function (error, result) {
+          should.not.exist(error);
+          result.should.be.exactly(true);
+          done();
+        });
+      });
+
+      it('should return false for 404 status code', function (done) {
+        testClient.httpSuccess.head404(function (error, result) {
+          should.not.exist(error);
+          result.should.be.exactly(false);
+          done();
+        });
+      });
+    });
+
+    describe('Head Exception Operations', function () {
+      var testOptions: any = clientOptions;
+      testOptions.requestOptions = { jar: true };
+      testOptions.filters = [new msRest.ExponentialRetryPolicyFilter(3, 0, 0, 0)];
+      testOptions.noRetryPolicy = true;
+      var testClient = new AutoRestHeadExceptionTestService(credentials, baseUri, clientOptions);
+
+      it('should return true for 200 status code', function (done) {
+        testClient.headException.head200(function (error, result) {
+          should.not.exist(error);
+          done();
+        });
+      });
+
+      it('should return true for 204 status code', function (done) {
+        testClient.headException.head204(function (error, result) {
+          should.not.exist(error);
+          done();
+        });
+      });
+
+      it('should return false for 404 status code', function (done) {
+        testClient.headException.head404(function (error, result) {
+          should.exist(error);
+          done();
+        });
+      });
+    });
+  });
+});
---
+++ test/startServer.ts
@@ -0,0 +1,42 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+import * as fs from 'fs';
+import * as child_process from 'child_process';
+import * as path from 'path';
+
+var child: child_process.ChildProcess;
+
+before(function (done) {
+  var started = false;
+  var out = fs.openSync(path.join(__dirname, 'server.log'), 'w');
+  fs.writeSync(out, 'Test run started at ' + new Date().toISOString() + '\n');
+  child = child_process.spawn("npm", ['run', 'start-test-server']);
+
+  child.stdout.on('data', function (data: Buffer) {
+    fs.writeSync(out, data.toString('UTF-8'));
+    if (data.toString().indexOf('started') > 0) {
+      started = true;
+      done();
+    }
+  });
+
+  child.stderr.on('data', (data) => {
+    const dataString = data.toString();
+    fs.writeSync(out, dataString);
+    if (!started) {
+      done(new Error(dataString));
+    }
+  });
+
+  child.on("close", (code, signal) => {
+    fs.closeSync(out);
+    if (!started) {
+      done();
+    }
+  });
+});
+
+after(async function () {
+  child.kill();
+});
