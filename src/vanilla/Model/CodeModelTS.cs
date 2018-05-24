b3169204d8f3866861d12862ac0d6d40010d9deb
Dan Schulte <daschult@microsoft.com>
2018-05-24 14:36:26 -0700

    Update version of ms-rest-js dependency

--- src/vanilla/Model/CodeModelTS.cs
+++ src/vanilla/Model/CodeModelTS.cs
@@ -282,7 +282,7 @@ namespace AutoRest.TypeScript.Model
 
         public virtual string PackageDependencies()
         {
-            return "\"ms-rest-js\": \"C:/Users/daschult/Sources/ms-rest-js\"";
+            return "\"ms-rest-js\": \"^0.7.206\"";
         }
 
         public virtual Method GetSampleMethod()
