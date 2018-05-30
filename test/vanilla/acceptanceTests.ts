a841352b26eb0a2d034f928327814b2987f34aab
Rikki Gibson <rigibson@microsoft.com>
2018-05-30 11:39:32 -0700

    Only run new browser test in browser

--- test/vanilla/acceptanceTests.ts
+++ test/vanilla/acceptanceTests.ts
@@ -1898,23 +1898,25 @@ describe('typescript', function () {
         });
       }
 
-      it('browser should report upload/download progress', async function() {
-        const content = new Uint8Array(1024*1024*1);
-        let uploadNotified = false;
-        let downloadNotified = false;
-        const response = await testClient.formdata.uploadFileViaBodyWithHttpOperationResponse(content, {
-          onUploadProgress: ev => {
-            uploadNotified = true;
-            ev.loadedBytes.should.be.a.Number;
-           },
-          onDownloadProgress: ev => {
-            downloadNotified = true;
-            ev.loadedBytes.should.be.a.Number;
-          }
+      if (!msRest.isNode) {
+        it('browser should report upload/download progress', async function() {
+          const content = new Uint8Array(1024*1024*1);
+          let uploadNotified = false;
+          let downloadNotified = false;
+          const response = await testClient.formdata.uploadFileViaBodyWithHttpOperationResponse(content, {
+            onUploadProgress: ev => {
+              uploadNotified = true;
+              ev.loadedBytes.should.be.a.Number;
+            },
+            onDownloadProgress: ev => {
+              downloadNotified = true;
+              ev.loadedBytes.should.be.a.Number;
+            }
+          });
+          assert(uploadNotified);
+          assert(downloadNotified);
         });
-        assert(uploadNotified);
-        assert(downloadNotified);
-      });
+      }
     });
 
     describe('Url Client', function () {
