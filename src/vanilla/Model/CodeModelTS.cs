8104308f0bdeba524201e2b64464a017b582e376
Dan Schulte <daschult@microsoft.com>
2018-05-30 11:59:17 -0700

    Change local references to be preview references

--- src/vanilla/Model/CodeModelTS.cs
+++ src/vanilla/Model/CodeModelTS.cs
@@ -326,7 +326,7 @@ namespace AutoRest.TypeScript.Model
 
         public virtual string PackageDependencies()
         {
-            return "\"ms-rest-js\": \"C:/Users/daschult/Sources/ms-rest-js\"";
+            return "\"ms-rest-js\": \"^0.7.219\"";
         }
 
         public virtual Method GetSampleMethod()
