0636e7c7620437147d1a56779df3739492590beb
Dan Schulte <daschult@microsoft.com>
2018-06-14 15:28:52 -0700

    Update dependency versions

--- src/azure/Model/CodeModelTSa.cs
+++ src/azure/Model/CodeModelTSa.cs
@@ -76,7 +76,7 @@ namespace AutoRest.TypeScript.Azure.Model
 
         public override string PackageDependencies()
         {
-            return "\"ms-rest-azure-js\": \"C:/Users/daschult/Sources/ms-rest-azure-js\"";
+            return "\"ms-rest-azure-js\": \"~0.6.75\"";
         }
     }
 }
