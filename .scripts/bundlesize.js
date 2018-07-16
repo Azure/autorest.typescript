1028064a557be1ba9d32341a9c229d2ebf17f118
Dan Schulte <daschult@microsoft.com>
2018-07-16 15:39:27 -0700

    Update bundleSize script

--- .scripts/bundlesize.js
+++ .scripts/bundlesize.js
@@ -1,11 +1,8 @@
-
 const { join } = require("path");
-
 const { promisify } = require("util");
 const cp = require("child_process");
 const exec = promisify(cp.exec);
 const fs = require("fs");
-const stat = promisify(fs.stat);
 const filesize = require("filesize");
 const dependencies = require("./dependencies");
 
@@ -14,7 +11,7 @@ async function getBundleSize() {
     const child = cp.spawn(join(__dirname, "../node_modules/.bin/webpack"), ['-p'], { maxBuffer: 1024 * 1024, stdio: 'inherit' });
     child.on('exit', () => resolve());
   });
-  const status = await stat(join(__dirname, "../testBundle.js"));
+  const status = fs.statSync(join(__dirname, "../testBundle.js"));
   return status.size;
 }
 
@@ -23,26 +20,52 @@ function execVerbose(script) {
   return exec(script, { maxBuffer: 1024 * 1024 });
 }
 
-async function main() {
-  await execVerbose("git reset --hard " + process.env.TRAVIS_BRANCH);
-  dependencies.refreshNodeModules();
-  const baseSize = await getBundleSize();
-
-  await execVerbose("git reset --hard " + process.env.TRAVIS_PULL_REQUEST_SHA);
-  dependencies.refreshNodeModules();
-  const headSize = await getBundleSize();
-
-  const change = (headSize / baseSize) - 1;
-  const percentChange = (Math.abs(change) * 100).toFixed(2) + '%';
-
+/**
+ * Output a message to the console.
+ */
+function outputMessage(message) {
   const json = JSON.stringify({
-    title: `Size ${change >= 0 ? "increased" : "decreased"} by ${percentChange} (${filesize(headSize - baseSize)})`,
+    title: message,
     summary: ""
   });
-
   console.log(`---output
 ${json}
----`);
+---`)
+}
+
+function outputErrorMessage(message, error) {
+  outputMessage(`${message}: ${JSON.stringify(error)}`);
+}
+
+async function main() {
+  let baseSize = undefined;
+  let headSize = undefined;
+
+  const branch = process.env.TRAVIS_BRANCH;
+  const prCommit = process.env.TRAVIS_PULL_REQUEST_SHA
+
+  try {
+    await execVerbose("git reset --hard " + branch);
+    dependencies.refreshNodeModules();
+    baseSize = await getBundleSize();
+
+    await execVerbose("git reset --hard " + prCommit);
+    dependencies.refreshNodeModules();
+    headSize = await getBundleSize();
+
+    const change = (headSize / baseSize) - 1;
+    const percentChange = (Math.abs(change) * 100).toFixed(2) + '%';
+
+    outputMessage(`Size ${change >= 0 ? "increased" : "decreased"} by ${percentChange} (${filesize(headSize - baseSize)})`);
+  } catch (error) {
+    if (baseSize == undefined) {
+      outputErrorMessage(`Failed to get the bundle size of the base branch (${branch})`, error);
+    } else if (headSize == undefined) {
+      outputErrorMessage(`Failed to get the bundle size of the PR commit (${prCommit})`, error);
+    } else {
+      outputErrorMessage(`Unrecognized error`, error);
+    }
+  }
 }
 
 main()
