bedd1fc0f0edda1dfe6d022805dea60ad6e5a180
Dan Schulte <daschult@microsoft.com>
2018-07-30 15:54:10 -0700

    Rewrite test

--- test/azure/azureCompositeClient.ts
+++ test/azure/azureCompositeClient.ts
@@ -3,33 +3,27 @@
 
 'use strict';
 
-import * as should from 'should';
-import * as assert from 'assert';
+import { AzureServiceClientOptions } from "ms-rest-azure-js";
 import * as msRest from 'ms-rest-js';
-import * as msRestAzure from 'ms-rest-azure-js';
-
 import { AzureCompositeModel, AzureCompositeModelModels } from './generated/AzureCompositeModelClient/azureCompositeModel';
-var dummyToken = 'dummy12321343423';
-var credentials = new msRest.TokenCredentials(dummyToken);
 
-var clientOptions: any = {};
-var baseUri = 'http://localhost:3000';
+const dummyToken = 'dummy12321343423';
+const credentials = new msRest.TokenCredentials(dummyToken);
+
+const clientOptions: AzureServiceClientOptions = {};
+const baseUri = 'http://localhost:3000';
 
 describe('typescript', function () {
-
   describe('Azure Composite Client', function () {
-    var testClient = new AzureCompositeModel(credentials, baseUri, clientOptions);
-    it('should get and put valid basic type properties', function (done) {
-      testClient.basic.getValid(function (error, result) {
-        should.not.exist(error);
-        result.id.should.equal(2);
-        result.name.should.equal('abc');
-        result.color.should.equal('YELLOW');
-        testClient.basic.putValid({ 'id': 2, 'name': 'abc', color: AzureCompositeModelModels.CMYKColors.Magenta }, function (error, result) {
-          should.not.exist(error);
-          done();
-        });
-      });
+    const testClient = new AzureCompositeModel(credentials, baseUri, clientOptions);
+
+    it('should get and put valid basic type properties', async () => {
+      const result = await testClient.basic.getValid();
+      result.id.should.equal(2);
+      result.name.should.equal('abc');
+      result.color.should.equal('YELLOW');
+
+      await testClient.basic.putValid({ 'id': 2, 'name': 'abc', color: AzureCompositeModelModels.CMYKColors.Magenta });
     });
   });
 });
