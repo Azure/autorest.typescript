8aaea661d603e7979d682b3496b4f6179871e09a
Rikki Gibson <rigibson@microsoft.com>
2018-08-28 18:10:19 -0700

    Add date and date-time-rfc1123 tests

--- test/date-time-as-string/dateAsStringTests.ts
+++ test/date-time-as-string/dateAsStringTests.ts
@@ -1,8 +1,10 @@
 import * as assert from "assert";
 import { AutoRestDateTimeTestService } from "./generated/BodyDateTime/autoRestDateTimeTestService";
+import { AutoRestDateTestService } from "./generated/BodyDate/autoRestDateTestService";
+import { AutoRestRFC1123DateTimeTestService } from './generated/BodyDateTimeRfc1123/autoRestRFC1123DateTimeTestService';
 
 const testClient = new AutoRestDateTimeTestService();
-describe("--model-date-time-as-string=false", function () {
+describe("--model-date-time-as-string=true", function () {
   it("should get a string date-time", async function () {
     const date = await testClient.datetime.getUtcMinDateTime();
     assert.strictEqual(typeof date, "string");
@@ -13,5 +15,17 @@ describe("--model-date-time-as-string=false", function () {
 
   it("should put a string date-time", async function () {
     await testClient.datetime.putUtcMinDateTime("9999-12-31");
+  });
+
+  it('should get a Date for "format": "date"', async function () {
+    const dateClient = new AutoRestDateTestService();
+    const date = await dateClient.dateModel.getMinDate();
+    assert(date instanceof Date);
+  });
+
+  it('should get a Date for "format": "date-time-rfc1123"', async function () {
+    const dateClient = new AutoRestRFC1123DateTimeTestService();
+    const date = await dateClient.datetimerfc1123.getUtcMinDateTime();
+    assert(date instanceof Date);
   });
 });
