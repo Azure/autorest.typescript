d59f0e97b05f3179fdc04bf8dd1c71316beee4b7
Dan Schulte <daschult@microsoft.com>
2018-09-06 15:30:37 -0700

    Add SourceCodeFolderPath setting

--- src/vanilla/CodeGeneratorTS.cs
+++ src/vanilla/CodeGeneratorTS.cs
@@ -39,16 +39,16 @@ namespace AutoRest.TypeScript
 
             // Service client
             var serviceClientTemplate = new ServiceClientTemplate {Model = codeModel};
-            await Write(serviceClientTemplate, codeModel.Name.ToCamelCase() + ".ts");
-            await Write(new ServiceClientContextTemplate {Model = codeModel }, codeModel.ContextName.ToCamelCase() + ".ts");
+            await Write(serviceClientTemplate, GetSourceCodeFilePath(codeModel, codeModel.Name.ToCamelCase() + ".ts"));
+            await Write(new ServiceClientContextTemplate {Model = codeModel }, GetSourceCodeFilePath(codeModel, codeModel.ContextName.ToCamelCase() + ".ts"));
 
             //Models
             if (codeModel.ModelTypes.Any())
             {
                 var modelIndexTemplate = new ModelIndexTemplate {Model = codeModel};
-                await Write(modelIndexTemplate, Path.Combine("models", "index.ts"));
+                await Write(modelIndexTemplate, GetSourceCodeFilePath(codeModel, "models", "index.ts"));
                 var mapperIndexTemplate = new MapperIndexTemplate {Model = codeModel};
-                await Write(mapperIndexTemplate, Path.Combine("models", "mappers.ts"));
+                await Write(mapperIndexTemplate, GetSourceCodeFilePath(codeModel, "models", "mappers.ts"));
             }
 
             bool serviceHasMappableParameters = codeModel.Methods
@@ -56,21 +56,21 @@ namespace AutoRest.TypeScript
                 .Any(p => p.Location != ParameterLocation.Body);
             if (serviceHasMappableParameters)
             {
-                await Write(new ParameterTemplate {Model = codeModel}, Path.Combine("models", "parameters.ts"));
+                await Write(new ParameterTemplate {Model = codeModel}, GetSourceCodeFilePath(codeModel, "models", "parameters.ts"));
             }
 
             //MethodGroups
             if (codeModel.MethodGroupModels.Any())
             {
                 var methodGroupIndexTemplate = new MethodGroupIndexTemplate {Model = codeModel};
-                await Write(methodGroupIndexTemplate, Path.Combine("operations", "index.ts"));
+                await Write(methodGroupIndexTemplate, GetSourceCodeFilePath(codeModel, "operations", "index.ts"));
 
                 foreach (var methodGroupModel in codeModel.MethodGroupModels)
                 {
                     var mappersTemplate = new MethodGroupMappersTemplate { Model = methodGroupModel };
-                    await Write(mappersTemplate, Path.Combine("models", methodGroupModel.MappersModuleName + ".ts"));
+                    await Write(mappersTemplate, GetSourceCodeFilePath(codeModel, "models", methodGroupModel.MappersModuleName + ".ts"));
                     var methodGroupTemplate = new MethodGroupTemplate {Model = methodGroupModel};
-                    await Write(methodGroupTemplate, Path.Combine("operations", methodGroupModel.TypeName.ToCamelCase() + ".ts"));
+                    await Write(methodGroupTemplate, GetSourceCodeFilePath(codeModel, "operations", methodGroupModel.TypeName.ToCamelCase() + ".ts"));
                 }
             }
 
@@ -78,40 +78,56 @@ namespace AutoRest.TypeScript
             {
                 // package.json
                 var packageJson = new PackageJson { Model = codeModel };
-                await Write(packageJson, Path.Combine("../", "package.json"));
+                await Write(packageJson, "package.json");
             }
 
             if (codeModel.Settings.GenerateReadmeMd || codeModel.Settings.GenerateMetadata)
             {
                 //README.md
                 var readme = new ReadmeTemplate { Model = codeModel };
-                await Write(readme, Path.Combine("../", "README.md"));
+                await Write(readme, "README.md");
             }
 
             if (codeModel.Settings.GenerateLicenseTxt)
             {
                 var license = new LicenseTemplate { Model = codeModel };
-                await Write(license, Path.Combine("../", "LICENSE.txt"));
+                await Write(license, "LICENSE.txt");
             }
 
             if (codeModel.Settings.GenerateMetadata)
             {
                 //tsconfig.json
                 var nodeTsConfig = new TsConfig { Model = codeModel };
-                await Write(nodeTsConfig, Path.Combine("../", "tsconfig.json"));
+                await Write(nodeTsConfig, "tsconfig.json");
 
                 //tsconfig.esm.json
                 var webpackTsConfig = new TsConfigWebpack();
-                await Write(webpackTsConfig, Path.Combine("../", "tsconfig.esm.json"));
+                await Write(webpackTsConfig, "tsconfig.esm.json");
 
                 // webpack.config.js
                 var webpackConfig = new WebpackConfig { Model = codeModel };
-                await Write(webpackConfig, Path.Combine("../", "webpack.config.js"));
+                await Write(webpackConfig, "webpack.config.js");
 
                 // .npmignore
                 var npmIgnore = new NpmIgnore { Model = codeModel };
-                await Write(npmIgnore, Path.Combine("../", ".npmignore"));
+                await Write(npmIgnore, ".npmignore");
             }
+        }
+
+        protected string GetSourceCodeFilePath(CodeModelTS codeModel, params string[] pathSegments)
+        {
+            return GetSourceCodeFilePath(codeModel.Settings, pathSegments);
+        }
+
+        protected string GetSourceCodeFilePath(GeneratorSettingsTS generatorSettings, params string[] pathSegments)
+        {
+            string[] totalPathSegments = new string[pathSegments.Length + 1];
+            totalPathSegments[0] = generatorSettings.SourceCodeFolderPath;
+            for (int i = 0; i < pathSegments.Length; i++)
+            {
+                totalPathSegments[1 + i] = pathSegments[i];
+            }
+            return Path.Combine(totalPathSegments).Replace('\\', '/');
         }
     }
 }
