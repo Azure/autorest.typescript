586f2acd166067e2fa7aa47bfced988217f43cd9
Rikki Gibson <rigibson@microsoft.com>
2018-09-11 19:24:11 -0700

    Fix some file copyright comments

--- test/vanilla/extensibleEnumsTests.ts
+++ test/vanilla/extensibleEnumsTests.ts
@@ -1,10 +1,10 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
 import { PetStoreInc } from "./generated/ExtensibleEnum/petStoreInc";
 import { DaysOfWeekExtensibleEnum, IntEnum } from "./generated/ExtensibleEnum/models";
 
 import * as assert from "assert";
-
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License. See License.txt in the project root for license information.
 
 const testClient = new PetStoreInc();
 
--- test/vanilla/multiCollectionFormatTests.ts
+++ test/vanilla/multiCollectionFormatTests.ts
@@ -1,7 +1,7 @@
-import { AutoRestUrlMutliCollectionFormatTestService } from "./generated/UrlMultiCollectionFormat/autoRestUrlMutliCollectionFormatTestService";
-
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
+
+import { AutoRestUrlMutliCollectionFormatTestService } from "./generated/UrlMultiCollectionFormat/autoRestUrlMutliCollectionFormatTestService";
 
 const testClient = new AutoRestUrlMutliCollectionFormatTestService();
 
