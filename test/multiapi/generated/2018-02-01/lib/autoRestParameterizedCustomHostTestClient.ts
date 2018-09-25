f6dbd02f44bafdc1e625b20d1b9650c842cf2c71
Dan Schulte <daschult@microsoft.com>
2018-09-25 14:12:22 -0700

    Remove optional parameter properties from Azure ServiceClient constructor comment

--- test/multiapi/generated/2018-02-01/lib/autoRestParameterizedCustomHostTestClient.ts
+++ test/multiapi/generated/2018-02-01/lib/autoRestParameterizedCustomHostTestClient.ts
@@ -20,27 +20,12 @@ class AutoRestParameterizedCustomHostTestClient extends AutoRestParameterizedCus
    * Initializes a new instance of the AutoRestParameterizedCustomHostTestClient class.
    * @constructor
    *
-   * @param {msRest.ServiceClientCredentials} credentials - Credentials needed for the client to connect to Azure.
+   * @param {msRest.ServiceClientCredentials} credentials Credentials needed for the client to
+   * connect to Azure.
    *
-   * @param {string} subscriptionId - The subscription id with value 'test12'.
+   * @param {string} subscriptionId The subscription id with value 'test12'.
    *
-   * @param {object} [options] - The parameter options
-   *
-   * @param {Array} [options.filters] - Filters to be added to the request pipeline
-   *
-   * @param {object} [options.requestOptions] - The request options. Detailed info can be found at
-   * {@link https://github.github.io/fetch/#Request Options doc}
-   *
-   * @param {boolean} [options.noRetryPolicy] - If set to true, turn off default retry policy
-   *
-   * @param {string} [options.dnsSuffix] - A string value that is used as a global part of the parameterized host. Default value 'host'.
-   *
-   * @param {string} [options.acceptLanguage] - The preferred language for the response.
-   *
-   * @param {number} [options.longRunningOperationRetryTimeout] - The retry timeout in seconds for Long Running Operations. Default value is 30.
-   *
-   * @param {boolean} [options.generateClientRequestId] - Whether a unique x-ms-client-request-id should be generated. When set to true a unique x-ms-client-request-id value is generated and included in each request. Default is true.
-   *
+   * @param {object} [options] The parameter options
    */
   constructor(credentials: msRest.ServiceClientCredentials, subscriptionId: string, options?: Models.AutoRestParameterizedCustomHostTestClientOptions) {
     super(credentials, subscriptionId, options);
