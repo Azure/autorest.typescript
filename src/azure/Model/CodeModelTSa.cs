6113c81b22b4256ea7236ee446d75f40c31c6d32
Rikki Gibson <rigibson@microsoft.com>
2018-10-05 09:47:41 -0700

    Update ms-rest[-azure]-js

--- src/azure/Model/CodeModelTSa.cs
+++ src/azure/Model/CodeModelTSa.cs
@@ -66,7 +66,7 @@ namespace AutoRest.TypeScript.Azure.Model
 
         public override string PackageDependencies()
         {
-            return "\"ms-rest-azure-js\": \"~0.17.165\"" + ",\n" + base.PackageDependencies();
+            return "\"ms-rest-azure-js\": \"^1.0.166\"" + ",\n" + base.PackageDependencies();
         }
 
         public string GenerateAzureServiceClientImports()
