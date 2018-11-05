215c92dfdbdd526c8a0a95dba13ac6cc5cbabddf
Dan Schulte <daschult@microsoft.com>
2018-11-05 12:43:01 -0800

    More changes to bundlesize.ts

--- .scripts/bundlesize.ts
+++ .scripts/bundlesize.ts
@@ -1,46 +1,36 @@
-import { promisify } from "util";
-import cp = require("child_process");
-import fs = require("fs");
-import path = require("path");
+import { spawnSync, SpawnSyncOptions } from "child_process";
+import * as fs from "fs";
+import * as path from "path";
+import * as filesize from "filesize";
 
-import filesize = require("filesize");
+function getBundleSize(ref: string): number {
+  runCommand(`git checkout ${ref}`);
+  runCommand("npm install");
+  runCommand("npm install", [], { cwd: "./test/multiapi" });
 
-const exec = promisify(cp.exec);
+  runCommand(`npx webpack`);
 
-async function getBundleSize(ref: string) {
-  await execVerbose(`git checkout ${ref}`);
-  await execVerbose("npm install --ignore-scripts");
-  await execVerbose("npm install", { cwd: "./test/multiapi" });
+  const testBundlePath: string = path.join(__dirname, "../testBundle.js");
+  const status: fs.Stats = fs.statSync(testBundlePath);
 
-  await new Promise((resolve) => {
-    const child: cp.ChildProcess = cp.spawn(path.join(__dirname, "../node_modules/.bin/webpack"), ['-p'], { stdio: 'inherit' });
-    child.on('exit', () => resolve());
-  });
-  const status: fs.Stats = fs.statSync(path.join(__dirname, "../testBundle.js"));
   return status.size;
 }
 
-function execVerbose(script: string, opts?: cp.ExecOptions): any {
-  const cwd = path.resolve(opts && opts.cwd || process.cwd());
-  console.log(`> ${cwd} - ${script}`);
-  return exec(script, { maxBuffer: 1024 * 1024, ...opts });
+function runCommand(command: string, args?: string[], options?: SpawnSyncOptions): void {
+  const cwd: string = path.resolve((options && options.cwd) || process.cwd());
+  console.log(`> ${cwd} - ${command}`);
+  spawnSync(command, args, { stdio: [0, 1, 2], maxBuffer: 1024 * 1024, ...options });
 }
 
 /**
  * Output a message to the console.
  */
-function outputMessage(message: string) {
+function outputMessage(message: string): void {
   const json = JSON.stringify({
     title: message,
     summary: ""
   });
-  console.log(`---output
-${json}
----`)
-}
-
-function outputErrorMessage(message: string, error: any) {
-  outputMessage(`${message}: ${JSON.stringify(error)}`);
+  console.log(`---output\n${json}\n---`)
 }
 
 function getRequiredEnvironmentVariable(environmentVariableName: string): string | undefined {
@@ -51,39 +41,21 @@ function getRequiredEnvironmentVariable(environmentVariableName: string): string
   return result;
 }
 
-async function main() {
-  let baseSize = undefined;
-  let headSize = undefined;
-
+try {
   const baseRef: string | undefined = getRequiredEnvironmentVariable("TRAVIS_BRANCH");
   const headRef: string | undefined = getRequiredEnvironmentVariable("TRAVIS_PULL_REQUEST_SHA");
 
   if (baseRef && headRef) {
-    try {
+    const baseSize: number = getBundleSize(baseRef);
+    const headSize: number = getBundleSize(headRef);
 
-      baseSize = await getBundleSize(baseRef);
-      headSize = await getBundleSize(headRef);
+    const change: number = (headSize / baseSize) - 1;
+    const percentChanged: string = (Math.abs(change) * 100).toFixed(2) + '%';
+    const bytesChanged: string = filesize(headSize - baseSize);
 
-      const change = (headSize / baseSize) - 1;
-      const percentChange = (Math.abs(change) * 100).toFixed(2) + '%';
-
-      outputMessage(`Size ${change >= 0 ? "increased" : "decreased"} by ${percentChange} (${filesize(headSize - baseSize)})`);
-    } catch (error) {
-      if (baseSize == undefined) {
-        outputErrorMessage(`ERROR: Failed to get the bundle size of the base commit (${baseRef})`, error);
-      } else if (headSize == undefined) {
-        outputErrorMessage(`ERROR: Failed to get the bundle size of the head commit (${headRef})`, error);
-      } else {
-        outputErrorMessage(`ERROR: Unrecognized error`, error);
-      }
-      throw error;
-    }
+    outputMessage(`Size ${change >= 0 ? "increased" : "decreased"} by ${percentChanged} (${bytesChanged})`);
   }
+} catch (error) {
+  outputMessage(`ERROR: ${JSON.stringify(error)}`);
+  process.exit(1);
 }
-
-main()
-  .catch(err => {
-    console.error(err.message);
-    console.error(err.stack);
-    process.exit(1);
-  });
