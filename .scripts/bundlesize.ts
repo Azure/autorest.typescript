7f013ff7e63aa383a128e7baa072fc38424cf277
Dan Schulte <daschult@microsoft.com>
2018-11-05 13:29:43 -0800

    Trying a few other changes

--- .scripts/bundlesize.ts
+++ .scripts/bundlesize.ts
@@ -1,14 +1,12 @@
-import { spawnSync, SpawnSyncOptions } from "child_process";
+import { ChildProcess, ExecOptions, spawn } from "child_process";
+import * as filesize from "filesize";
 import * as fs from "fs";
 import * as path from "path";
-import * as filesize from "filesize";
 
-function getBundleSize(ref: string): number {
-  runCommand(`git checkout ${ref}`);
-  runCommand("npm install");
-  runCommand("npm install", [], { cwd: "./test/multiapi" });
-
-  runCommand(`npx webpack`);
+async function getBundleSize(ref: string): Promise<number> {
+  await runCommand("git", ["checkout", ref]);
+  await runCommand("npm", ["install"], { cwd: "./test/multiapi" });
+  await runCommand("npx", ["webpack"]);
 
   const testBundlePath: string = path.join(__dirname, "../testBundle.js");
   const status: fs.Stats = fs.statSync(testBundlePath);
@@ -16,10 +14,25 @@ function getBundleSize(ref: string): number {
   return status.size;
 }
 
-function runCommand(command: string, args?: string[], options?: SpawnSyncOptions): void {
+function runCommand(command: string, args?: string[], options?: ExecOptions): Promise<number> {
   const cwd: string = path.resolve((options && options.cwd) || process.cwd());
-  console.log(`> ${cwd} - ${command}`);
-  spawnSync(command, args, { stdio: [0, 1, 2], maxBuffer: 1024 * 1024, ...options });
+  const commandString: string = `${command}${args && args.length ? " " + args.join(" ") : ""}`;
+  console.log(`> ${cwd} - ${commandString}`);
+  const childProcess: ChildProcess = spawn(command, args, { stdio: [0, 1, 2], shell: true, ...options });
+  return new Promise<number>((resolve, reject) => {
+    childProcess.on("close", (exitCode: number) => {
+      resolve(exitCode);
+    });
+    childProcess.on("disconnect", () => {
+      reject(new Error(`Disconnected from ${command}`));
+    })
+    childProcess.on("error", (error: Error) => {
+      reject(error);
+    });
+    childProcess.on("exit", (exitCode: number) => {
+      resolve(exitCode);
+    });
+  });
 }
 
 /**
@@ -41,21 +54,25 @@ function getRequiredEnvironmentVariable(environmentVariableName: string): string
   return result;
 }
 
-try {
-  const baseRef: string | undefined = getRequiredEnvironmentVariable("TRAVIS_BRANCH");
-  const headRef: string | undefined = getRequiredEnvironmentVariable("TRAVIS_PULL_REQUEST_SHA");
+async function main(): Promise<void> {
+  try {
+    const baseRef: string | undefined = getRequiredEnvironmentVariable("TRAVIS_BRANCH");
+    const headRef: string | undefined = getRequiredEnvironmentVariable("TRAVIS_PULL_REQUEST_SHA");
 
-  if (baseRef && headRef) {
-    const baseSize: number = getBundleSize(baseRef);
-    const headSize: number = getBundleSize(headRef);
+    if (baseRef && headRef) {
+      const baseSize: number = await getBundleSize(baseRef);
+      const headSize: number = await getBundleSize(headRef);
 
-    const change: number = (headSize / baseSize) - 1;
-    const percentChanged: string = (Math.abs(change) * 100).toFixed(2) + '%';
-    const bytesChanged: string = filesize(headSize - baseSize);
+      const change: number = (headSize / baseSize) - 1;
+      const percentChanged: string = (Math.abs(change) * 100).toFixed(2) + '%';
+      const bytesChanged: string = filesize(headSize - baseSize);
 
-    outputMessage(`Size ${change >= 0 ? "increased" : "decreased"} by ${percentChanged} (${bytesChanged})`);
+      outputMessage(`Size ${change >= 0 ? "increased" : "decreased"} by ${percentChanged} (${bytesChanged})`);
+    }
+  } catch (error) {
+    outputMessage(`ERROR: ${JSON.stringify(error)}`);
+    process.exit(1);
   }
-} catch (error) {
-  outputMessage(`ERROR: ${JSON.stringify(error)}`);
-  process.exit(1);
 }
+
+main();
