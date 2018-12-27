00108657d54e5f45130da4b56b59b1856ec4c28d
Dan Schulte <daschult@microsoft.com>
2018-12-26 19:18:23 -0800

    Update enum tests name

--- test/enumtypes/acceptanceTests.ts
+++ test/enumtypes/acceptanceTests.ts
@@ -1,6 +1,6 @@
 import { AutoRestSwaggerBATService, AutoRestSwaggerBATServiceModels } from "./generated/BodyString/autoRestSwaggerBATService";
 
-describe("model-enum-as-union flag", function () {
+describe("enum-types flag", function () {
   it("should perform basic put requests", async function () {
     const client = new AutoRestSwaggerBATService();
     await client.enumModel.putNotExpandable(AutoRestSwaggerBATServiceModels.Colors.Redcolor);
