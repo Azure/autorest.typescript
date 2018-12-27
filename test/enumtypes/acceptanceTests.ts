1df679a8a32487a4a5ae67000d0411fa61d2ecc6
Dan Schulte <daschult@microsoft.com>
2018-12-26 19:18:04 -0800

    Fix enum test

--- test/enumtypes/acceptanceTests.ts
+++ test/enumtypes/acceptanceTests.ts
@@ -1,8 +1,8 @@
-import { AutoRestSwaggerBATService } from "./generated/BodyString/autoRestSwaggerBATService";
+import { AutoRestSwaggerBATService, AutoRestSwaggerBATServiceModels } from "./generated/BodyString/autoRestSwaggerBATService";
 
-describe("model-enum-as-union flag", function() {
-    it("should perform basic put requests", async function() {
-        const client = new AutoRestSwaggerBATService();
-        await client.enumModel.putNotExpandable("red color");
-    });
+describe("model-enum-as-union flag", function () {
+  it("should perform basic put requests", async function () {
+    const client = new AutoRestSwaggerBATService();
+    await client.enumModel.putNotExpandable(AutoRestSwaggerBATServiceModels.Colors.Redcolor);
+  });
 });
