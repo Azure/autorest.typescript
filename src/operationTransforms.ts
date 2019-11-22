1d4ed64ca0ae974b8d0c843a2212d2edf743f5af
Jose Manuel Heredia Hidalgo <joheredi@microsoft.com>
2019-11-22 15:31:00 -0800

    Generate operation specs

---
+++ src/operationTransforms.ts
@@ -0,0 +1,136 @@
+import {
+  OperationSpec,
+  Serializer,
+  HttpMethods,
+  OperationResponse
+} from "@azure/core-http";
+import {
+  Operation,
+  Request,
+  SchemaResponse,
+  Response,
+  Schema,
+  SchemaType,
+  ChoiceSchema
+} from "@azure-tools/codemodel";
+import { getLanguageMetadata } from "./transforms";
+
+export function transformOperationSpec(operation: Operation): OperationSpec {
+  // Extract protocol information
+  const httpInfo = extractHttpDetails(operation.request);
+  const serializer = new Serializer();
+
+  return {
+    ...httpInfo,
+    responses: extractResponses(operation.responses, operation.exceptions),
+    serializer
+  };
+}
+
+export function extractHttpDetails({ protocol }: Request) {
+  if (!protocol.http) {
+    throw new Error("operation doesn't contain a definition for HTTP protocol");
+  }
+  const { path, method } = protocol.http;
+
+  return {
+    path,
+    httpMethod: method.toUpperCase() as HttpMethods
+  };
+}
+
+export type OperationResponses = { [responseCode: string]: OperationResponse };
+export function extractResponses(
+  responses?: Array<SchemaResponse | Response>,
+  exceptions: Array<SchemaResponse | Response> = []
+): OperationResponses {
+  if (!responses || !responses.length) {
+    return {};
+  }
+  const schemaResponses = extractSchemaResponses(responses);
+  const defaultResponse = extractSchemaResponses(exceptions);
+
+  return {
+    ...schemaResponses,
+    ...defaultResponse
+  };
+}
+
+export interface SpecType {
+  name: any;
+  allowedValues?: any[];
+}
+
+export function getSpecType(responseSchema: Schema): any {
+  let typeName: string;
+  let allowedValues: any[] | undefined = undefined;
+  switch (responseSchema.type) {
+    case SchemaType.ByteArray:
+      typeName = "Base64Url";
+      break;
+    case SchemaType.String:
+    case SchemaType.Constant:
+      typeName = "String";
+      break;
+    case SchemaType.Choice:
+      const choiceSchema = responseSchema as ChoiceSchema;
+      typeName = "Enum";
+      allowedValues = choiceSchema.choices.map(choice => choice.value);
+      break;
+    case SchemaType.Object:
+      const name = getLanguageMetadata(responseSchema.language).name;
+      return `Mappers.${name}`;
+    default:
+      throw new Error(`Unsupported Spec Type ${responseSchema.type}`);
+  }
+
+  let result = {
+    name: typeName
+  };
+
+  return !!allowedValues ? { ...result, allowedValues } : result;
+}
+
+export function extractBasicResponses(
+  responses: Array<Response | SchemaResponse>
+) {
+  return (responses as any[]).filter((r: SchemaResponse) => !r.schema);
+}
+
+export function extractSchemaResponses(
+  responses: Array<Response | SchemaResponse>
+) {
+  return responses.reduce(
+    (result: OperationResponses, response: SchemaResponse | Response) => {
+      if (!response.protocol.http) {
+        return result;
+      }
+
+      const statusCodes = response.protocol.http.statusCodes;
+
+      if (!statusCodes || !statusCodes.length) {
+        return result;
+      }
+
+      const statusCode = statusCodes[0];
+      result[statusCode] = {};
+      const schemaResponse = response as any;
+      if (schemaResponse.schema) {
+        result[statusCode] = {
+          bodyMapper: getBodyMapperFromSchema(schemaResponse.schema)
+        };
+      }
+      return result;
+    },
+    {}
+  );
+}
+
+function getBodyMapperFromSchema(responseSchema: Schema) {
+  const responseType = getSpecType(responseSchema);
+  return !responseType.name
+    ? responseType
+    : {
+        type: responseType
+      };
+}
---
+++ test/unit/operationTransforms.spec.ts
@@ -0,0 +1,192 @@
+import * as assert from "assert";
+import {
+  transformOperationSpec,
+  getSpecType
+} from "../../src/operationTransforms";
+import {
+  Operation,
+  SchemaResponse,
+  SchemaType,
+  Schema,
+  ChoiceValue,
+  ChoiceSchema,
+  ObjectSchema
+} from "@azure-tools/codemodel";
+import { KnownMediaType } from "@azure-tools/codegen";
+import { OperationSpec } from "@azure/core-http";
+
+const choice = new ChoiceSchema("mockChoice", "", {
+  choices: [
+    new ChoiceValue("red", "", "red color"),
+    new ChoiceValue("green-color", "", "green-color"),
+    new ChoiceValue("blue_color", "", "blue_color")
+  ]
+});
+
+const constantSchema = new Schema(
+  "paths·string-null·get·responses·200·content·application-json·schema",
+  "",
+  SchemaType.Constant
+);
+
+const objectSchema = new ObjectSchema("RefColorConstant", "", {});
+const errorSchema = new ObjectSchema("ErrorModel", "", {});
+
+describe("OperationTransforms", () => {
+  describe("getSpecType", () => {
+    it("should return string when type is constant or string", () => {
+      assert.strictEqual(
+        getSpecType({ type: SchemaType.Constant } as any).name,
+        "String",
+        "constant"
+      );
+      assert.strictEqual(
+        getSpecType({ type: SchemaType.String } as any).name,
+        "String",
+        "string"
+      );
+    });
+    it("should return Base64Url when type is ByteArray", () => {
+      assert.strictEqual(
+        getSpecType({ type: SchemaType.ByteArray } as any).name,
+        "Base64Url"
+      );
+    });
+    it("should return the right Enum type for Choice", () => {
+      const choiceType = getSpecType(choice);
+      assert.strictEqual(choiceType.name, "Enum");
+      assert.deepEqual(choiceType.allowedValues, [
+        "red color",
+        "green-color",
+        "blue_color"
+      ]);
+    });
+    it("should return a reference to a mapper when type is object", () => {
+      const objectType = getSpecType(objectSchema);
+      assert.strictEqual(objectType, `Mappers.RefColorConstant`);
+    });
+  });
+
+  describe("transformOperationSpec", () => {
+    describe("Simple get operation", () => {
+      const operationPath = "/string/null";
+      const getErrorResponseSchema = () => {
+        const schema = errorSchema;
+        const response = new SchemaResponse(schema);
+
+        response.protocol = {
+          http: {
+            knownMediaType: KnownMediaType.Json,
+            statusCodes: ["default"],
+            mediaTypes: "application/json"
+          } as any
+        };
+
+        return response;
+      };
+      const get200ResponseSchema = (schema: Schema) => {
+        const response = new SchemaResponse(schema);
+
+        response.protocol = {
+          http: {
+            knownMediaType: KnownMediaType.Json,
+            statusCodes: [200],
+            mediaTypes: "application/json"
+          } as any
+        };
+
+        return response;
+      };
+
+      const getOperation = (responseSchema?: SchemaResponse) => {
+        return new Operation("", "", {
+          request: {
+            parameters: [],
+            protocol: {
+              http: {
+                path: operationPath,
+                method: "get",
+                url: "{$host}"
+              }
+            }
+          },
+          responses: [
+            responseSchema || { protocol: { http: { statusCodes: ["200"] } } }
+          ],
+          exceptions: [getErrorResponseSchema()],
+          language: {
+            default: {
+              name: "getNull",
+              description: "Get null string value value"
+            }
+          }
+        });
+      };
+
+      const checkHttpMethodAndPath = (operationSpec: OperationSpec) => {
+        assert.strictEqual(
+          operationSpec.httpMethod,
+          "GET",
+          `expected HTTPMethod to be GET, actual ${operationSpec.httpMethod}`
+        );
+
+        assert.strictEqual(
+          operationSpec.path,
+          operationPath,
+          `expected PATH to be ${operationPath}, actual ${operationSpec.path}`
+        );
+      };
+
+      it("should create an operation spec  with correct http details", () => {
+        const okResponseSchema = get200ResponseSchema(constantSchema);
+        const mockOperation = getOperation(okResponseSchema);
+        const operationSpec = transformOperationSpec(mockOperation);
+        checkHttpMethodAndPath(operationSpec);
+      });
+
+      it("should create an operation spec with correct responses from a basic response", () => {
+        const mockOperation = getOperation();
+        const operationSpec = transformOperationSpec(mockOperation);
+        checkHttpMethodAndPath(operationSpec);
+        assert.deepEqual(operationSpec.responses[200], {});
+      });
+
+      it("should create an operation spec with correct responses spec and cosntant schema response", () => {
+        const okResponseSchema = get200ResponseSchema(constantSchema);
+        const mockOperation = getOperation(okResponseSchema);
+        const operationSpec = transformOperationSpec(mockOperation);
+        checkHttpMethodAndPath(operationSpec);
+        const okResponse = operationSpec.responses[200];
+        // assert.strictEqual(
+        //   okResponse.bodyMapper!.serializedName,
+        //   "parsedResponse"
+        // );
+        assert.deepEqual(okResponse.bodyMapper!.type, { name: "String" });
+        assert.deepEqual(
+          operationSpec.responses.default.bodyMapper,
+          "Mappers.ErrorModel"
+        );
+      });
+
+      it("should create an operation spec with correct responses spec and choice schema response", () => {
+        const okResponseSchema = get200ResponseSchema(choice);
+        const mockOperation = getOperation(okResponseSchema);
+        const operationSpec = transformOperationSpec(mockOperation);
+        checkHttpMethodAndPath(operationSpec);
+        const okResponse = operationSpec.responses[200];
+        // assert.strictEqual(
+        //   okResponse.bodyMapper!.serializedName,
+        //   "parsedResponse"
+        // );
+        assert.deepEqual(okResponse.bodyMapper!.type, {
+          name: "Enum",
+          allowedValues: ["red color", "green-color", "blue_color"]
+        });
+        assert.deepEqual(
+          operationSpec.responses.default.bodyMapper,
+          "Mappers.ErrorModel"
+        );
+      });
+    });
+  });
+});
