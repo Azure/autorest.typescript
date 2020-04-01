a4f6322059714390a832b73a8ac82361b61736e1
Jose Manuel Heredia Hidalgo <joheredi@microsoft.com>
2020-04-01 15:36:25 -0700

    Initial LRO Generation

---
+++ test/integration/generated/lro/src/lro/lroPoller.ts
@@ -0,0 +1,70 @@
+import { Poller } from "@azure/core-lro";
+import { OperationSpec, OperationArguments, delay } from "@azure/core-http";
+import { BaseResult, LROOperationState } from "./models";
+import { makeOperation } from "./operation";
+
+export type SendOperationFn<TResult extends BaseResult> = (
+  args: OperationArguments,
+  spec: OperationSpec
+) => Promise<TResult>;
+
+export interface LROPollerOptions<TResult extends BaseResult> {
+  /**
+   * Defines how much time the poller is going to wait before making a new request to the service.
+   */
+  intervalInMs?: number;
+  /**
+   * Arguments used to send the initial operation
+   */
+  initialOperationArguments: OperationArguments;
+  /**
+   * Operation spec provided for the initial operation
+   */
+  initialOperationSpec: OperationSpec;
+  /**
+   * Result from the initial operation
+   */
+  initialOperationResult: TResult;
+  /**
+   * Function to execute an operation based on an operation spec and arguments
+   */
+  sendOperation: SendOperationFn<TResult>;
+}
+
+export class LROPoller<TResult extends BaseResult> extends Poller<
+  LROOperationState<TResult>,
+  TResult
+> {
+  private intervalInMs: number;
+
+  constructor({
+    initialOperationArguments,
+    initialOperationResult,
+    initialOperationSpec,
+    sendOperation,
+    intervalInMs = 2000
+  }: LROPollerOptions<TResult>) {
+    const state: LROOperationState<TResult> = {
+      // Initial operation will become the last operation
+      lastOperation: {
+        args: initialOperationArguments,
+        spec: initialOperationSpec,
+        result: initialOperationResult
+      },
+      sendOperation,
+      result: initialOperationResult
+    };
+
+    const operation = makeOperation(state);
+    super(operation);
+
+    this.intervalInMs = intervalInMs;
+  }
+
+  /**
+   * The method used by the poller to wait before attempting to update its operation.
+   */
+  async delay(): Promise<void> {
+    delay(this.intervalInMs);
+  }
+}
---
+++ test/integration/generated/lro/src/lro/models.ts
@@ -0,0 +1,42 @@
+import {
+  OperationArguments,
+  OperationSpec,
+  RestResponse
+} from "@azure/core-http";
+import { PollOperationState, PollOperation } from "@azure/core-lro";
+
+export interface BaseResult extends RestResponse {
+  location?: string;
+  operationLocation?: string;
+  azureAsyncOperation?: string;
+  provisioningState?: string;
+  properties?: {
+    provisioningState?: string;
+  };
+}
+
+export interface LastOperation<TResult extends BaseResult> {
+  args: OperationArguments;
+  spec: OperationSpec;
+  result: TResult;
+}
+
+export interface LROOperationState<TResult extends BaseResult>
+  extends PollOperationState<TResult> {
+  lastOperation: LastOperation<TResult>;
+  sendOperation: (
+    args: OperationArguments,
+    spec: OperationSpec
+  ) => Promise<TResult>;
+}
+
+export interface LROStrategy<TResult extends BaseResult> {
+  isTerminal: () => boolean;
+  sendFinalRequest: () => Promise<LastOperation<TResult>>;
+  poll: () => Promise<LastOperation<TResult>>;
+}
+
+export type LROOperation<TResult extends BaseResult> = PollOperation<
+  LROOperationState<TResult>,
+  TResult
+>;
