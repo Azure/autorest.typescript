a6306a7f3512b46e56c1b68d5d2c3976f5c84cb2
Jose Manuel Heredia Hidalgo <joheredi@microsoft.com>
2020-05-05 16:27:11 -0700

    remove network to help review

--- test/utils/smoke-test-list.ts
+++ test/utils/smoke-test-list.ts
@@ -4,7 +4,6 @@ export interface SpecDefinition {
 }
 
 export const readmes: SpecDefinition[] = [
-  { path: "./.tmp/specs/specification/network/resource-manager/readme.md" },
   { path: "./.tmp/specs/specification/keyvault/resource-manager/readme.md" },
   { path: "./.tmp/specs/specification/storage/resource-manager/readme.md" },
   { path: "./.tmp/specs/specification/msi/resource-manager/readme.md" },
