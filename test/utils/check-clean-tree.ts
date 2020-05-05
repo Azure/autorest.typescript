083d0e34eaf60d0bf25261d11165b6a3c5de4515
Jose Manuel Heredia Hidalgo <joheredi@microsoft.com>
2020-05-05 11:44:49 -0700

    add npm script and check tree on ci

--- test/utils/check-clean-tree.ts
+++ test/utils/check-clean-tree.ts
@@ -1,10 +1,9 @@
 import { spawn, ChildProcess } from "child_process";
 
-const onExit = (childProcess: ChildProcess) => {
+const onExit = (childProcess: ChildProcess): Promise<string[]> => {
   let messages: string[] = [];
   return new Promise((resolve, reject) => {
     if (childProcess.stdout) {
-      console.log("HEY!!!");
       childProcess.stdout.on("data", message => messages.push(message));
     }
     childProcess.once("exit", (code: number, signal: string) => {
@@ -27,11 +26,30 @@ async function check_tree() {
     })
   );
 
+  // If there is any output from this command it means that
+  // there are non committed changes so we need to handle
+  // stout
   const messages = await onExit(
     spawn("git", ["diff", "--staged", "--compact-summary"])
   );
 
-  console.log(messages);
+  if (messages.length !== 0) {
+    // Once we have verified that there are non committed changes
+    // run git diff again, this time forwarding the stout to present
+    // a readable hint to the user
+    await onExit(
+      spawn("git", ["diff", "--staged", "--compact-summary"], {
+        stdio: [process.stdin, process.stdout, process.stderr]
+      })
+    );
+
+    throw new Error(
+      "Git tree is dirty, regenerate all test swaggers and make sure that there are no un-intended changes"
+    );
+  }
 }
 
-check_tree().catch(console.error);
+check_tree().catch(error => {
+  console.error(error);
+  process.exit(-1);
+});
