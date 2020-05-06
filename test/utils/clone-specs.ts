d8572ffe38102c5b8ec2a4dd68e626a0190b839c
Jose Manuel Heredia Hidalgo <joheredi@microsoft.com>
2020-05-06 11:26:27 -0700

    Selectively fetch branches and commits

--- test/utils/clone-specs.ts
+++ test/utils/clone-specs.ts
@@ -1,6 +1,7 @@
-import { promises, exists, rmdirSync } from "fs";
+import { exists } from "fs";
 import { spawn, ChildProcess } from "child_process";
-import { SPECS_PATH } from "./constants";
+import { SPECS_PATH, DEFAULT_SPEC_BRANCH } from "./constants";
+import { readmes } from "./smoke-test-list";
 
 const SPECS_REPO = "https://github.com/Azure/azure-rest-api-specs.git";
 
@@ -30,8 +31,22 @@ async function removeSpecsPath() {
 }
 
 async function cloneSpecsRepo() {
-  const childProcess = spawn("git", ["clone", SPECS_REPO, SPECS_PATH], {
-    stdio: [process.stdin, process.stdout, process.stderr]
+  const childProcess = spawn(
+    "git",
+    ["clone", SPECS_REPO, SPECS_PATH, "--depth", "1", "--single-branch"],
+    {
+      stdio: [process.stdin, process.stdout, process.stderr]
+    }
+  );
+
+  return await onExit(childProcess);
+}
+
+async function fetchBranchOrCommit(branch: string) {
+  console.log(`Fetching branch ${branch}`);
+  const childProcess = spawn("git", ["fetch", "origin", branch], {
+    stdio: [process.stdin, process.stdout, process.stderr],
+    cwd: SPECS_PATH
   });
 
   return await onExit(childProcess);
@@ -45,8 +60,24 @@ async function removeIfExists() {
 }
 
 async function cloneSpecs() {
+  const branches = [
+    ...readmes.reduce<Set<string>>(
+      (set, readme) => {
+        if (readme.branch) {
+          set.add(readme.branch);
+        }
+        return set;
+      },
+      new Set<string>([DEFAULT_SPEC_BRANCH])
+    )
+  ];
+
   await removeIfExists();
   await cloneSpecsRepo();
+
+  for (let branch of branches) {
+    await fetchBranchOrCommit(branch);
+  }
 }
 
 cloneSpecs().catch(console.error);
