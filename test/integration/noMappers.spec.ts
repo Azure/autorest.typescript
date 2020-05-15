72af82562235832b8b55eeac10d2de2be75c39ec
Jose Manuel Heredia Hidalgo <joheredi@microsoft.com>
2020-05-15 11:35:08 -0700

    [v6] Fix failing ci (#645)
    * Restructure CI
    
    * Reuse Build steps

--- test/integration/noMappers.spec.ts
+++ test/integration/noMappers.spec.ts
@@ -2,7 +2,7 @@ import { NoMappersClient } from "./generated/noMappers/src/noMappersClient";
 import { assert } from "chai";
 describe("Swagger that needs no mapper", () => {
   it("should be able to instantiate a new client without Typescript errors", () => {
-    const client = new NoMappersClient("http://localhost:3000");
+    const client = new NoMappersClient("http://localhost:3000", "one");
     assert.isDefined(client);
   });
 });
