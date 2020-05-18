a4df799d7e05dd65247810657bac70668beb8247
Jose Manuel Heredia Hidalgo <joheredi@microsoft.com>
2020-05-18 15:32:10 -0700

    [v6] Add SQL and ARM to smoke test (#646)
    * Add SQL and ARM to smoke test
    
    * Fix name
    
    * Pass param
    
    * dedupe storage
    
    * Update grouping
    
    * Add one more tag

---
+++ test/smoke/generated/arm-package-features-2015-12/README.md
@@ -0,0 +1,27 @@
+## Azure FeatureClient SDK for JavaScript
+
+This package contains an isomorphic SDK for FeatureClient.
+
+### Currently supported environments
+
+- Node.js version 8.x.x or higher
+- Browser JavaScript
+
+### How to Install
+
+```bash
+npm install arm-package-features-2015-12
+```
+
+### How to use
+
+#### Sample code
+
+Refer the sample code in the [azure-sdk-for-js-samples](https://github.com/Azure/azure-sdk-for-js-samples) repository.
+
+## Related projects
+
+- [Microsoft Azure SDK for Javascript](https://github.com/Azure/azure-sdk-for-js)
+
+
+![Impressions](https://azure-sdk-impressions.azurewebsites.net/api/impressions/azure-sdk-for-js%2Fsdk%2Fcdn%2Farm-cdn%2FREADME.png)
--- test/utils/smoke-test-list.ts
+++ test/utils/smoke-test-list.ts
@@ -1,28 +1,84 @@
+type BuildTag = "ci_1" | "ci_2" | "ci_3";
+
 export interface SpecDefinition {
   path: string;
   branch?: string;
   params?: string[];
+  outputFolderName?: string;
+  buildTag?: BuildTag;
 }
 
 export enum AutorestParams {
   ModelDedup = "--modelerfour.lenient-model-deduplication"
 }
 
+const getArmReadmes = (): SpecDefinition[] => {
+  const armTags = [
+    "package-features-2015-12",
+    "package-locks-2016-09",
+    "package-policy-2019-09",
+    "package-resources-2019-08",
+    "package-subscriptions-2019-06",
+    "package-links-2016-09",
+    "package-managedapplications-2018-06",
+    "package-deploymentscripts-2019-10-preview"
+  ];
+  return armTags.map(tag => ({
+    path: "./.tmp/specs/specification/resources/resource-manager/readme.md",
+    params: [`--tag=${tag}`],
+    outputFolderName: `arm-${tag}`,
+    buildTag: "ci_1"
+  }));
+};
+
 export const readmes: SpecDefinition[] = [
-  { path: "./.tmp/specs/specification/web/resource-manager/readme.md" },
-  { path: "./.tmp/specs/specification/monitor/data-plane/readme.md" },
-  { path: "./.tmp/specs/specification/graphrbac/data-plane/readme.md" },
+  ...getArmReadmes(),
+  {
+    path: "./.tmp/specs/specification/sql/resource-manager/readme.md",
+    params: [AutorestParams.ModelDedup],
+    buildTag: "ci_1"
+  },
+  {
+    path: "./.tmp/specs/specification/web/resource-manager/readme.md",
+    buildTag: "ci_2"
+  },
+  {
+    path: "./.tmp/specs/specification/monitor/data-plane/readme.md",
+    buildTag: "ci_2"
+  },
+  {
+    path: "./.tmp/specs/specification/graphrbac/data-plane/readme.md",
+    buildTag: "ci_2"
+  },
   {
     path: "./.tmp/specs/specification/cosmos-db/resource-manager/readme.md",
-    params: [AutorestParams.ModelDedup]
+    params: [AutorestParams.ModelDedup],
+    buildTag: "ci_2"
   },
-  { path: "./.tmp/specs/specification/compute/resource-manager/readme.md" },
-  { path: "./.tmp/specs/specification/network/resource-manager/readme.md" },
-  { path: "./.tmp/specs/specification/keyvault/resource-manager/readme.md" },
-  { path: "./.tmp/specs/specification/storage/resource-manager/readme.md" },
-  { path: "./.tmp/specs/specification/msi/resource-manager/readme.md" },
+  {
+    path: "./.tmp/specs/specification/compute/resource-manager/readme.md",
+    buildTag: "ci_2"
+  },
+  {
+    path: "./.tmp/specs/specification/network/resource-manager/readme.md",
+    buildTag: "ci_3"
+  },
+  {
+    path: "./.tmp/specs/specification/keyvault/resource-manager/readme.md",
+    buildTag: "ci_3"
+  },
+  {
+    path: "./.tmp/specs/specification/storage/resource-manager/readme.md",
+    params: [AutorestParams.ModelDedup],
+    buildTag: "ci_3"
+  },
+  {
+    path: "./.tmp/specs/specification/msi/resource-manager/readme.md",
+    buildTag: "ci_3"
+  },
   {
     path:
-      "./.tmp/specs/specification/adhybridhealthservice/resource-manager/readme.md"
+      "./.tmp/specs/specification/adhybridhealthservice/resource-manager/readme.md",
+    buildTag: "ci_3"
   }
 ];
