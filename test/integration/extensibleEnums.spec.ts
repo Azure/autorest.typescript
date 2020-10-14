94497274cec311cdecb203694015c3845b07e058
Sarangan Rajamanickam <sarajama@microsoft.com>
2020-10-14 20:53:17 +0000

    Change expect to assert

--- test/integration/extensibleEnums.spec.ts
+++ test/integration/extensibleEnums.spec.ts
@@ -1,5 +1,8 @@
-import { expect } from "chai";
-import { ExtensibleEnumsClient } from "./generated/extensibleEnums/src";
+import { assert } from "chai";
+import {
+  ExtensibleEnumsClient,
+  PetGetByPetIdResponse
+} from "./generated/extensibleEnums/src";
 
 describe("Integration tests for extensible enums", () => {
   let client: ExtensibleEnumsClient;
@@ -10,35 +13,44 @@ describe("Integration tests for extensible enums", () => {
 
   it("sends an unexpected enum value successfully", async () => {
     const response = await client.pet.getByPetId("casper");
-    expect(response).to.deep.equal({
+    const expected: Partial<PetGetByPetIdResponse> = {
       daysOfWeek: "Weekend",
       intEnum: "2",
       name: "Casper Ghosty"
-    });
-    expect(response._response.status).to.equal(200, `Unexpected status code.`);
+    };
+    assert.deepEqual(response, expected);
+    assert.equal(response._response.status, 200);
   });
 
   it("sends an expected enum value successfully", async () => {
     const response = await client.pet.getByPetId("tommy");
-    expect(response).to.deep.equal({
+    const expected: Partial<PetGetByPetIdResponse> = {
       daysOfWeek: "Monday",
       intEnum: "1",
       name: "Tommy Tomson"
-    });
-    expect(response._response.status).to.equal(200, `Unexpected status code.`);
+    };
+    assert.deepEqual(response, expected);
+    assert.equal(response._response.status, 200);
   });
 
   it("sends an allowed enum value successfully", async () => {
     const response = await client.pet.getByPetId("scooby");
-    expect(response).to.deep.equal({
+    const expected: Partial<PetGetByPetIdResponse> = {
       daysOfWeek: "Thursday",
       intEnum: "2.1",
       name: "Scooby Scarface"
-    });
-    expect(response._response.status).to.equal(200, `Unexpected status code.`);
+    };
+    assert.deepEqual(response, expected);
+    assert.equal(response._response.status, 200);
   });
 
   it("sends and receives enum value successfully", async () => {
+    const expected: Partial<PetGetByPetIdResponse> = {
+      name: "Retriever",
+      intEnum: "3",
+      daysOfWeek: "Sunday"
+    };
+
     const response = await client.pet.addPet({
       petParam: {
         name: "Retriever",
@@ -46,11 +58,7 @@ describe("Integration tests for extensible enums", () => {
         daysOfWeek: "Sunday"
       }
     });
-    expect(response).to.deep.equal({
-      name: "Retriever",
-      intEnum: "3",
-      daysOfWeek: "Sunday"
-    });
-    expect(response._response.status).to.equal(200, `Unexpected status code.`);
+    assert.deepEqual(response, expected);
+    assert.equal(response._response.status, 200);
   });
 });
