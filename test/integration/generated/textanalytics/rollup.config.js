59553f68b30405abd15b938cdfc25dd3c77c4c7d
Sarangan Rajamanickam <sarajama@microsoft.com>
2021-03-01 21:46:54 -0800

    Update Rollup Config of text analytics

--- test/integration/generated/textanalytics/rollup.config.js
+++ test/integration/generated/textanalytics/rollup.config.js
@@ -13,15 +13,11 @@ import sourcemaps from "rollup-plugin-sourcemaps";
 /** @type {rollup.RollupFileOptions} */
 const config = {
   input: "./esm/generatedClient.js",
-  external: ["@azure/core-http"],
   output: {
     file: "./dist/textanalytics.js",
     format: "umd",
     name: "Textanalytics",
     sourcemap: true,
-    globals: {
-      "@azure/core-http": "coreHttp"
-    },
     banner: `/*
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Licensed under the MIT License. See License.txt in the project root for license information.
