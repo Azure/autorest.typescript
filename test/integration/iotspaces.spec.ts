9502e58ac8a70144359fd93fba8e42214b17f353
Sarangan Rajamanickam <sarajama@microsoft.com>
2021-06-28 01:30:06 -0700

    Fix CI Failures

--- test/integration/iotspaces.spec.ts
+++ test/integration/iotspaces.spec.ts
@@ -1,11 +1,27 @@
 import { IoTSpacesClient } from "./generated/iotspaces/src";
+import { TokenCredential } from "@azure/core-auth";
 import { assert } from "chai";
 
 describe("Integration tests for IoTSpaces", () => {
   let client: IoTSpacesClient;
 
   it("should create a client successfully", async () => {
-    client = new IoTSpacesClient(undefined);
+    const expectedScopes = [
+      "https://microsoft.com/.default",
+      "http://microsoft.com/.default"
+    ];
+
+    const mockCredential: TokenCredential = {
+      getToken: async scopes => {
+        assert.deepEqual(scopes, expectedScopes);
+        return {
+          token: "test-token",
+          expiresOnTimestamp: 111111
+        };
+      }
+    };
+
+    client = new IoTSpacesClient(mockCredential);
     assert.notEqual(client, null);
   });
 });
