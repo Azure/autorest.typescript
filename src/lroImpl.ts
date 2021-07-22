44f35e0f4d904f71ebf047fc14fa54a43b80e604
deyaaeldeen <dealmaha@microsoft.com>
2021-07-22 23:46:19 +0000

    apply changes in main

--- src/lroImpl.ts
+++ src/lroImpl.ts
@@ -3,17 +3,22 @@ import { LongRunningOperation, LroResponse } from "@azure/core-lro";
 export class LroImpl<T> implements LongRunningOperation<T> {
   constructor(
     private sendOperationFn: (args: any, spec: any) => Promise<LroResponse<T>>,
-    private args: any,
-    private spec: any,
-    public requestPath: string = spec.path,
+    private args: Record<string, unknown>,
+    private spec: {
+      readonly requestBody?: unknown;
+      readonly path?: string;
+      readonly httpMethod: string;
+    } & Record<string, any>,
+    public requestPath: string = spec.path!,
     public requestMethod: string = spec.httpMethod
   ) {}
   public async sendInitialRequest(): Promise<LroResponse<T>> {
     return this.sendOperationFn(this.args, this.spec);
   }
   public async sendPollRequest(path: string): Promise<LroResponse<T>> {
+    const { requestBody, ...restSpec } = this.spec;
     return this.sendOperationFn(this.args, {
-      ...this.spec,
+      ...restSpec,
       path,
       httpMethod: "GET"
     });
