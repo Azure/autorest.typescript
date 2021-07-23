b07cba38f26fd787220691a2c2b4f2e716604aab
Jose Manuel Heredia Hidalgo <joheredi@microsoft.com>
2021-07-23 15:36:24 -0700

    re-generate multipleInheritance

---
+++ test/integration/generated/multipleInheritanceRest/src/index.ts
@@ -0,0 +1,11 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import MultipleInheritanceRestClient from "./multipleInheritanceRestClient";
+
+export * from "./multipleInheritanceRestClient";
+export * from "./models";
+export * from "./parameters";
+export * from "./responses";
+
+export default MultipleInheritanceRestClient;
