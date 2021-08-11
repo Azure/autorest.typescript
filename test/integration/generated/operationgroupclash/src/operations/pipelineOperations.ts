95cacede8819ee017da6be46e62a5e3c3f425338
Jose Manuel Heredia Hidalgo <joheredi@microsoft.com>
2021-08-11 16:44:44 -0700

    Reserve pipeline client member (#1150)

---
+++ test/integration/generated/operationgroupclash/src/operations/pipelineOperations.ts
@@ -0,0 +1,49 @@
+import { PipelineOperations } from "../operationsInterfaces";
+import * as coreClient from "@azure/core-client";
+import * as Mappers from "../models/mappers";
+import * as Parameters from "../models/parameters";
+import { OperationGroupClashClientContext } from "../operationGroupClashClientContext";
+import {
+  PipelineOperationsApiV1ValueGetOptionalParams,
+  PipelineOperationsApiV1ValueGetResponse
+} from "../models";
+
+/** Class representing a PipelineOperations. */
+export class PipelineOperationsImpl implements PipelineOperations {
+  private readonly client: OperationGroupClashClientContext;
+
+  /**
+   * Initialize a new instance of the class PipelineOperations class.
+   * @param client Reference to the service client
+   */
+  constructor(client: OperationGroupClashClientContext) {
+    this.client = client;
+  }
+
+  /** @param options The options parameters. */
+  apiV1ValueGet(
+    options?: PipelineOperationsApiV1ValueGetOptionalParams
+  ): Promise<PipelineOperationsApiV1ValueGetResponse> {
+    return this.client.sendOperationRequest(
+      { options },
+      apiV1ValueGetOperationSpec
+    );
+  }
+}
+// Operation Specifications
+const serializer = coreClient.createSerializer(Mappers, /* isXml */ false);
+
+const apiV1ValueGetOperationSpec: coreClient.OperationSpec = {
+  path: "/api/v1/pipeline",
+  httpMethod: "GET",
+  responses: {
+    200: {
+      bodyMapper: {
+        type: { name: "Sequence", element: { type: { name: "String" } } }
+      }
+    }
+  },
+  urlParameters: [Parameters.$host],
+  headerParameters: [Parameters.accept, Parameters.apiVersion],
+  serializer
+};
