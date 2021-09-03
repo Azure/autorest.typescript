0c81898175c666958686ddcc5a1fe3ea5fbbe2eb
qiaozha <qiaozha@microsoft.com>
2021-09-03 20:07:27 +0800

    remove-duplicate-import

--- test/smoke/generated/arm-package-features-2015-12/src/featureClient.ts
+++ test/smoke/generated/arm-package-features-2015-12/src/featureClient.ts
@@ -8,7 +8,6 @@
 
 import * as coreClient from "@azure/core-client";
 import * as coreAuth from "@azure/core-auth";
-import "@azure/core-paging";
 import { PagedAsyncIterableIterator } from "@azure/core-paging";
 import { FeaturesImpl } from "./operations";
 import { Features } from "./operationsInterfaces";
