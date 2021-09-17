54bd7c9d61b019ea6e65d9426b2b66c347d33f05
Jose Manuel Heredia Hidalgo <joheredi@microsoft.com>
2021-09-17 09:45:11 -0500

    Update generated test swaggers

---
+++ test/integration/generated/multipleInheritanceRest/src/outputModels.ts
@@ -0,0 +1,28 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+export interface PetOutput {
+  name: string;
+}
+
+export interface HorseOutput extends PetOutput {
+  isAShowHorse?: boolean;
+}
+
+export interface ErrorModelOutput {
+  status?: number;
+  message?: string;
+}
+
+export interface FelineOutput {
+  meows?: boolean;
+  hisses?: boolean;
+}
+
+export interface CatOutput extends PetOutput, FelineOutput {
+  likesMilk?: boolean;
+}
+
+export interface KittenOutput extends CatOutput {
+  eatsMiceYet?: boolean;
+}
