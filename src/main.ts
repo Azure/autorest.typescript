da466501a0d32c15f8b2c9d5894318d0c6d1a8dc
dw511214992 <dw511214992@gmail.com>
2022-03-09 12:10:04 +0800

    fix unknown error

--- src/main.ts
+++ src/main.ts
@@ -23,7 +23,7 @@ export async function processRequest(host: AutorestExtensionHost) {
       ? await generateRestLevelClient()
       : await generateTypeScriptLibrary(session.model, host);
     session.info(`Autorest.Typescript took ${Date.now() - start}ms`);
-  } catch (err) {
+  } catch (err: any) {
     session.error("An error was encountered while handling a request:", err);
     throw err;
   }
