ceafb91930b23a962dce3d13881ab2b5f74aa476
qiaozha <qiaozha@microsoft.com>
2022-03-23 09:44:56 +0800

    prettier update

--- test/rlcIntegration/userAgentRest.spec.ts
+++ test/rlcIntegration/userAgentRest.spec.ts
@@ -4,24 +4,24 @@ import BodyStringRest, {
 } from "./generated/bodyStringRest/src";
 
 describe("UserAgentRest", () => {
-    let client: BodyStringRestClient;
-  
-    beforeEach(() => {
-      client = BodyStringRest();
+  let client: BodyStringRestClient;
+
+  beforeEach(() => {
+    client = BodyStringRest();
+  });
+
+  describe("Acceptance tests", () => {
+    it("should send correct user agent prefix string for rlc", async () => {
+      const client: BodyStringRestClient = BodyStringRest();
+      const result = await client
+        .path("/string/nullBase64UrlEncoding")
+        .get({ allowInsecureConnection: true });
+      assert.equal(result.status, "200");
+      const userAgent = result.request.headers?.get("user-agent");
+      assert.isTrue(userAgent !== undefined);
+      if (userAgent) {
+        assert.isTrue(userAgent.indexOf("azsdk-js-body-string-rest") > -1);
+      }
     });
-  
-    describe("Acceptance tests", () => {
-        it("should send correct user agent prefix string for rlc", async () => {
-            const client: BodyStringRestClient = BodyStringRest();
-            const result = await client
-              .path("/string/nullBase64UrlEncoding")
-              .get({ allowInsecureConnection: true });
-            assert.equal(result.status, "200");
-            const userAgent = result.request.headers?.get("user-agent");
-            assert.isTrue(userAgent !== undefined);
-            if (userAgent) {
-              assert.isTrue(userAgent.indexOf("azsdk-js-body-string-rest") > -1);
-            }
-        });
-    });
+  });
 });
