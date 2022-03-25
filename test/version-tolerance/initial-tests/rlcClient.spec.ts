a160928cd6f272e0d144edcc40af55c85ba3fd43
qiaozha <qiaozha@microsoft.com>
2022-03-25 15:38:48 +0800

    add-more-dpg-test-coverage-for-rlc

--- test/version-tolerance/initial-tests/rlcClient.spec.ts
+++ test/version-tolerance/initial-tests/rlcClient.spec.ts
@@ -18,12 +18,41 @@ describe(`RLC Version Tolerance ${phase} phase`, async () => {
       assert.equal(result.status, "200");
     });
 
+    it("Header request with no param to optional", async () => {
+      const result = await client
+        .path("/serviceDriven/parameters")
+        .head();
+
+      assert.equal(result.status, "200");
+    });
+
+    it("Query parameters required to optional", async () => {
+      const result = await client.path("/serviceDriven/parameters").put({
+        queryParameters: {
+          requiredParam: "aaa"
+        }
+      });
+
+      assert.equal(result.status, "200");
+    });
+
+
     it("Body payload required to optional", async () => {
       const result = await client.path("/serviceDriven/parameters").post({
         body: { url: "http://example.org/myimage.jpeg" },
         contentType: "application/json"
       });
 
+      assert.equal(result.status, "200");
+    });
+
+    it("Query parameters with more optional param", async () => {
+      const result = await client.path("/serviceDriven/moreParameters").get();
+      assert.equal(result.status, "200");
+    });
+
+    it("Call glass breaker", async () => {
+      const result = await client.pathUnchecked("/servicedriven/glassbreaker").get();
       assert.equal(result.status, "200");
     });
   });
@@ -36,12 +65,33 @@ describe(`RLC Version Tolerance ${phase} phase`, async () => {
       assert.equal(result.status, "200");
     });
 
+    it("Header request with no param to optional", async () => {
+      const result = await client.params.headNoParams()
+
+      assert.equal(result.status, "200");
+    });
+
+    it("Query parameters required to optional", async () => {
+      const result = await client.params.putRequiredOptional({
+        queryParameters: {
+          requiredParam: "aaa"
+        }
+      });
+
+      assert.equal(result.status, "200");
+    });
+
     it("Body payload required to optional", async () => {
       const result = await client.params.postParameters({
         body: { url: "http://example.org/myimage.jpeg" },
         contentType: "application/json"
       });
 
+      assert.equal(result.status, "200");
+    });
+
+    it("Query parameters with more optional param", async () => {
+      const result = await client.params.getOptional();
       assert.equal(result.status, "200");
     });
   });
