65986bb16f3b326f5526a3365a453815d05b8234
Matthew Podwysocki <matthewp@microsoft.com>
2022-06-18 02:13:25 -0400

    Removing lookbehind RegEx in AutoRest (#1424)
    * Removing lookbehind RegEx in AutoRest
    
    * Updating smoke tests
    
    * Updating smoke tests

--- test/smoke/generated/arm-package-subscriptions-2019-06/src/subscriptionClient.ts
+++ test/smoke/generated/arm-package-subscriptions-2019-06/src/subscriptionClient.ts
@@ -84,7 +84,7 @@ export class SubscriptionClient extends coreClient.ServiceClient {
         if (param.length > 1) {
           const newParams = param[1].split("&").map((item) => {
             if (item.indexOf("api-version") > -1) {
-              return item.replace(/(?<==).*$/, apiVersion);
+              return "api-version=" + apiVersion;
             } else {
               return item;
             }
