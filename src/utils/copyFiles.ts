ff267ad930616521e0a369533adfc00da25b0a84
Sarangan Rajamanickam <sarajama@microsoft.com>
2022-06-21 12:41:30 -0700

    Replace Copy command with a script (#1427)

---
+++ src/utils/copyFiles.ts
@@ -0,0 +1,36 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import * as fsextra from "fs-extra";
+
+export function copyFiles(srcDir: string, destDir: string[]) {
+  const dirContents: string[] = fsextra.readdirSync(srcDir);
+  dirContents.forEach((dirEntry: string) => {
+    if (dirEntry.endsWith(".hbs")) {
+      destDir.forEach((destDirEntry: string) => {
+        console.log(
+          `Copying ${srcDir}/${dirEntry} to ${destDirEntry}/${dirEntry}`
+        );
+        fsextra.copyFileSync(
+          `${srcDir}/${dirEntry}`,
+          `${destDirEntry}/${dirEntry}`
+        );
+      });
+    }
+  });
+}
+
+copyFiles("./src/restLevelClient", ["./dist/src/restLevelClient", "./dist"]);
+copyFiles("./src/generators/test", ["./dist/src/generators/test", "./dist"]);
+copyFiles("./src/generators/static", [
+  "./dist/src/generators/static",
+  "./dist"
+]);
+copyFiles("./src/generators/samples", [
+  "./dist/src/generators/samples",
+  "./dist"
+]);
+copyFiles("./src/restLevelClient/samples", [
+  "./dist/src/restLevelClient/samples",
+  "./dist"
+]);
