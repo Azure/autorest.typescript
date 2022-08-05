a6f7646fedc321c193607e52d10cc350c48cf824
Mary Gao <yanmeigao1210@gmail.com>
2022-08-05 13:06:42 +0800

    Init models

--- packages/rlc-codegen/src/interfaces.ts
+++ packages/rlc-codegen/src/interfaces.ts
@@ -47,4 +47,47 @@ export interface RLCModel {
   srcPath: string;
   paths: Paths;
   options?: RLCOptions;
+  params?: OperationParameter[];
+  respons?: OperationResponse[];
+}
+
+export interface OperationParameter {
+  operationName: string;
+  parameters: ParameterMetadata[];
+}
+
+export interface ParameterMetadata {
+  type: "query" | "path" | "header" | "body";
+  name: string;
+  param: Schema;
+}
+
+export interface OperationResponse {
+  operationName: string;
+  responses: ResponseMetadata[];
+}
+
+export interface ResponseMetadata {
+  statusCode: string;
+  description?: string;
+  headers?: Record<string, Schema>;
+  body?: Schema;
+}
+export interface Schema {
+  name: string;
+  type: string;
+  description?: string;
+  required?: boolean;
+  default?: any;
+  readOnly?: boolean;
+  usage?: SchemaContext[];
+}
+
+export enum SchemaContext {
+  /** Schema is used as an input to an operation. */
+  Input = "input",
+  /** Schema is used as an output from an operation. */
+  Output = "output",
+  /** Schema is used as an exception from an operation. */
+  Exception = "exception"
 }
