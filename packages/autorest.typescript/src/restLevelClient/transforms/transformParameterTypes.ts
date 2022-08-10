278b1cdbb8c80fe41c902b0f6c3f585c2f24f3ce
Mary Gao <yanmeigao1210@gmail.com>
2022-08-10 21:00:03 +0800

    Update format

--- packages/autorest.typescript/src/restLevelClient/transforms/transformParameterTypes.ts
+++ packages/autorest.typescript/src/restLevelClient/transforms/transformParameterTypes.ts
@@ -1,6 +1,6 @@
 import {
-    CodeModel, Operation, Parameter, ParameterLocation, Property, SchemaContext,
-    Request as M4OperationRequest
+  CodeModel, Operation, Parameter, ParameterLocation, Property, SchemaContext,
+  Request as M4OperationRequest
 } from "@autorest/codemodel";
 import { ImportKind, OperationParameter, ParameterBodyMetadata, ParameterBodySchema, ParameterMetadata, ParameterMetadatas, Schema } from "@azure-tools/rlc-codegen";
 import { getLanguageMetadata } from "../../utils/languageHelpers";
@@ -10,199 +10,199 @@ import { getOperationParameters } from "../helpers/operationHelpers";
 import { getElementType, primitiveSchemaToType } from "../schemaHelpers";
 
 export function transformParameterTypes(model: CodeModel, importDetails: Map<ImportKind, Set<string>>) {
-    const rlcParameters: OperationParameter[] = [];
-    let importedModels = new Set<string>();
-    const operations = getAllOperations(model);
-    for (const operation of operations) {
-        const operationName = normalizeName(
-            getLanguageMetadata(operation.language).name,
-            NameType.Interface
-        );
-        const rlcParameter: OperationParameter = {
-            operationName,
-            parameters: []
-        };
-        const requestCount = operation?.requests?.length ?? 0;
-        for (let i = 0; i < requestCount; i++) {
-            const parameters = getOperationParameters(operation, i);
-            const request = operation.requests ? operation.requests[i] : undefined;
-            // transform query param
-            const queryParams = transformQueryParameters(
-                parameters,
-                importedModels);
-            // transform path param
-            const pathParams = transformPathParameters(
-                parameters,
-                model,
-                importedModels
-            )
-            // transform header param
-            const headerParams = transformHeaderParameters(
-                parameters,
-                importedModels);
-            // transform content type param
-            const contentTypeParam = transformContentTypeParameter(
-                request
-            );
-            // transform body
-            const bodyParameter = transformBodyParameters(
-                parameters,
-                importedModels
-            );
-            rlcParameter.parameters.push({
-                parameters: [...queryParams, ...pathParams, ...headerParams, ...contentTypeParam],
-                body: bodyParameter
-            });
-        }
-        rlcParameters.push(rlcParameter);
+  const rlcParameters: OperationParameter[] = [];
+  let importedModels = new Set<string>();
+  const operations = getAllOperations(model);
+  for (const operation of operations) {
+    const operationName = normalizeName(
+      getLanguageMetadata(operation.language).name,
+      NameType.Interface
+    );
+    const rlcParameter: OperationParameter = {
+      operationName,
+      parameters: []
+    };
+    const requestCount = operation?.requests?.length ?? 0;
+    for (let i = 0; i < requestCount; i++) {
+      const parameters = getOperationParameters(operation, i);
+      const request = operation.requests ? operation.requests[i] : undefined;
+      // transform query param
+      const queryParams = transformQueryParameters(
+        parameters,
+        importedModels);
+      // transform path param
+      const pathParams = transformPathParameters(
+        parameters,
+        model,
+        importedModels
+      )
+      // transform header param
+      const headerParams = transformHeaderParameters(
+        parameters,
+        importedModels);
+      // transform content type param
+      const contentTypeParam = transformContentTypeParameter(
+        request
+      );
+      // transform body
+      const bodyParameter = transformBodyParameters(
+        parameters,
+        importedModels
+      );
+      rlcParameter.parameters.push({
+        parameters: [...queryParams, ...pathParams, ...headerParams, ...contentTypeParam],
+        body: bodyParameter
+      });
     }
-    if (importedModels.size > 0) {
-        importDetails.set(ImportKind.ParameterInput, importedModels);
-    }
-    return rlcParameters;
+    rlcParameters.push(rlcParameter);
+  }
+  if (importedModels.size > 0) {
+    importDetails.set(ImportKind.ParameterInput, importedModels);
+  }
+  return rlcParameters;
 }
 
 function transformQueryParameters(
-    parameters: Parameter[],
-    importedModels: Set<string>,
+  parameters: Parameter[],
+  importedModels: Set<string>,
 ): ParameterMetadata[] {
-    const queryParameters = parameters.filter(
-        p => p.protocol.http?.in === "query"
-    );
-    return (queryParameters || []).map(qp =>
-        getParameterMetadata("query", qp, importedModels)
-    );
+  const queryParameters = parameters.filter(
+    p => p.protocol.http?.in === "query"
+  );
+  return (queryParameters || []).map(qp =>
+    getParameterMetadata("query", qp, importedModels)
+  );
 }
 
 function transformPathParameters(
-    parameters: Parameter[],
-    model: CodeModel,
-    importedModels: Set<string>,
+  parameters: Parameter[],
+  model: CodeModel,
+  importedModels: Set<string>,
 ): ParameterMetadata[] {
-    const pathParameters = parameters.filter(
-        p => p.protocol.http?.in === ParameterLocation.Uri && model.globalParameters?.indexOf(p) === -1
-    );
+  const pathParameters = parameters.filter(
+    p => p.protocol.http?.in === ParameterLocation.Uri && model.globalParameters?.indexOf(p) === -1
+  );
 
-    return (pathParameters || []).map(qp =>
-        getParameterMetadata("path", qp, importedModels, true)
-    );
+  return (pathParameters || []).map(qp =>
+    getParameterMetadata("path", qp, importedModels, true)
+  );
 }
 
 function transformHeaderParameters(
-    parameters: Parameter[],
-    importedModels: Set<string>,
+  parameters: Parameter[],
+  importedModels: Set<string>,
 ): ParameterMetadata[] {
-    const headerParameters = parameters.filter(
-        p => p.protocol.http?.in === "header"
-    );
-    return (headerParameters || []).map(qp =>
-        getParameterMetadata("header", qp, importedModels, true)
-    );
+  const headerParameters = parameters.filter(
+    p => p.protocol.http?.in === "header"
+  );
+  return (headerParameters || []).map(qp =>
+    getParameterMetadata("header", qp, importedModels, true)
+  );
 }
 
 function transformContentTypeParameter(
-    request: M4OperationRequest | undefined
+  request: M4OperationRequest | undefined
 ): [ParameterMetadata] | [] {
-    if (!request) {
-        return [];
-    }
-    const mediaTypes: string[] = request.protocol.http?.mediaTypes ?? [];
-    if (!mediaTypes.length) {
-        return [];
-    }
+  if (!request) {
+    return [];
+  }
+  const mediaTypes: string[] = request.protocol.http?.mediaTypes ?? [];
+  if (!mediaTypes.length) {
+    return [];
+  }
 
-    return [{
-        type: "header",
-        name: "contentType",
-        param: {
-            name: "contentType",
-            type: mediaTypes.map(mt => `"${mt}"`).join(" | "),
-            description: "Request content type",
-            required: false
-        }
-    }];
+  return [{
+    type: "header",
+    name: "contentType",
+    param: {
+      name: "contentType",
+      type: mediaTypes.map(mt => `"${mt}"`).join(" | "),
+      description: "Request content type",
+      required: false
+    }
+  }];
 }
 
 function transformBodyParameters(
-    parameters: Parameter[],
-    importedModels: Set<string>,
+  parameters: Parameter[],
+  importedModels: Set<string>,
 ): ParameterBodyMetadata | undefined {
-    const bodyParameters = parameters.filter(p => p.protocol.http?.in === "body");
-    if (!bodyParameters.length) {
-        return undefined;
-    }
-    const isPartialBody = bodyParameters.some(p => p.isPartialBody);
-    const rlcBodyParam: ParameterBodyMetadata = {
-        isPartialBody,
-    };
-    if (isPartialBody) {
-        rlcBodyParam.body = bodyParameters.map(bp =>
-            getParamterSchema(bp, importedModels)
-        );
-    } else {
-        rlcBodyParam.body = [
-            getParamterSchema(bodyParameters[0], importedModels)
-        ]
-    }
+  const bodyParameters = parameters.filter(p => p.protocol.http?.in === "body");
+  if (!bodyParameters.length) {
+    return undefined;
+  }
+  const isPartialBody = bodyParameters.some(p => p.isPartialBody);
+  const rlcBodyParam: ParameterBodyMetadata = {
+    isPartialBody,
+  };
+  if (isPartialBody) {
+    rlcBodyParam.body = bodyParameters.map(bp =>
+      getParamterSchema(bp, importedModels)
+    );
+  } else {
+    rlcBodyParam.body = [
+      getParamterSchema(bodyParameters[0], importedModels)
+    ]
+  }
 
-    return rlcBodyParam;
+  return rlcBodyParam;
 }
 
 function getParameterMetadata(
-    paramType: "query" | "path" | "header",
-    parameter: Property | Parameter,
-    importedModels = new Set<string>(),
-    isPrimitiveSchema = false) {
-    const schema: Schema = getParamterSchema(parameter, importedModels, isPrimitiveSchema);
-    return {
-        type: paramType,
-        name: schema.name,
-        param: schema
-    };;
+  paramType: "query" | "path" | "header",
+  parameter: Property | Parameter,
+  importedModels = new Set<string>(),
+  isPrimitiveSchema = false) {
+  const schema: Schema = getParamterSchema(parameter, importedModels, isPrimitiveSchema);
+  return {
+    type: paramType,
+    name: schema.name,
+    param: schema
+  };;
 }
 
 function getParamterSchema(
-    parameter: Property | Parameter,
-    importedModels = new Set<string>(),
-    isPrimitiveSchema = false
+  parameter: Property | Parameter,
+  importedModels = new Set<string>(),
+  isPrimitiveSchema = false
 ): Schema {
-    const propertyLangMetadata = getLanguageMetadata(parameter.language);
-    const propertyName = `"${propertyLangMetadata.serializedName ??
-        (parameter as Property).serializedName}"`;
+  const propertyLangMetadata = getLanguageMetadata(parameter.language);
+  const propertyName = `"${propertyLangMetadata.serializedName ??
+    (parameter as Property).serializedName}"`;
 
-    if (!propertyName) {
-        throw new Error(
-            `Couldn't find name for property ${JSON.stringify(propertyLangMetadata)}`
-        );
-    }
+  if (!propertyName) {
+    throw new Error(
+      `Couldn't find name for property ${JSON.stringify(propertyLangMetadata)}`
+    );
+  }
 
-    const description = getDocs(parameter);
-    let type;
-    if (isPrimitiveSchema) {
-        type = primitiveSchemaToType(parameter.schema, [
-            SchemaContext.Input,
-            SchemaContext.Exception
-        ]);
-    } else {
-        type = getElementType(parameter.schema, [SchemaContext.Input], importedModels);
-    }
+  const description = getDocs(parameter);
+  let type;
+  if (isPrimitiveSchema) {
+    type = primitiveSchemaToType(parameter.schema, [
+      SchemaContext.Input,
+      SchemaContext.Exception
+    ]);
+  } else {
+    type = getElementType(parameter.schema, [SchemaContext.Input], importedModels);
+  }
 
-    return {
-        name: propertyName,
-        type,
-        description,
-        required: parameter.required
-    };
+  return {
+    name: propertyName,
+    type,
+    description,
+    required: parameter.required
+  };
 }
 
 /**
  * Flattens all operations from operationGroups
  */
 function getAllOperations(model: CodeModel): Operation[] {
-    const operations: Operation[] = [];
-    for (const operationGroup of model.operationGroups) {
-        operations.push(...operationGroup.operations);
-    }
+  const operations: Operation[] = [];
+  for (const operationGroup of model.operationGroups) {
+    operations.push(...operationGroup.operations);
+  }
 
-    return operations;
+  return operations;
 }
--- packages/autorest.typescript/src/restLevelClient/transforms/transformResponseTypes.ts
+++ packages/autorest.typescript/src/restLevelClient/transforms/transformResponseTypes.ts
@@ -5,110 +5,110 @@ import { responseToSchemaResponse } from "../operationHelpers";
 import { getElementType, getFormatDocs, primitiveSchemaToType } from "../schemaHelpers";
 
 export function transformResponseTypes(model: CodeModel, importDetails: Map<ImportKind, Set<string>>): OperationResponse[] {
-    const operations = getAllOperationRequests(model);
-    const rlcResponses: OperationResponse[] = [];
-    let importedModels = new Set<string>();
-    for (const operation of operations) {
-        const responses = mergeResponsesAndExceptions(operation);
-        const operationLanguageMetadata = getLanguageMetadata(operation.language);
-        const rlcOperationUnit: OperationResponse = {
-            operationName: `${operationLanguageMetadata.name}`,
-            responses: []
-        };
-        for (const response of responses) {
-            // We'll need http information such as headers, statusCodes, etc continue if not available
-            if (!response.protocol.http) {
-                continue;
-            }
-            let schemaResponse: SchemaResponse = responseToSchemaResponse(response);
-            // transform header
-            const headers = transformHeaders(response);
-            // transform body
-            const body = transformBody(response, importedModels);
-            const rlcResponseUnit: ResponseMetadata = {
-                statusCode: getStatusCode(schemaResponse),
-                description: operationLanguageMetadata.description,
-                headers,
-                body
-            };
-            rlcOperationUnit.responses.push(rlcResponseUnit);
-        }
-        rlcResponses.push(rlcOperationUnit);
+  const operations = getAllOperationRequests(model);
+  const rlcResponses: OperationResponse[] = [];
+  let importedModels = new Set<string>();
+  for (const operation of operations) {
+    const responses = mergeResponsesAndExceptions(operation);
+    const operationLanguageMetadata = getLanguageMetadata(operation.language);
+    const rlcOperationUnit: OperationResponse = {
+      operationName: `${operationLanguageMetadata.name}`,
+      responses: []
+    };
+    for (const response of responses) {
+      // We'll need http information such as headers, statusCodes, etc continue if not available
+      if (!response.protocol.http) {
+        continue;
+      }
+      let schemaResponse: SchemaResponse = responseToSchemaResponse(response);
+      // transform header
+      const headers = transformHeaders(response);
+      // transform body
+      const body = transformBody(response, importedModels);
+      const rlcResponseUnit: ResponseMetadata = {
+        statusCode: getStatusCode(schemaResponse),
+        description: operationLanguageMetadata.description,
+        headers,
+        body
+      };
+      rlcOperationUnit.responses.push(rlcResponseUnit);
     }
-    if (importedModels.size > 0) {
-        importDetails.set(ImportKind.ResponseOutput, importedModels);
-    }
-    return rlcResponses;
+    rlcResponses.push(rlcOperationUnit);
+  }
+  if (importedModels.size > 0) {
+    importDetails.set(ImportKind.ResponseOutput, importedModels);
+  }
+  return rlcResponses;
 }
 
 
 function transformHeaders(response: Response): ResponseHeaderSchema[] | undefined {
-    // Check if there are any required headers
-    const hasDefinedHeaders =
-        Boolean(response.protocol.http?.headers) &&
-        Boolean(response.protocol.http?.headers.length);
-    if (!hasDefinedHeaders) {
-        return;
-    }
+  // Check if there are any required headers
+  const hasDefinedHeaders =
+    Boolean(response.protocol.http?.headers) &&
+    Boolean(response.protocol.http?.headers.length);
+  if (!hasDefinedHeaders) {
+    return;
+  }
 
-    return response.protocol.http?.headers.map((h: HttpHeader) => {
-        const header: ResponseHeaderSchema = {
-            name: `"${h.header.toLowerCase()}"`,
-            description: getLanguageMetadata(h.language).description,
-            type: primitiveSchemaToType(h.schema, [
-                SchemaContext.Output,
-                SchemaContext.Exception
-            ]),
-            required: false
-        };
-        return header;
-    });
+  return response.protocol.http?.headers.map((h: HttpHeader) => {
+    const header: ResponseHeaderSchema = {
+      name: `"${h.header.toLowerCase()}"`,
+      description: getLanguageMetadata(h.language).description,
+      type: primitiveSchemaToType(h.schema, [
+        SchemaContext.Output,
+        SchemaContext.Exception
+      ]),
+      required: false
+    };
+    return header;
+  });
 }
 
 function transformBody(response: Response, importedModels: Set<string>): ResponseBodySchema | undefined {
-    let schemaResponse: SchemaResponse = responseToSchemaResponse(response);
-    const bodyType = getElementType(
-        schemaResponse.schema,
-        [SchemaContext.Output, SchemaContext.Exception],
-        importedModels
-    );;
-    const bodyDescription = getFormatDocs(schemaResponse.schema);
-    if (!bodyType) {
-        return;
-    }
-    return {
-        name: "body",
-        type: bodyType,
-        description: bodyDescription
-    };
+  let schemaResponse: SchemaResponse = responseToSchemaResponse(response);
+  const bodyType = getElementType(
+    schemaResponse.schema,
+    [SchemaContext.Output, SchemaContext.Exception],
+    importedModels
+  );;
+  const bodyDescription = getFormatDocs(schemaResponse.schema);
+  if (!bodyType) {
+    return;
+  }
+  return {
+    name: "body",
+    type: bodyType,
+    description: bodyDescription
+  };
 }
 
 // Gets a list of all the available operations requests in the specification
 function getAllOperationRequests(model: CodeModel) {
-    let operations: Operation[] = [];
-    for (const og of model.operationGroups) {
-        for (const operation of og.operations) {
-            if (operation.requests?.length) {
-                operations.push(operation);
-            }
-        }
+  let operations: Operation[] = [];
+  for (const og of model.operationGroups) {
+    for (const operation of og.operations) {
+      if (operation.requests?.length) {
+        operations.push(operation);
+      }
     }
-    return operations;
+  }
+  return operations;
 }
 
 // Since REST clients don't throw on non-success status codes we treat responses and exceptions the same
 function mergeResponsesAndExceptions(operation: Operation) {
-    const responses: Response[] = [];
+  const responses: Response[] = [];
 
-    if (operation.responses) {
-        responses.push(...operation.responses);
-    }
+  if (operation.responses) {
+    responses.push(...operation.responses);
+  }
 
-    if (operation.exceptions) {
-        responses.push(...operation.exceptions);
-    }
+  if (operation.exceptions) {
+    responses.push(...operation.exceptions);
+  }
 
-    return responses;
+  return responses;
 }
 
 /**
@@ -116,15 +116,15 @@ function mergeResponsesAndExceptions(operation: Operation) {
  * @param response - response definition to extract the status code from
  */
 export function getStatusCode(response: Response): string {
-    if (response.protocol.http === undefined) {
-        throw new Error(
-            "generateResponseTypes.getStatusCode: Can't extract response definition statusCode from undefined http metadata"
-        );
-    }
+  if (response.protocol.http === undefined) {
+    throw new Error(
+      "generateResponseTypes.getStatusCode: Can't extract response definition statusCode from undefined http metadata"
+    );
+  }
 
-    // Extracting the status code for the response definition. Eventhough it is represented as an array of status codes
-    // in M4, in reality it can only contain a single status code, hence we are always picking the first one.
-    let statusCode = response.protocol.http?.statusCodes[0];
+  // Extracting the status code for the response definition. Eventhough it is represented as an array of status codes
+  // in M4, in reality it can only contain a single status code, hence we are always picking the first one.
+  let statusCode = response.protocol.http?.statusCodes[0];
 
-    return `"${statusCode}"`;
+  return `"${statusCode}"`;
 }
