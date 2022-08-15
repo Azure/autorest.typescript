82961e1d2d5dd7ba89678205e30da4f41782512f
Mary Gao <yanmeigao1210@gmail.com>
2022-08-15 20:00:05 +0800

    Update license header

--- packages/rlc-codegen/src/buildIsUnexpectedHelper.ts
+++ packages/rlc-codegen/src/buildIsUnexpectedHelper.ts
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
 import { Paths, RLCModel } from "./interfaces.js";
 import * as path from "path";
 import * as prettier from "prettier";
