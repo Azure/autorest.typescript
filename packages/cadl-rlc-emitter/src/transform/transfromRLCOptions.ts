50bc223c45c57ed05c2a565420eea8b316ba00cc
Qiaoqiao Zhang <55688292+qiaozha@users.noreply.github.com>
2022-08-24 17:14:01 +0800

    use auth in cadl (#1512)
    * use auth in cadl
    
    * resolve comments

--- packages/cadl-rlc-emitter/src/transform/transfromRLCOptions.ts
+++ packages/cadl-rlc-emitter/src/transform/transfromRLCOptions.ts
@@ -1,6 +1,6 @@
 import { RLCOptions } from "@azure-tools/rlc-codegen";
 import { getServiceNamespace, Program } from "@cadl-lang/compiler";
-import { getServers } from "@cadl-lang/rest/http";
+import { getAuthentication, getServers } from "@cadl-lang/rest/http";
 import { readFileSync, existsSync } from "fs";
 import { join } from "path";
 
@@ -24,5 +24,61 @@ export function transformRLCOptions(program: Program): RLCOptions {
       config.endpoint = host[0].url;
     }
   }
+  const securityInfo = processAuth(program);
+  config.addCredentials =
+    !securityInfo && config.addCredentials === false
+      ? false
+      : securityInfo
+      ? securityInfo.addCredentials
+      : config.addCredentials;
+  config.credentialScopes =
+    securityInfo && securityInfo.credentialScopes
+      ? securityInfo.credentialScopes
+      : config.credentialScopes;
+  config.credentialKeyHeaderName =
+    securityInfo && securityInfo.credentialKeyHeaderName
+      ? securityInfo.credentialKeyHeaderName
+      : config.credentialKeyHeaderName;
   return config;
+}
+
+function processAuth(program: Program) {
+  const serviceNs = getServiceNamespace(program);
+  if (!serviceNs) {
+    return undefined;
+  }
+  const authorization = getAuthentication(program, serviceNs);
+  if (!authorization || !authorization.options) {
+    return undefined;
+  }
+  const securityInfo: RLCOptions = {};
+  for (const option of authorization.options) {
+    for (const auth of option.schemes) {
+      switch (auth.type) {
+        case "http":
+          break;
+        case "apiKey":
+          if (auth.in === "cookie") {
+            return undefined;
+          }
+          securityInfo.addCredentials = true;
+          securityInfo.credentialKeyHeaderName = auth.name;
+          break;
+        case "oauth2":
+          const flow = auth.flows[0];
+          if (flow === undefined) {
+            return undefined;
+          }
+          securityInfo.addCredentials = true;
+          if (!securityInfo.credentialScopes) {
+            securityInfo.credentialScopes = [];
+          }
+          securityInfo.credentialScopes.push(...flow.scopes);
+          break;
+        default:
+          break;
+      }
+    }
+  }
+  return securityInfo;
 }
