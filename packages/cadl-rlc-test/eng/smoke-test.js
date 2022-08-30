197df9f3a78d91170e7de1d5dafd5732f0a66e99
qiaozha <qiaozha@microsoft.com>
2022-08-30 11:02:44 +0800

    enable smoke-test

--- packages/cadl-rlc-test/eng/smoke-test.js
+++ packages/cadl-rlc-test/eng/smoke-test.js
@@ -1,33 +1,45 @@
 
 
 import { readdir } from "fs/promises";
-import { join } from "path";
-import { spawnSync } from "child_process";
-import { resolveProject } from "./resolveRoot";
+import { join, dirname } from "path";
+import { execSync } from "child_process";
+import { fileURLToPath } from 'url';
 
 function generate(path) {
   const command = `cd ${path} && cadl compile ./spec`;
   console.log(command);
-  spawnSync(command);
+  const result = execSync(command);
+  if (result.stderr) {
+    console.log(Error(result.stderr));
+    process.exitCode = 1;
+  }
 }
 
 function build(path) {
   const command = `cd ${path}/cadl-output && npm install && npm run build`;
   console.log(command);
-  spawnSync(command);
+  const result = execSync(command);
+  if (result.stderr) {
+    console.log(Error(result.stderr));
+    process.exitCode = 1;
+  }
 }
 
 async function main() {
   const folder = process.argv[4];
-  const { path: root } = await resolveProject(__dirname);
+
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  const root = join(__dirname, "..");
 
   const folders = folder
     ? [folder]
     : (await readdir(join(root, "test")));
 
   for (const folder of folders) {
-    generate(folder);
-    build(folder);
+    const path = join(root, 'test', folder);
+    generate(path);
+    build(path);
   }
 }
 
