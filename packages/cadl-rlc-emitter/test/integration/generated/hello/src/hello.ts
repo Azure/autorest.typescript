2d6a7e08bb0bd8cf7bf11db45b31c0871619c121
Qiaoqiao Zhang <qiaozha@microsoft.com>
2022-09-02 16:38:11 +0800

    fix hello world name

--- packages/cadl-rlc-emitter/test/integration/generated/hello/src/hello.ts
+++ packages/cadl-rlc-emitter/test/integration/generated/hello/src/hello.ts
@@ -5,7 +5,7 @@ export default function createClient(options: ClientOptions = {}): HelloClient {
   const baseUrl = options.baseUrl ?? "http://localhost:3000";
   options.apiVersion = options.apiVersion ?? "1.0.0";
 
-  const userAgentInfo = `azsdk-js-hello-wrold-rest/1.0.0`;
+  const userAgentInfo = `azsdk-js-hello-world-rest/1.0.0`;
   const userAgentPrefix =
     options.userAgentOptions && options.userAgentOptions.userAgentPrefix
       ? `${options.userAgentOptions.userAgentPrefix} ${userAgentInfo}`
