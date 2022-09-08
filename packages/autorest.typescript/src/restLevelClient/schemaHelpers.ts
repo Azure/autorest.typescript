573c90406fe8e2f796fa719f7c51906c773fc90d
Mary Gao <yanmeigao1210@gmail.com>
2022-09-08 13:23:23 +0800

    Revert change

--- packages/autorest.typescript/src/restLevelClient/schemaHelpers.ts
+++ packages/autorest.typescript/src/restLevelClient/schemaHelpers.ts
@@ -116,6 +116,7 @@ export function primitiveSchemaToType(
         : "Date | string";
     case SchemaType.Char:
       return "string";
+    case SchemaType.ByteArray:
     case SchemaType.Duration:
     case SchemaType.Credential:
     case SchemaType.UnixTime:
@@ -123,7 +124,6 @@ export function primitiveSchemaToType(
     case SchemaType.Uuid:
     case SchemaType.String:
       return "string";
-    case SchemaType.ByteArray:
     case SchemaType.Binary:
       return schemaUse.includes(SchemaContext.Output)
         ? "Uint8Array"
