449b206d6db1d7d1a69f1919f22de666088d9f50
Mary Gao <yanmeigao1210@gmail.com>
2022-09-29 16:24:30 +0800

    Remove rlc readme gen ut in autorest

--- packages/autorest.typescript/test/unit/generators/static/readmeFileGenerator.spec.ts
+++ packages/autorest.typescript/test/unit/generators/static/readmeFileGenerator.spec.ts
@@ -79,64 +79,10 @@ describe("readmeFileGenerator", () => {
         /(\r\n|\n|\r)/gm,
         " "
       );
-      writeFileSync(path.join(__dirname, "files/case-hlcReadme.md"), getFirstFileContent(project));
-      assert.strictEqual(actualContents, expectedContends);
-    });
-
-    it("should generate rest-level client readme file correctly", async () => {
-      const autorestOption = {
-        srcPath: ".",
-        packageDetails: {
-          name: "@azure-rest/agrifood-farming",
-          nameWithoutScope: "agrifood-farming",
-          scopeName: "azure-rest",
-          version: "1.0.0-beta.1"
-        },
-        azureOutputDirectory: "sdk/agrifood/agrifood-farming-rest",
-        licenseHeader: false,
-        hideClients: true,
-        azureArm: false,
-        addCredentials: true,
-        generateMetadata: true,
-        isTestPackage: false,
-        ignoreNullableOnOptional: false,
-        useCoreV2: true,
-        allowInsecureConnection: true,
-        restLevelClient: true,
-        productDocLink: "https://azure.microsoft.com/en-us/services/purview/",
-        dependencyInfo: {
-          link:
-            "https://docs.microsoft.com/en-us/azure/purview/create-catalog-portal#add-a-security-principal-to-a-data-plane-role",
-          description: "enable AAD authentication on your Purview resource"
-        }
-      } as autorestSession.AutorestOptions;
-      const codeModel = new CodeModel("testCodeModel");
-      // set client details info
-      codeModel.language = {
-        default: {
-          name: "AgfoodClient",
-          description: ""
-        }
-      };
-      codeModel.info.title = "AgfoodClient";
-      codeModel.info.description = "Simple test description";
-      sinon.replace(
-        autorestSession,
-        "getAutorestOptions",
-        () => autorestOption
+      writeFileSync(
+        path.join(__dirname, "files/case-hlcReadme.md"),
+        getFirstFileContent(project)
       );
-      const project = getEmptyProject();
-      generateReadmeFile(codeModel, project);
-      const expectedContends = readFileSync(
-        path.join(__dirname, "files/case-rlcReadme.md"),
-        "utf-8"
-      ).replace(/(\r\n|\n|\r)/gm, " ");
-      const actualContents = getFirstFileContent(project).replace(
-        /(\r\n|\n|\r)/gm,
-        " "
-      );
-      // writeFileSync(path.join(__dirname, "files/case-rlcReadme2.md"), getFirstFileContent(project));
-      // Mitigate the newline issues
       assert.strictEqual(actualContents, expectedContends);
     });
   });
