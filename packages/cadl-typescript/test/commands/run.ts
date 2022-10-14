5d81661b0c1337dc24f3ebf80eb3111df2ec6e5c
qiaozha <qiaozha@microsoft.com>
2022-10-14 17:27:57 +0800

    use cadl-project in cadl-ranch

--- packages/cadl-typescript/test/commands/run.ts
+++ packages/cadl-typescript/test/commands/run.ts
@@ -1,8 +1,7 @@
-import { spawn } from "child_process";
+import { execSync } from "child_process";
 import { dirname, join as joinPath } from "path";
 import { fileURLToPath } from "url";
 import { CadlRanchConfig } from "./cadl-ranch-list.js";
-import { onExit } from "./childProcessOnExit.js";
 
 export async function runCadl(config: CadlRanchConfig) {
   const targetFolder = config.outputPath,
@@ -17,30 +16,26 @@ export async function runCadl(config: CadlRanchConfig) {
     "..",
     `./node_modules/@azure-tools/cadl-ranch-specs/http/${sourceCadl}`
   );
-  const emitterPath = joinPath(
-    `${__dirname}`,
-    "..",
-    "..",
-    "./dist/src/index.js"
-  );
+  // const emitterPath = joinPath(
+  //   `${__dirname}`,
+  //   "..",
+  //   "..",
+  //   "./dist/src/index.js"
+  // );
   const outputPath = joinPath(
     `${__dirname}`,
     "..",
     `./integration/generated/${targetFolder}`
   );
-  const cadlCommand = `cadl`;
+  const cadlCommand = `cd ${outputPath} && cadl`;
   const commandArguments: string[] = [
     "compile",
     `${cadlPath}`,
-    `--emit=${emitterPath}`,
-    `--output-path=${outputPath}`
+    `--output-path=.`
   ];
-  console.log(`${cadlCommand} ${commandArguments.join(" ")}`);
-  const childProcess = spawn(cadlCommand, commandArguments, {
-    stdio: [process.stdin, process.stdout, process.stderr],
-    shell: process.platform === "win32"
-  });
-  const result = await onExit(childProcess);
+  const command = `${cadlCommand} ${commandArguments.join(" ")}`;
+  console.log(command);
+  const result = execSync(command);
   console.log(`=== End ${targetFolder} ===`);
   return result;
 }
