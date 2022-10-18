b04a19c8b171819f4717b8ada840fa263e36f041
Jeremy Meng <yumeng@microsoft.com>
2022-10-18 23:57:56 +0000

    Update readme generation
    - to not mention NodeJS version
    - use `require` in logger code snippets

--- packages/rlc-common/src/metadata/buildReadmeFile.ts
+++ packages/rlc-common/src/metadata/buildReadmeFile.ts
@@ -35,7 +35,7 @@ Key links:
 
 ### Currently supported environments
 
-- Node.js version 14.x.x or higher
+- LTS versions of Node.js
 
 ### Prerequisites
 
@@ -72,7 +72,7 @@ AZURE_CLIENT_ID, AZURE_TENANT_ID, AZURE_CLIENT_SECRET
 Enabling logging may help uncover useful information about failures. In order to see a log of HTTP requests and responses, set the \`AZURE_LOG_LEVEL\` environment variable to \`info\`. Alternatively, logging can be enabled at runtime by calling \`setLogLevel\` in the \`@azure/logger\`:
 
 \`\`\`javascript
-import { setLogLevel } from "@azure/logger";
+const { setLogLevel } = require("@azure/logger");
 
 setLogLevel("info");
 \`\`\`
