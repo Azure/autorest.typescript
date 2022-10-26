c3794643854e2eb0838f58d2ba02ff55af927863
Mary Gao <yanmeigao1210@gmail.com>
2022-10-26 11:57:39 +0800

    Update generation for helper fix

--- packages/autorest.typescript/test/smoke/generated/synapse-artifacts-rest/src/isUnexpected.ts
+++ packages/autorest.typescript/test/smoke/generated/synapse-artifacts-rest/src/isUnexpected.ts
@@ -1162,12 +1162,12 @@ export function isUnexpected(
   const method = response.request.method;
   let pathDetails = responseMap[`${method} ${url.pathname}`];
   if (!pathDetails) {
-    pathDetails = geParametrizedPathSuccess(method, url.pathname);
+    pathDetails = getParametrizedPathSuccess(method, url.pathname);
   }
   return !pathDetails.includes(response.status);
 }
 
-function geParametrizedPathSuccess(method: string, path: string): string[] {
+function getParametrizedPathSuccess(method: string, path: string): string[] {
   const pathParts = path.split("/");
 
   // Iterate the responseMap to find a match
@@ -1181,49 +1181,42 @@ function geParametrizedPathSuccess(method: string, path: string): string[] {
     // Get each part of the url path
     const candidateParts = candidatePath.split("/");
 
-    // If the candidate and actual paths don't match in size
-    // we move on to the next candidate path
-    if (
-      candidateParts.length === pathParts.length &&
-      hasParametrizedPath(key)
+    // track if we have found a match to return the values found.
+    let found = true;
+    for (
+      let i = candidateParts.length - 1, j = pathParts.length - 1;
+      i >= 1 && j >= 1;
+      i--, j--
     ) {
-      // track if we have found a match to return the values found.
-      let found = true;
-      for (let i = 0; i < candidateParts.length; i++) {
-        if (
-          candidateParts[i]?.startsWith("{") &&
-          candidateParts[i]?.endsWith("}")
-        ) {
-          // If the current part of the candidate is a "template" part
-          // it is a match with the actual path part on hand
-          // skip as the parameterized part can match anything
-          continue;
-        }
-
-        // If the candidate part is not a template and
-        // the parts don't match mark the candidate as not found
-        // to move on with the next candidate path.
-        if (candidateParts[i] !== pathParts[i]) {
-          found = false;
-          break;
-        }
+      if (
+        candidateParts[i]?.startsWith("{") &&
+        candidateParts[i]?.endsWith("}")
+      ) {
+        // If the current part of the candidate is a "template" part
+        // it is a match with the actual path part on hand
+        // skip as the parameterized part can match anything
+        continue;
       }
 
-      // We finished evaluating the current candidate parts
-      // if all parts matched we return the success values form
-      // the path mapping.
-      if (found) {
-        return value;
+      // If the candidate part is not a template and
+      // the parts don't match mark the candidate as not found
+      // to move on with the next candidate path.
+      if (candidateParts[i] !== pathParts[j]) {
+        found = false;
+        break;
       }
+    }
+
+    // We finished evaluating the current candidate parts
+    // if all parts matched we return the success values form
+    // the path mapping.
+    if (found) {
+      return value;
     }
   }
 
   // No match was found, return an empty array.
   return [];
-}
-
-function hasParametrizedPath(path: string): boolean {
-  return path.includes("/{");
 }
 
 function getPathFromMapKey(mapKey: string): string {
