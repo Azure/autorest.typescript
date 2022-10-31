020bf56155ea8ebeea0803d2143004d866a27666
Mary Gao <yanmeigao1210@gmail.com>
2022-10-31 15:39:24 +0800

    Fix is unexpected helper (#1636)
    * Fix the isUnexpected helper function
    
    * Update the inUnexpected helper
    
    * Update generation for helper fix
    
    * Update the helper function
    
    * Update the isUnexpected helper
    
    * Fix ci failure
    
    * Update the helper function
    
    * Regenerate these files

--- packages/rlc-common/src/buildIsUnexpectedHelper.ts
+++ packages/rlc-common/src/buildIsUnexpectedHelper.ts
@@ -112,7 +112,7 @@ export function buildIsUnexpectedHelper(model: RLCModel) {
           } pathDetails = responseMap[\`\${method} \${url.pathname}\`];
           if (!pathDetails) {`,
         hasTemplate
-          ? "pathDetails = geParametrizedPathSuccess(method, url.pathname);"
+          ? "pathDetails = getParametrizedPathSuccess(method, url.pathname);"
           : `return true;`,
         `  }
           return !pathDetails.includes(response.status);
@@ -122,7 +122,7 @@ export function buildIsUnexpectedHelper(model: RLCModel) {
     if (hasTemplate) {
       isErrorHelper.addFunction({
         isExported: false,
-        name: "geParametrizedPathSuccess",
+        name: "getParametrizedPathSuccess",
         parameters: [
           {
             name: "method",
@@ -136,73 +136,73 @@ export function buildIsUnexpectedHelper(model: RLCModel) {
         returnType: `string[]`,
         statements: [
           `
-        const pathParts = path.split("/");
-
-        // Iterate the responseMap to find a match
-        for (const [key, value] of Object.entries(responseMap)) {
-          // Extracting the path from the map key which is in format
-          // GET /path/foo
-          if (!key.startsWith(method)) {
-            continue;
-          }
-          const candidatePath = getPathFromMapKey(key);
-          // Get each part of the url path
-          const candidateParts = candidatePath.split("/");
-      
-          // If the candidate and actual paths don't match in size
-          // we move on to the next candidate path
-          if (
-            candidateParts.length === pathParts.length &&
-            hasParametrizedPath(key)
-          ) {
+          const pathParts = path.split("/");
+            
+          // Traverse list to match the longest candidate
+          // matchedLen: the length of candidate path
+          // matchedValue: the matched status code array
+          let matchedLen = -1,
+            matchedValue: string[] = [];
+          
+          // Iterate the responseMap to find a match
+          for (const [key, value] of Object.entries(responseMap)) {
+            // Extracting the path from the map key which is in format
+            // GET /path/foo
+            if (!key.startsWith(method)) {
+              continue;
+            }
+            const candidatePath = getPathFromMapKey(key);
+            // Get each part of the url path
+            const candidateParts = candidatePath.split("/");
+        
             // track if we have found a match to return the values found.
             let found = true;
-            for (let i = 0; i < candidateParts.length; i++) {
+            for (
+              let i = candidateParts.length - 1, j = pathParts.length - 1;
+              i >= 1 && j >= 1;
+              i--, j--
+            ) {
               if (
                 candidateParts[i]?.startsWith("{") &&
-                candidateParts[i]?.endsWith("}")
+                candidateParts[i]?.indexOf("}") !== -1
               ) {
+                const start = candidateParts[i]!.indexOf("}") + 1,
+                  end = candidateParts[i]?.length;
                 // If the current part of the candidate is a "template" part
-                // it is a match with the actual path part on hand
-                // skip as the parameterized part can match anything
+                // Try to use the suffix of pattern to match the path
+                // {guid} ==> $
+                // {guid}:export ==> :export$
+                const isMatched = new RegExp(
+                  \`\${candidateParts[i]?.slice(start, end)}\`
+                ).test(pathParts[j] || '');
+        
+                if (!isMatched) {
+                  found = false;
+                  break;
+                }
                 continue;
               }
-      
+        
               // If the candidate part is not a template and
               // the parts don't match mark the candidate as not found
               // to move on with the next candidate path.
-              if (candidateParts[i] !== pathParts[i]) {
+              if (candidateParts[i] !== pathParts[j]) {
                 found = false;
                 break;
               }
             }
-      
+        
             // We finished evaluating the current candidate parts
-            // if all parts matched we return the success values form
-            // the path mapping.
-            if (found) {
-              return value;
+            // Update the matched value if and only if we found the longer pattern
+            if (found && candidatePath.length > matchedLen) {
+              matchedLen = candidatePath.length;
+              matchedValue = value;
             }
           }
-        }
-      
-        // No match was found, return an empty array.
-        return [];
+        
+          return matchedValue;
         `
         ]
-      });
-
-      isErrorHelper.addFunction({
-        isExported: false,
-        name: "hasParametrizedPath",
-        parameters: [
-          {
-            name: "path",
-            type: "string"
-          }
-        ],
-        returnType: `boolean`,
-        statements: [`return path.includes("/{");`]
       });
 
       isErrorHelper.addFunction({
