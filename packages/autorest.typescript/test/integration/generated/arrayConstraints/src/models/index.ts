26d513f678a9e2090b8649e5e8592b16161697de
Mary Gao <yanmeigao1210@gmail.com>
2022-11-02 13:29:49 +0800

    Fix the choice bugs

--- packages/autorest.typescript/test/integration/generated/arrayConstraints/src/models/index.ts
+++ packages/autorest.typescript/test/integration/generated/arrayConstraints/src/models/index.ts
@@ -13,7 +13,7 @@ export interface Product {
   string?: string;
   /** The OS of agent machine */
   custom?: OS;
-  /** Device Update Sku */
+  /** Product Sku */
   sku?: SKU;
 }
 
--- packages/autorest.typescript/test/integration/generated/bodyComplexWithTracing/src/bodyComplexWithTracing.ts
+++ packages/autorest.typescript/test/integration/generated/bodyComplexWithTracing/src/bodyComplexWithTracing.ts
@@ -65,7 +65,7 @@ export class BodyComplexWithTracing extends coreClient.ServiceClient {
       userAgentOptions: {
         userAgentPrefix
       },
-      baseUri: options.endpoint ?? options.baseUri ?? "http://localhost:3000"
+      endpoint: options.endpoint ?? options.baseUri ?? "http://localhost:3000"
     };
     super(optionsWithDefaults);
 
