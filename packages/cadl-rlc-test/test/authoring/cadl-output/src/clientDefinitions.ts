b0910917cbd494b92fb9dae7b4e964703f80978c
Mary Gao <yanmeigao1210@gmail.com>
2022-11-11 10:48:09 +0800

    Update dependency version

--- packages/cadl-rlc-test/test/authoring/cadl-output/src/clientDefinitions.ts
+++ packages/cadl-rlc-test/test/authoring/cadl-output/src/clientDefinitions.ts
@@ -54,6 +54,140 @@ import {
 } from "./responses";
 import { Client, StreamableMethod } from "@azure-rest/core-client";
 
+/** Contains operations for Projects operations */
+export interface ProjectsOperations {
+  /** Creates a new project or updates an existing one. */
+  createOrUpdate(
+    projectName: string,
+    options: ProjectsCreateOrUpdateParameters
+  ): StreamableMethod<
+    | ProjectsCreateOrUpdate200Response
+    | ProjectsCreateOrUpdate201Response
+    | ProjectsCreateOrUpdateDefaultResponse
+  >;
+  /** Gets the details of a project. */
+  get(
+    projectName: string,
+    options?: ProjectsGetParameters
+  ): StreamableMethod<ProjectsGet200Response | ProjectsGetDefaultResponse>;
+  /** Deletes a project. */
+  delete(
+    projectName: string,
+    options?: ProjectsDeleteParameters
+  ): StreamableMethod<
+    ProjectsDelete202Response | ProjectsDeleteDefaultResponse
+  >;
+  /** Lists the existing projects. */
+  list(
+    options?: ProjectsListParameters
+  ): StreamableMethod<ProjectsList200Response | ProjectsListDefaultResponse>;
+  /** Triggers a job to export a project's data. */
+  export(
+    projectName: string,
+    options: ProjectsExportParameters
+  ): StreamableMethod<
+    ProjectsExport202Response | ProjectsExportDefaultResponse
+  >;
+  /** Triggers a job to export a project's data. */
+  importx(
+    projectName: string,
+    options?: ProjectsImportxParameters
+  ): StreamableMethod<
+    ProjectsImportx202Response | ProjectsImportxDefaultResponse
+  >;
+  /** Triggers a training job for a project. */
+  train(
+    projectName: string,
+    options: ProjectsTrainParameters
+  ): StreamableMethod<ProjectsTrain202Response | ProjectsTrainDefaultResponse>;
+}
+
+/** Contains operations for Deployments operations */
+export interface DeploymentsOperations {
+  /** Gets the details of a deployment. */
+  getDeployment(
+    projectName: string,
+    deploymentName: string,
+    options?: DeploymentsGetDeploymentParameters
+  ): StreamableMethod<
+    | DeploymentsGetDeployment200Response
+    | DeploymentsGetDeploymentDefaultResponse
+  >;
+  /** Creates a new deployment or replaces an existing one. */
+  deployProject(
+    projectName: string,
+    deploymentName: string,
+    options?: DeploymentsDeployProjectParameters
+  ): StreamableMethod<
+    | DeploymentsDeployProject200Response
+    | DeploymentsDeployProject201Response
+    | DeploymentsDeployProjectDefaultResponse
+  >;
+  /** Deletes a project deployment. */
+  deleteDeployment(
+    projectName: string,
+    deploymentName: string,
+    options?: DeploymentsDeleteDeploymentParameters
+  ): StreamableMethod<
+    | DeploymentsDeleteDeployment202Response
+    | DeploymentsDeleteDeploymentDefaultResponse
+  >;
+  /** Lists the existing deployments. */
+  list(
+    projectName: string,
+    options?: DeploymentsListParameters
+  ): StreamableMethod<
+    DeploymentsList200Response | DeploymentsListDefaultResponse
+  >;
+  /** Swaps two existing deployments with each other. */
+  swapDeployments(
+    projectName: string,
+    options: DeploymentsSwapDeploymentsParameters
+  ): StreamableMethod<
+    | DeploymentsSwapDeployments202Response
+    | DeploymentsSwapDeploymentsDefaultResponse
+  >;
+}
+
+/** Contains operations for Jobs operations */
+export interface JobsOperations {
+  /** Gets the status of an existing deployment job. */
+  getDeploymentStatus(
+    projectName: string,
+    deploymentName: string,
+    jobId: string,
+    options?: JobsGetDeploymentStatusParameters
+  ): StreamableMethod<
+    JobsGetDeploymentStatus200Response | JobsGetDeploymentStatusDefaultResponse
+  >;
+  /** Gets the status of an existing swap deployment job. */
+  getSwapDeploymentsStatus(
+    projectName: string,
+    deploymentName: string,
+    jobId: string,
+    options?: JobsGetSwapDeploymentsStatusParameters
+  ): StreamableMethod<
+    | JobsGetSwapDeploymentsStatus200Response
+    | JobsGetSwapDeploymentsStatusDefaultResponse
+  >;
+}
+
+/** Contains operations for Global operations */
+export interface GlobalOperations {
+  getSupportedLanguages(
+    options?: GlobalGetSupportedLanguagesParameters
+  ): StreamableMethod<
+    | GlobalGetSupportedLanguages200Response
+    | GlobalGetSupportedLanguagesDefaultResponse
+  >;
+  listTrainingConfigVersions(
+    options?: GlobalListTrainingConfigVersionsParameters
+  ): StreamableMethod<
+    | GlobalListTrainingConfigVersions200Response
+    | GlobalListTrainingConfigVersionsDefaultResponse
+  >;
+}
+
 export interface ProjectsCreateOrUpdate {
   /** Creates a new project or updates an existing one. */
   patch(
@@ -254,4 +388,8 @@ export interface Routes {
 export type MicrosoftCognitiveLanguageServiceAnalyzeTextAuthoringClient =
   Client & {
     path: Routes;
+    projects: ProjectsOperations;
+    deployments: DeploymentsOperations;
+    jobs: JobsOperations;
+    global: GlobalOperations;
   };
