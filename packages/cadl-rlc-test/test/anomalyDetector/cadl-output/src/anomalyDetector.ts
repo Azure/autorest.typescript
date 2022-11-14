fa7ad4fae7d53668e8683b7796c26a88652b9e24
Qiaoqiao Zhang <qiaozha@microsoft.com>
2022-11-14 15:15:22 +0800

    qiaozha/add-anomaly-detector-cadl-smoke-test

---
+++ packages/cadl-rlc-test/test/anomalyDetector/cadl-output/src/anomalyDetector.ts
@@ -0,0 +1,46 @@
+import { getClient, ClientOptions } from "@azure-rest/core-client";
+import { KeyCredential } from "@azure/core-auth";
+import { AnomalyDetectorClient } from "./clientDefinitions";
+
+/**
+ * Initialize a new instance of the class AnomalyDetectorClient class.
+ * @param Endpoint type: string
+ * @param ApiVersion type: "v1.1"
+ * @param credentials type: KeyCredential
+ */
+export default function createClient(
+  Endpoint: string,
+  ApiVersion: "v1.1",
+  credentials: KeyCredential,
+  options: ClientOptions = {}
+): AnomalyDetectorClient {
+  const baseUrl =
+    options.baseUrl ?? `${Endpoint}/anomalydetector/${ApiVersion}`;
+
+  options = {
+    ...options,
+    credentials: {
+      apiKeyHeaderName: "Ocp-Apim-Subscription-Key",
+    },
+  };
+
+  const userAgentInfo = `azsdk-js-ai-anomaly-detector-rest/1.0.0-beta.1`;
+  const userAgentPrefix =
+    options.userAgentOptions && options.userAgentOptions.userAgentPrefix
+      ? `${options.userAgentOptions.userAgentPrefix} ${userAgentInfo}`
+      : `${userAgentInfo}`;
+  options = {
+    ...options,
+    userAgentOptions: {
+      userAgentPrefix,
+    },
+  };
+
+  const client = getClient(
+    baseUrl,
+    credentials,
+    options
+  ) as AnomalyDetectorClient;
+
+  return client;
+}
--- packages/cadl-typescript/src/modelUtils.ts
+++ packages/cadl-typescript/src/modelUtils.ts
@@ -388,6 +388,9 @@ function getSchemaForModel(
     NameType.Interface,
     true /** shouldGuard */
   );
+  if (modelSchema.name === "AnomalyValue") {
+    modelSchema;
+  }
 
   modelSchema.properties = {};
   const derivedModels = model.derivedModels.filter(includeDerivedModel);
