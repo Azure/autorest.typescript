4d74c203af7747daa88c9eb575eba537deb0b995
Mary Gao <yanmeigao1210@gmail.com>
2023-01-12 10:21:06 +0800

    Fix the test cases

--- packages/cadl-typescript/test/integration/fixedEnums.spec.ts
+++ packages/cadl-typescript/test/integration/fixedEnums.spec.ts
@@ -19,11 +19,11 @@ describe("FixedEnums Rest Client", () => {
     }
   });
 
-  // FIXME core-client can't JSON.stringfy this value
-  it.skip("should put known value", async () => {
+  it.only("should put known value", async () => {
     try {
       const result = await client.path("/enums/fixed/string/known-value").put({
-        body: "Monday"
+        body: "Monday",
+        contentType: "application/json"
       });
       assert.strictEqual(result.status, "204");
     } catch (err) {
@@ -31,13 +31,13 @@ describe("FixedEnums Rest Client", () => {
     }
   });
 
-  // FIXME core-client can't JSON.stringfy this value
-  it.skip("should put unknown value and receives 500", async () => {
+  it.only("should put unknown value and receives 500", async () => {
     try {
       const result = await client
         .path("/enums/fixed/string/unknown-value")
         .put({
-          body: "Weekend" as any
+          body: "Weekend" as any,
+          contentType: "application/json"
         });
       assert.strictEqual(result.status, "500");
     } catch (err) {
