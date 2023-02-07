f119a57f1e5072664b5de4c55bc169a3699ab574
Mary Gao <yanmeigao1210@gmail.com>
2023-02-07 15:41:51 +0800

    Update file

--- packages/autorest.typescript/test/rlcIntegration/generated/lroRest/src/lRORestClient.ts
+++ packages/autorest.typescript/test/rlcIntegration/generated/lroRest/src/lRORestClient.ts
@@ -6,7 +6,7 @@ import { LRORestClient } from "./clientDefinitions";
 
 /**
  * Initialize a new instance of `LRORestClient`
- * @param options type: `ClientOptions`, the parameter for all optional parameters
+ * @param options type: ClientOptions, the parameter for all optional parameters
  */
 export default function createClient(
   options: ClientOptions = {}
