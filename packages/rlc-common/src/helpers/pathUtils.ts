db444a35bd46b376a3693a10215f52442cb3e4f4
Qiaoqiao Zhang <qiaozha@microsoft.com>
2023-02-09 10:46:17 +0800

    fix issues in windows

--- packages/rlc-common/src/helpers/pathUtils.ts
+++ packages/rlc-common/src/helpers/pathUtils.ts
@@ -1,7 +1,9 @@
+import * as path from "path";
+
 // FIXME https://github.com/Azure/autorest.typescript/issues/1720
 export function getRelativePartFromSrcPath(srcPath: string) {
-  const relativePart = srcPath.substring(srcPath.indexOf("/src/") + 4);
-  return relativePart.startsWith("/")
+  const relativePart = srcPath.substring(srcPath.indexOf(path.sep + "src") + 4);
+  return relativePart.startsWith(path.sep)
     ? relativePart.substring(1)
     : relativePart;
 }
