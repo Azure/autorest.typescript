c89a924e445f913b6a8f214ef7131f3183b1ef2d
qiaozha <qiaozha@microsoft.com>
2023-02-09 19:38:44 +0800

    fix format

--- packages/rlc-common/src/helpers/pathUtils.ts
+++ packages/rlc-common/src/helpers/pathUtils.ts
@@ -1,7 +1,7 @@
 import * as path from "path";
 
 export function getRelativePartFromSrcPath(srcPath: string) {
-  const sep = srcPath.includes(path.sep + "src") ? path.sep: "/"
+  const sep = srcPath.includes(path.sep + "src") ? path.sep : "/";
   const relativePart = srcPath.substring(srcPath.indexOf(sep + "src") + 4);
   return relativePart.startsWith(sep)
     ? relativePart.substring(1)
