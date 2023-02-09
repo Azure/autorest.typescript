f4549d6b66bc6a4470cbdade385bca490cc82fa7
Qiaoqiao Zhang <55688292+qiaozha@users.noreply.github.com>
2023-02-09 11:56:00 +0800

    Update packages/rlc-common/src/helpers/pathUtils.ts
    Co-authored-by: Mary Gao <yanmeigao1210@gmail.com>

--- packages/rlc-common/src/helpers/pathUtils.ts
+++ packages/rlc-common/src/helpers/pathUtils.ts
@@ -1,6 +1,5 @@
 import * as path from "path";
 
-// FIXME https://github.com/Azure/autorest.typescript/issues/1720
 export function getRelativePartFromSrcPath(srcPath: string) {
   const relativePart = srcPath.substring(srcPath.indexOf(path.sep + "src") + 4);
   return relativePart.startsWith(path.sep)
