dff8bb9cb408f7430407f1d8d012d51e888df031
Mary Gao <yanmeigao1210@gmail.com>
2023-04-24 14:02:53 +0800

    Remove useless code

--- packages/typespec-ts/test/integration/generated/lro/lroBasic/src/outputModels.ts
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT license.
-
-/** Details about a user. */
-export interface UserOutput {
-  /** The name of user. */
-  name: string;
-}
--- packages/typespec-ts/test/integration/generated/lro/lroBasic/src/parameters.ts
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT license.
-
-import { RequestParameters } from "@azure-rest/core-client";
-
-export type CreateParameters = RequestParameters;
-export type PollingParameters = RequestParameters;
-export type GetParameters = RequestParameters;
