218a13f64adabf4e6d8826f01ffe4f747dfb722b
Mary Gao <yanmeigao1210@gmail.com>
2023-04-24 14:39:41 +0800

    Update the test cases for spread parameters

--- packages/typespec-ts/test/commands/cadl-ranch-list.ts
+++ packages/typespec-ts/test/commands/cadl-ranch-list.ts
@@ -89,6 +89,10 @@ export const cadls: CadlRanchConfig[] = [
     inputPath: "parameters/collection-format"
   },
   {
+    outputPath: "parameters/spread",
+    inputPath: "parameters/spread"
+  },
+  {
     outputPath: "projection",
     inputPath: "projection/projected-name"
   },
