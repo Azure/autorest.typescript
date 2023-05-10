d8fb74bc86f0a738d20f1030f46910bbbb665a87
Mary Gao <yanmeigao1210@gmail.com>
2023-05-10 18:43:49 +0800

    Update the new content type

--- packages/typespec-ts/test/commands/override-dev-version.ts
+++ packages/typespec-ts/test/commands/override-dev-version.ts
@@ -44,6 +44,7 @@ async function main() {
         }
       }
     }
+    console.log(`New content type: ${content.toString()}`);
     // eslint-disable-next-line no-console
     console.log(`Updated ${packageJsonPath}`);
     await writeFile(packageJsonPath, JSON.stringify(packageJson, null, 2));
