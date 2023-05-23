c78e3464667e973aeece58b14bed9e9d4f9682b7
Mary Gao <yanmeigao1210@gmail.com>
2023-05-23 14:19:13 +0800

    Upgrade the cadl-ranch version to v15 (#1863)
    * Upgrade the cadl-ranch version
    
    * Add test cases for encode
    
    * Typo fix
    
    * Change to workspace

---
+++ packages/typespec-ts/test/integration/generated/encode/src/models.ts
@@ -0,0 +1,18 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+export interface DefaultDurationProperty {
+  value: string;
+}
+
+export interface ISO8601DurationProperty {
+  value: string;
+}
+
+export interface Int32SecondsDurationProperty {
+  value: number;
+}
+
+export interface FloatSecondsDurationProperty {
+  value: number;
+}
