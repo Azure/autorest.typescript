3c03a9d39761a4e53d88bfbea5236557e2202b9f
Mary Gao <yanmeigao1210@gmail.com>
2023-05-29 17:58:19 +0800

    Revert int test change

--- packages/typespec-ts/test/modularIntegration/generated/lro/modular/src/index.ts
+++
@@ -1,7 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT license.
-
-export { User } from "./api/models.js";
-export { BeginCreateOrReplaceOptions as CreateOrReplaceOptions } from "./api/operations.js";
-export { StandardClient } from "./StandardClient.js";
-export { ClientOptions, RequestOptions } from "./common/interfaces.js";
