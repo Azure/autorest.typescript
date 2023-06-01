059ce66113920e05af5f219101aaeb9c139da9e2
Mary Gao <yanmeigao1210@gmail.com>
2023-06-01 18:27:39 +0800

    Update the logic

--- packages/typespec-ts/src/modelUtils.ts
+++ packages/typespec-ts/src/modelUtils.ts
@@ -232,11 +232,12 @@ function getSchemaForScalar(
   scalar: Scalar,
   relevantProperty?: ModelProperty
 ) {
-  let result = getSchemaForStdScalar(program, scalar, relevantProperty);
+  let result;
   const encodeData = getEncode(program, scalar);
-  if (!result && encodeData) {
-    result = getSchemaForScalar(program, dpgContext, encodeData.type);
-  } else if (!result && scalar.baseScalar) {
+  result = encodeData
+    ? getSchemaForScalar(program, dpgContext, encodeData.type)
+    : getSchemaForStdScalar(program, scalar, relevantProperty);
+  if (!result && scalar.baseScalar) {
     result = getSchemaForScalar(program, dpgContext, scalar.baseScalar);
   }
   return applyIntrinsicDecorators(program, scalar, result);
