e70d871bea7f42ed614cd6ccbcebe7654847084c
Mary Gao <yanmeigao1210@gmail.com>
2023-06-15 15:25:10 +0800

    Update the load test cases

---
+++ packages/typespec-test/test/loadTest/generated/typespec-ts/src/clientDefinitions.ts
@@ -0,0 +1,354 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import {
+  CreateOrUpdateTestParameters,
+  DeleteTestParameters,
+  GetTestParameters,
+  ListTestsParameters,
+  UploadTestFileParameters,
+  GetTestFileParameters,
+  DeleteTestFileParameters,
+  ListTestFilesParameters,
+  CreateOrUpdateAppComponentsParameters,
+  GetAppComponentsParameters,
+  CreateOrUpdateServerMetricsConfigParameters,
+  GetServerMetricsConfigParameters,
+  GetTestRunParameters,
+  CreateOrUpdateTestRunParameters,
+  DeleteTestRunParameters,
+  GetTestRunFileParameters,
+  ListTestRunsParameters,
+  StopTestRunParameters,
+  ListMetricNamespacesParameters,
+  ListMetricDefinitionsParameters,
+  ListMetricsParameters,
+  ListMetricDimensionValuesParameters,
+} from "./parameters";
+import {
+  CreateOrUpdateTest200Response,
+  CreateOrUpdateTest201Response,
+  CreateOrUpdateTestDefaultResponse,
+  DeleteTest204Response,
+  DeleteTestDefaultResponse,
+  GetTest200Response,
+  GetTestDefaultResponse,
+  ListTests200Response,
+  ListTestsDefaultResponse,
+  UploadTestFile201Response,
+  UploadTestFileDefaultResponse,
+  GetTestFile200Response,
+  GetTestFileDefaultResponse,
+  DeleteTestFile204Response,
+  DeleteTestFileDefaultResponse,
+  ListTestFiles200Response,
+  ListTestFilesDefaultResponse,
+  CreateOrUpdateAppComponents200Response,
+  CreateOrUpdateAppComponents201Response,
+  CreateOrUpdateAppComponentsDefaultResponse,
+  GetAppComponents200Response,
+  GetAppComponentsDefaultResponse,
+  CreateOrUpdateServerMetricsConfig200Response,
+  CreateOrUpdateServerMetricsConfig201Response,
+  CreateOrUpdateServerMetricsConfigDefaultResponse,
+  GetServerMetricsConfig200Response,
+  GetServerMetricsConfigDefaultResponse,
+  GetTestRun200Response,
+  GetTestRunDefaultResponse,
+  CreateOrUpdateTestRun200Response,
+  CreateOrUpdateTestRun201Response,
+  CreateOrUpdateTestRunDefaultResponse,
+  DeleteTestRun204Response,
+  DeleteTestRunDefaultResponse,
+  GetTestRunFile200Response,
+  GetTestRunFileDefaultResponse,
+  ListTestRuns200Response,
+  ListTestRunsDefaultResponse,
+  StopTestRun200Response,
+  StopTestRunDefaultResponse,
+  ListMetricNamespaces200Response,
+  ListMetricNamespacesDefaultResponse,
+  ListMetricDefinitions200Response,
+  ListMetricDefinitionsDefaultResponse,
+  ListMetrics200Response,
+  ListMetricsDefaultResponse,
+  ListMetricDimensionValues200Response,
+  ListMetricDimensionValuesDefaultResponse,
+} from "./responses";
+import { Client, StreamableMethod } from "@azure-rest/core-client";
+
+export interface CreateOrUpdateTest {
+  /** Create a new test or update an existing test. */
+  patch(
+    options: CreateOrUpdateTestParameters
+  ): StreamableMethod<
+    | CreateOrUpdateTest200Response
+    | CreateOrUpdateTest201Response
+    | CreateOrUpdateTestDefaultResponse
+  >;
+  /** Delete a test by its name. */
+  delete(
+    options?: DeleteTestParameters
+  ): StreamableMethod<DeleteTest204Response | DeleteTestDefaultResponse>;
+  /** Get load test details by test name */
+  get(
+    options?: GetTestParameters
+  ): StreamableMethod<GetTest200Response | GetTestDefaultResponse>;
+}
+
+export interface ListTests {
+  /**
+   * Get all load tests by the fully qualified resource Id e.g
+   * subscriptions/{subId}/resourceGroups/{rg}/providers/Microsoft.LoadTestService/loadtests/{resName}.
+   */
+  get(
+    options?: ListTestsParameters
+  ): StreamableMethod<ListTests200Response | ListTestsDefaultResponse>;
+}
+
+export interface UploadTestFile {
+  /**
+   * Upload input file for a given test name. File size can't be more than 50 MB.
+   * Existing file with same name for the given test will be overwritten. File
+   * should be provided in the request body as application/octet-stream.
+   */
+  put(
+    options: UploadTestFileParameters
+  ): StreamableMethod<
+    UploadTestFile201Response | UploadTestFileDefaultResponse
+  >;
+  /** Get test file by the file name. */
+  get(
+    options?: GetTestFileParameters
+  ): StreamableMethod<GetTestFile200Response | GetTestFileDefaultResponse>;
+  /** Delete file by the file name for a test */
+  delete(
+    options?: DeleteTestFileParameters
+  ): StreamableMethod<
+    DeleteTestFile204Response | DeleteTestFileDefaultResponse
+  >;
+}
+
+export interface ListTestFiles {
+  /** Get all test files. */
+  get(
+    options?: ListTestFilesParameters
+  ): StreamableMethod<ListTestFiles200Response | ListTestFilesDefaultResponse>;
+}
+
+export interface CreateOrUpdateAppComponents {
+  /** Associate an app component (collection of azure resources) to a test */
+  patch(
+    options: CreateOrUpdateAppComponentsParameters
+  ): StreamableMethod<
+    | CreateOrUpdateAppComponents200Response
+    | CreateOrUpdateAppComponents201Response
+    | CreateOrUpdateAppComponentsDefaultResponse
+  >;
+  /** Get associated app component (collection of azure resources) for the given test. */
+  get(
+    options?: GetAppComponentsParameters
+  ): StreamableMethod<
+    GetAppComponents200Response | GetAppComponentsDefaultResponse
+  >;
+}
+
+export interface CreateOrUpdateServerMetricsConfig {
+  /** Configure server metrics for a test */
+  patch(
+    options: CreateOrUpdateServerMetricsConfigParameters
+  ): StreamableMethod<
+    | CreateOrUpdateServerMetricsConfig200Response
+    | CreateOrUpdateServerMetricsConfig201Response
+    | CreateOrUpdateServerMetricsConfigDefaultResponse
+  >;
+  /** List server metrics configuration for the given test. */
+  get(
+    options?: GetServerMetricsConfigParameters
+  ): StreamableMethod<
+    GetServerMetricsConfig200Response | GetServerMetricsConfigDefaultResponse
+  >;
+}
+
+export interface GetTestRun {
+  /** Get test run details by name. */
+  get(
+    options?: GetTestRunParameters
+  ): StreamableMethod<GetTestRun200Response | GetTestRunDefaultResponse>;
+  /** Create and start a new test run with the given name. */
+  patch(
+    options: CreateOrUpdateTestRunParameters
+  ): StreamableMethod<
+    | CreateOrUpdateTestRun200Response
+    | CreateOrUpdateTestRun201Response
+    | CreateOrUpdateTestRunDefaultResponse
+  >;
+  /** Delete a test run by its name. */
+  delete(
+    options?: DeleteTestRunParameters
+  ): StreamableMethod<DeleteTestRun204Response | DeleteTestRunDefaultResponse>;
+}
+
+export interface GetTestRunFile {
+  /** Get test run file by file name. */
+  get(
+    options?: GetTestRunFileParameters
+  ): StreamableMethod<
+    GetTestRunFile200Response | GetTestRunFileDefaultResponse
+  >;
+}
+
+export interface ListTestRuns {
+  /** Get all test runs with given filters */
+  get(
+    options?: ListTestRunsParameters
+  ): StreamableMethod<ListTestRuns200Response | ListTestRunsDefaultResponse>;
+}
+
+export interface StopTestRun {
+  /** Stop test run by name. */
+  post(
+    options?: StopTestRunParameters
+  ): StreamableMethod<StopTestRun200Response | StopTestRunDefaultResponse>;
+}
+
+export interface ListMetricNamespaces {
+  /** List the metric namespaces for a load test run. */
+  get(
+    options?: ListMetricNamespacesParameters
+  ): StreamableMethod<
+    ListMetricNamespaces200Response | ListMetricNamespacesDefaultResponse
+  >;
+}
+
+export interface ListMetricDefinitions {
+  /** List the metric definitions for a load test run. */
+  get(
+    options?: ListMetricDefinitionsParameters
+  ): StreamableMethod<
+    ListMetricDefinitions200Response | ListMetricDefinitionsDefaultResponse
+  >;
+}
+
+export interface ListMetrics {
+  /** List the metric values for a load test run. */
+  post(
+    options: ListMetricsParameters
+  ): StreamableMethod<ListMetrics200Response | ListMetricsDefaultResponse>;
+}
+
+export interface ListMetricDimensionValues {
+  /** List the dimension values for the given metric dimension name. */
+  get(
+    options: ListMetricDimensionValuesParameters
+  ): StreamableMethod<
+    | ListMetricDimensionValues200Response
+    | ListMetricDimensionValuesDefaultResponse
+  >;
+}
+
+export interface CreateOrUpdateAppComponents {
+  /** Associate an app component (collection of azure resources) to a test run */
+  patch(
+    options: CreateOrUpdateAppComponentsParameters
+  ): StreamableMethod<
+    | CreateOrUpdateAppComponents200Response
+    | CreateOrUpdateAppComponents201Response
+    | CreateOrUpdateAppComponentsDefaultResponse
+  >;
+  /**
+   * Get associated app component (collection of azure resources) for the given test
+   * run.
+   */
+  get(
+    options?: GetAppComponentsParameters
+  ): StreamableMethod<
+    GetAppComponents200Response | GetAppComponentsDefaultResponse
+  >;
+}
+
+export interface CreateOrUpdateServerMetricsConfig {
+  /** Configure server metrics for a test run */
+  patch(
+    options: CreateOrUpdateServerMetricsConfigParameters
+  ): StreamableMethod<
+    | CreateOrUpdateServerMetricsConfig200Response
+    | CreateOrUpdateServerMetricsConfig201Response
+    | CreateOrUpdateServerMetricsConfigDefaultResponse
+  >;
+  /** List server metrics configuration for the given test run. */
+  get(
+    options?: GetServerMetricsConfigParameters
+  ): StreamableMethod<
+    GetServerMetricsConfig200Response | GetServerMetricsConfigDefaultResponse
+  >;
+}
+
+export interface Routes {
+  /** Resource for '/tests/\{testId\}' has methods for the following verbs: patch, delete, get */
+  (path: "/tests/{testId}", testId: string): CreateOrUpdateTest;
+  /** Resource for '/tests' has methods for the following verbs: get */
+  (path: "/tests"): ListTests;
+  /** Resource for '/tests/\{testId\}/files/\{fileName\}' has methods for the following verbs: put, get, delete */
+  (
+    path: "/tests/{testId}/files/{fileName}",
+    testId: string,
+    fileName: string
+  ): UploadTestFile;
+  /** Resource for '/tests/\{testId\}/files' has methods for the following verbs: get */
+  (path: "/tests/{testId}/files", testId: string): ListTestFiles;
+  /** Resource for '/tests/\{testId\}/app-components' has methods for the following verbs: patch, get */
+  (
+    path: "/tests/{testId}/app-components",
+    testId: string
+  ): CreateOrUpdateAppComponents;
+  /** Resource for '/tests/\{testId\}/server-metrics-config' has methods for the following verbs: patch, get */
+  (
+    path: "/tests/{testId}/server-metrics-config",
+    testId: string
+  ): CreateOrUpdateServerMetricsConfig;
+  /** Resource for '/test-runs/\{testRunId\}' has methods for the following verbs: get, patch, delete */
+  (path: "/test-runs/{testRunId}", testRunId: string): GetTestRun;
+  /** Resource for '/test-runs/\{testRunId\}/files/\{fileName\}' has methods for the following verbs: get */
+  (
+    path: "/test-runs/{testRunId}/files/{fileName}",
+    testRunId: string,
+    fileName: string
+  ): GetTestRunFile;
+  /** Resource for '/test-runs' has methods for the following verbs: get */
+  (path: "/test-runs"): ListTestRuns;
+  /** Resource for '/test-runs/\{testRunId\}:stop' has methods for the following verbs: post */
+  (path: "/test-runs/{testRunId}:stop", testRunId: string): StopTestRun;
+  /** Resource for '/test-runs/\{testRunId\}/metric-namespaces' has methods for the following verbs: get */
+  (
+    path: "/test-runs/{testRunId}/metric-namespaces",
+    testRunId: string
+  ): ListMetricNamespaces;
+  /** Resource for '/test-runs/\{testRunId\}/metric-definitions' has methods for the following verbs: get */
+  (
+    path: "/test-runs/{testRunId}/metric-definitions",
+    testRunId: string
+  ): ListMetricDefinitions;
+  /** Resource for '/test-runs/\{testRunId\}/metrics' has methods for the following verbs: post */
+  (path: "/test-runs/{testRunId}/metrics", testRunId: string): ListMetrics;
+  /** Resource for '/test-runs/\{testRunId\}/metric-dimensions/\{name\}/values' has methods for the following verbs: get */
+  (
+    path: "/test-runs/{testRunId}/metric-dimensions/{name}/values",
+    testRunId: string,
+    name: string
+  ): ListMetricDimensionValues;
+  /** Resource for '/test-runs/\{testRunId\}/app-components' has methods for the following verbs: patch, get */
+  (
+    path: "/test-runs/{testRunId}/app-components",
+    testRunId: string
+  ): CreateOrUpdateAppComponents;
+  /** Resource for '/test-runs/\{testRunId\}/server-metrics-config' has methods for the following verbs: patch, get */
+  (
+    path: "/test-runs/{testRunId}/server-metrics-config",
+    testRunId: string
+  ): CreateOrUpdateServerMetricsConfig;
+}
+
+export type AzureLoadTestingClient = Client & {
+  path: Routes;
+};
---
+++ packages/typespec-test/test/loadTest/generated/typespec-ts/src/isUnexpected.ts
@@ -0,0 +1,347 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import {
+  CreateOrUpdateTest200Response,
+  CreateOrUpdateTest201Response,
+  CreateOrUpdateTestDefaultResponse,
+  DeleteTest204Response,
+  DeleteTestDefaultResponse,
+  GetTest200Response,
+  GetTestDefaultResponse,
+  ListTests200Response,
+  ListTestsDefaultResponse,
+  UploadTestFile201Response,
+  UploadTestFileDefaultResponse,
+  GetTestFile200Response,
+  GetTestFileDefaultResponse,
+  DeleteTestFile204Response,
+  DeleteTestFileDefaultResponse,
+  ListTestFiles200Response,
+  ListTestFilesDefaultResponse,
+  CreateOrUpdateAppComponents200Response,
+  CreateOrUpdateAppComponents201Response,
+  CreateOrUpdateAppComponentsDefaultResponse,
+  GetAppComponents200Response,
+  GetAppComponentsDefaultResponse,
+  CreateOrUpdateServerMetricsConfig200Response,
+  CreateOrUpdateServerMetricsConfig201Response,
+  CreateOrUpdateServerMetricsConfigDefaultResponse,
+  GetServerMetricsConfig200Response,
+  GetServerMetricsConfigDefaultResponse,
+  GetTestRun200Response,
+  GetTestRunDefaultResponse,
+  CreateOrUpdateTestRun200Response,
+  CreateOrUpdateTestRun201Response,
+  CreateOrUpdateTestRunLogicalResponse,
+  CreateOrUpdateTestRunDefaultResponse,
+  DeleteTestRun204Response,
+  DeleteTestRunDefaultResponse,
+  GetTestRunFile200Response,
+  GetTestRunFileDefaultResponse,
+  ListTestRuns200Response,
+  ListTestRunsDefaultResponse,
+  StopTestRun200Response,
+  StopTestRunDefaultResponse,
+  ListMetricNamespaces200Response,
+  ListMetricNamespacesDefaultResponse,
+  ListMetricDefinitions200Response,
+  ListMetricDefinitionsDefaultResponse,
+  ListMetrics200Response,
+  ListMetricsDefaultResponse,
+  ListMetricDimensionValues200Response,
+  ListMetricDimensionValuesDefaultResponse,
+} from "./responses";
+
+const responseMap: Record<string, string[]> = {
+  "PATCH /tests/{testId}": ["200", "201"],
+  "DELETE /tests/{testId}": ["204"],
+  "GET /tests/{testId}": ["200"],
+  "GET /tests": ["200"],
+  "PUT /tests/{testId}/files/{fileName}": ["201"],
+  "GET /tests/{testId}/files/{fileName}": ["200"],
+  "DELETE /tests/{testId}/files/{fileName}": ["204"],
+  "GET /tests/{testId}/files": ["200"],
+  "PATCH /tests/{testId}/app-components": ["200", "201"],
+  "GET /tests/{testId}/app-components": ["200"],
+  "PATCH /tests/{testId}/server-metrics-config": ["200", "201"],
+  "GET /tests/{testId}/server-metrics-config": ["200"],
+  "GET /test-runs/{testRunId}": ["200"],
+  "PATCH /test-runs/{testRunId}": ["200", "201"],
+  "DELETE /test-runs/{testRunId}": ["204"],
+  "GET /test-runs/{testRunId}/files/{fileName}": ["200"],
+  "GET /test-runs": ["200"],
+  "POST /test-runs/{testRunId}:stop": ["200"],
+  "GET /test-runs/{testRunId}/metric-namespaces": ["200"],
+  "GET /test-runs/{testRunId}/metric-definitions": ["200"],
+  "POST /test-runs/{testRunId}/metrics": ["200"],
+  "GET /test-runs/{testRunId}/metric-dimensions/{name}/values": ["200"],
+  "PATCH /test-runs/{testRunId}/app-components": ["200", "201"],
+  "GET /test-runs/{testRunId}/app-components": ["200"],
+  "PATCH /test-runs/{testRunId}/server-metrics-config": ["200", "201"],
+  "GET /test-runs/{testRunId}/server-metrics-config": ["200"],
+};
+
+export function isUnexpected(
+  response:
+    | CreateOrUpdateTest200Response
+    | CreateOrUpdateTest201Response
+    | CreateOrUpdateTestDefaultResponse
+): response is CreateOrUpdateTestDefaultResponse;
+export function isUnexpected(
+  response: DeleteTest204Response | DeleteTestDefaultResponse
+): response is DeleteTestDefaultResponse;
+export function isUnexpected(
+  response: GetTest200Response | GetTestDefaultResponse
+): response is GetTestDefaultResponse;
+export function isUnexpected(
+  response: ListTests200Response | ListTestsDefaultResponse
+): response is ListTestsDefaultResponse;
+export function isUnexpected(
+  response: UploadTestFile201Response | UploadTestFileDefaultResponse
+): response is UploadTestFileDefaultResponse;
+export function isUnexpected(
+  response: GetTestFile200Response | GetTestFileDefaultResponse
+): response is GetTestFileDefaultResponse;
+export function isUnexpected(
+  response: DeleteTestFile204Response | DeleteTestFileDefaultResponse
+): response is DeleteTestFileDefaultResponse;
+export function isUnexpected(
+  response: ListTestFiles200Response | ListTestFilesDefaultResponse
+): response is ListTestFilesDefaultResponse;
+export function isUnexpected(
+  response:
+    | CreateOrUpdateAppComponents200Response
+    | CreateOrUpdateAppComponents201Response
+    | CreateOrUpdateAppComponentsDefaultResponse
+): response is CreateOrUpdateAppComponentsDefaultResponse;
+export function isUnexpected(
+  response: GetAppComponents200Response | GetAppComponentsDefaultResponse
+): response is GetAppComponentsDefaultResponse;
+export function isUnexpected(
+  response:
+    | CreateOrUpdateServerMetricsConfig200Response
+    | CreateOrUpdateServerMetricsConfig201Response
+    | CreateOrUpdateServerMetricsConfigDefaultResponse
+): response is CreateOrUpdateServerMetricsConfigDefaultResponse;
+export function isUnexpected(
+  response:
+    | GetServerMetricsConfig200Response
+    | GetServerMetricsConfigDefaultResponse
+): response is GetServerMetricsConfigDefaultResponse;
+export function isUnexpected(
+  response: GetTestRun200Response | GetTestRunDefaultResponse
+): response is GetTestRunDefaultResponse;
+export function isUnexpected(
+  response:
+    | CreateOrUpdateTestRun200Response
+    | CreateOrUpdateTestRun201Response
+    | CreateOrUpdateTestRunLogicalResponse
+    | CreateOrUpdateTestRunDefaultResponse
+): response is CreateOrUpdateTestRunDefaultResponse;
+export function isUnexpected(
+  response: DeleteTestRun204Response | DeleteTestRunDefaultResponse
+): response is DeleteTestRunDefaultResponse;
+export function isUnexpected(
+  response: GetTestRunFile200Response | GetTestRunFileDefaultResponse
+): response is GetTestRunFileDefaultResponse;
+export function isUnexpected(
+  response: ListTestRuns200Response | ListTestRunsDefaultResponse
+): response is ListTestRunsDefaultResponse;
+export function isUnexpected(
+  response: StopTestRun200Response | StopTestRunDefaultResponse
+): response is StopTestRunDefaultResponse;
+export function isUnexpected(
+  response:
+    | ListMetricNamespaces200Response
+    | ListMetricNamespacesDefaultResponse
+): response is ListMetricNamespacesDefaultResponse;
+export function isUnexpected(
+  response:
+    | ListMetricDefinitions200Response
+    | ListMetricDefinitionsDefaultResponse
+): response is ListMetricDefinitionsDefaultResponse;
+export function isUnexpected(
+  response: ListMetrics200Response | ListMetricsDefaultResponse
+): response is ListMetricsDefaultResponse;
+export function isUnexpected(
+  response:
+    | ListMetricDimensionValues200Response
+    | ListMetricDimensionValuesDefaultResponse
+): response is ListMetricDimensionValuesDefaultResponse;
+export function isUnexpected(
+  response:
+    | CreateOrUpdateAppComponents200Response
+    | CreateOrUpdateAppComponents201Response
+    | CreateOrUpdateAppComponentsDefaultResponse
+): response is CreateOrUpdateAppComponentsDefaultResponse;
+export function isUnexpected(
+  response: GetAppComponents200Response | GetAppComponentsDefaultResponse
+): response is GetAppComponentsDefaultResponse;
+export function isUnexpected(
+  response:
+    | CreateOrUpdateServerMetricsConfig200Response
+    | CreateOrUpdateServerMetricsConfig201Response
+    | CreateOrUpdateServerMetricsConfigDefaultResponse
+): response is CreateOrUpdateServerMetricsConfigDefaultResponse;
+export function isUnexpected(
+  response:
+    | GetServerMetricsConfig200Response
+    | GetServerMetricsConfigDefaultResponse
+): response is GetServerMetricsConfigDefaultResponse;
+export function isUnexpected(
+  response:
+    | CreateOrUpdateTest200Response
+    | CreateOrUpdateTest201Response
+    | CreateOrUpdateTestDefaultResponse
+    | DeleteTest204Response
+    | DeleteTestDefaultResponse
+    | GetTest200Response
+    | GetTestDefaultResponse
+    | ListTests200Response
+    | ListTestsDefaultResponse
+    | UploadTestFile201Response
+    | UploadTestFileDefaultResponse
+    | GetTestFile200Response
+    | GetTestFileDefaultResponse
+    | DeleteTestFile204Response
+    | DeleteTestFileDefaultResponse
+    | ListTestFiles200Response
+    | ListTestFilesDefaultResponse
+    | CreateOrUpdateAppComponents200Response
+    | CreateOrUpdateAppComponents201Response
+    | CreateOrUpdateAppComponentsDefaultResponse
+    | GetAppComponents200Response
+    | GetAppComponentsDefaultResponse
+    | CreateOrUpdateServerMetricsConfig200Response
+    | CreateOrUpdateServerMetricsConfig201Response
+    | CreateOrUpdateServerMetricsConfigDefaultResponse
+    | GetServerMetricsConfig200Response
+    | GetServerMetricsConfigDefaultResponse
+    | GetTestRun200Response
+    | GetTestRunDefaultResponse
+    | CreateOrUpdateTestRun200Response
+    | CreateOrUpdateTestRun201Response
+    | CreateOrUpdateTestRunLogicalResponse
+    | CreateOrUpdateTestRunDefaultResponse
+    | DeleteTestRun204Response
+    | DeleteTestRunDefaultResponse
+    | GetTestRunFile200Response
+    | GetTestRunFileDefaultResponse
+    | ListTestRuns200Response
+    | ListTestRunsDefaultResponse
+    | StopTestRun200Response
+    | StopTestRunDefaultResponse
+    | ListMetricNamespaces200Response
+    | ListMetricNamespacesDefaultResponse
+    | ListMetricDefinitions200Response
+    | ListMetricDefinitionsDefaultResponse
+    | ListMetrics200Response
+    | ListMetricsDefaultResponse
+    | ListMetricDimensionValues200Response
+    | ListMetricDimensionValuesDefaultResponse
+): response is
+  | CreateOrUpdateTestDefaultResponse
+  | DeleteTestDefaultResponse
+  | GetTestDefaultResponse
+  | ListTestsDefaultResponse
+  | UploadTestFileDefaultResponse
+  | GetTestFileDefaultResponse
+  | DeleteTestFileDefaultResponse
+  | ListTestFilesDefaultResponse
+  | CreateOrUpdateAppComponentsDefaultResponse
+  | GetAppComponentsDefaultResponse
+  | CreateOrUpdateServerMetricsConfigDefaultResponse
+  | GetServerMetricsConfigDefaultResponse
+  | GetTestRunDefaultResponse
+  | CreateOrUpdateTestRunDefaultResponse
+  | DeleteTestRunDefaultResponse
+  | GetTestRunFileDefaultResponse
+  | ListTestRunsDefaultResponse
+  | StopTestRunDefaultResponse
+  | ListMetricNamespacesDefaultResponse
+  | ListMetricDefinitionsDefaultResponse
+  | ListMetricsDefaultResponse
+  | ListMetricDimensionValuesDefaultResponse {
+  const lroOriginal = response.headers["x-ms-original-url"];
+  const url = new URL(lroOriginal ?? response.request.url);
+  const method = response.request.method;
+  let pathDetails = responseMap[`${method} ${url.pathname}`];
+  if (!pathDetails) {
+    pathDetails = getParametrizedPathSuccess(method, url.pathname);
+  }
+  return !pathDetails.includes(response.status);
+}
+
+function getParametrizedPathSuccess(method: string, path: string): string[] {
+  const pathParts = path.split("/");
+
+  // Traverse list to match the longest candidate
+  // matchedLen: the length of candidate path
+  // matchedValue: the matched status code array
+  let matchedLen = -1,
+    matchedValue: string[] = [];
+
+  // Iterate the responseMap to find a match
+  for (const [key, value] of Object.entries(responseMap)) {
+    // Extracting the path from the map key which is in format
+    // GET /path/foo
+    if (!key.startsWith(method)) {
+      continue;
+    }
+    const candidatePath = getPathFromMapKey(key);
+    // Get each part of the url path
+    const candidateParts = candidatePath.split("/");
+
+    // track if we have found a match to return the values found.
+    let found = true;
+    for (
+      let i = candidateParts.length - 1, j = pathParts.length - 1;
+      i >= 1 && j >= 1;
+      i--, j--
+    ) {
+      if (
+        candidateParts[i]?.startsWith("{") &&
+        candidateParts[i]?.indexOf("}") !== -1
+      ) {
+        const start = candidateParts[i]!.indexOf("}") + 1,
+          end = candidateParts[i]?.length;
+        // If the current part of the candidate is a "template" part
+        // Try to use the suffix of pattern to match the path
+        // {guid} ==> $
+        // {guid}:export ==> :export$
+        const isMatched = new RegExp(
+          `${candidateParts[i]?.slice(start, end)}`
+        ).test(pathParts[j] || "");
+
+        if (!isMatched) {
+          found = false;
+          break;
+        }
+        continue;
+      }
+
+      // If the candidate part is not a template and
+      // the parts don't match mark the candidate as not found
+      // to move on with the next candidate path.
+      if (candidateParts[i] !== pathParts[j]) {
+        found = false;
+        break;
+      }
+    }
+
+    // We finished evaluating the current candidate parts
+    // Update the matched value if and only if we found the longer pattern
+    if (found && candidatePath.length > matchedLen) {
+      matchedLen = candidatePath.length;
+      matchedValue = value;
+    }
+  }
+
+  return matchedValue;
+}
+
+function getPathFromMapKey(mapKey: string): string {
+  const pathStart = mapKey.indexOf("/");
+  return mapKey.slice(pathStart);
+}
