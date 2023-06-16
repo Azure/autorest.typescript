afcef8c6a0244ac292a0642496c3fe36769cd8e0
Qiaoqiao Zhang <55688292+qiaozha@users.noreply.github.com>
2023-06-17 07:44:28 +0800

    Update packages/typespec-ts/src/utils/clientUtils.ts
    Co-authored-by: Jeff Fisher <xirzec@xirzec.com>

--- packages/typespec-ts/src/utils/clientUtils.ts
+++ packages/typespec-ts/src/utils/clientUtils.ts
@@ -6,11 +6,21 @@ import {
 
 export function getRLCClients(dpgContext: SdkContext): SdkClient[] {
   const clientsInSdk = listClients(dpgContext);
-  const clients = listClients(dpgContext, false);
   // to respect the boundaries that is defined by one @client
   if (clientsInSdk.length === 1) {
     return clientsInSdk;
   }
-  // return the original @service clients
-  return clients;
+  
+  // Otherwise use service as the client
+  const services = listServices(context.program);
+
+  return services.map((service) => {
+    const clientName = service.type.name + "Client";
+    return {
+      kind: "SdkClient",
+      name: clientName,
+      service: service.type,
+      type: service.type,
+    };
+  });
 }
