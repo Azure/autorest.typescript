9cf8bae36751b6e97d6946949e9668443d35ef9f
qiaozha <qiaozha@microsoft.com>
2023-06-28 14:31:05 +0800

    resolve comments

--- packages/typespec-ts/src/utils/clientUtils.ts
+++ packages/typespec-ts/src/utils/clientUtils.ts
@@ -1,32 +1,18 @@
 import {
   SdkClient,
-  SdkContext,
-  listClients
+  SdkContext
 } from "@azure-tools/typespec-client-generator-core";
-import { Namespace } from "@typespec/compiler";
+import { listServices } from "@typespec/compiler";
 
 export function getRLCClients(dpgContext: SdkContext): SdkClient[] {
-  const clientsInSdk = listClients(dpgContext);
-  const sdkClientsNamespacesMap = new Map<Namespace, SdkClient[]>();
-  const validClients: SdkClient[] = [];
-  for (const client of clientsInSdk) {
-    const clients = sdkClientsNamespacesMap.get(client.service) ?? [];
-    clients.push(client);
-    sdkClientsNamespacesMap.set(client.service, clients);
-  }
-  sdkClientsNamespacesMap.forEach((value, service) => {
-    if (value.length === 1 && value[0]) {
-      // to respect the boundaries that is defined by one @client
-      validClients.push(value[0]);
-    } else {
-      // otherwise use service as the client
-      validClients.push({
-        kind: "SdkClient",
-        name: service.name + "Client",
-        service: service,
-        type: service
-      });
+  const services = listServices(dpgContext.program);
+
+  return services.map(service => {
+    return {
+      kind: "SdkClient",
+      name: service.type.name + "Client",
+      service: service.type,
+      type: service.type
     }
   });
-  return validClients;
 }
