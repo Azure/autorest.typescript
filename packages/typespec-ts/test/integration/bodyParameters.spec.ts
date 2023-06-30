4d7c3948afb61ad08b929545a9d13b22234684ae
Mary Gao <yanmeigao1210@gmail.com>
2023-06-30 16:56:34 +0800

    Enable body paramter test cases

--- packages/typespec-ts/test/integration/bodyParameters.spec.ts
+++ packages/typespec-ts/test/integration/bodyParameters.spec.ts
@@ -1,19 +1,19 @@
-import ParametersSpreadClientFactory, {
-  SpreadClient
-} from "./generated/parameters/spread/src/index.js";
+import BodyOptionalityClientFactory, {
+  BodyOptionalityClient
+} from "./generated/parameters/body-optionality/src/index.js";
 import { assert } from "chai";
-describe("HelloClient Rest Client", () => {
-  let client: SpreadClient;
+describe("BodyOptionalityClient Rest Client", () => {
+  let client: BodyOptionalityClient;
 
   beforeEach(() => {
-    client = ParametersSpreadClientFactory({ allowInsecureConnection: true });
+    client = BodyOptionalityClientFactory({ allowInsecureConnection: true });
   });
 
-  it("should spread named model", async () => {
+  it("should support optional-explicit body", async () => {
     try {
       const result = await client
-        .path("/parameters/spread/model/request-body")
-        .put({
+        .path("/parameters/body-optionality/optional-explicit/set")
+        .post({
           body: {
             name: "foo"
           }
@@ -24,54 +24,24 @@ describe("HelloClient Rest Client", () => {
     }
   });
 
-  it("should spread alias with only body param", async () => {
+  it("should support optional-explicit omitted body", async () => {
     try {
       const result = await client
-        .path("/parameters/spread/alias/request-body")
-        .put({
-          body: {
-            name: "foo"
-          }
-        });
+        .path("/parameters/body-optionality/optional-explicit/omit")
+        .post();
       assert.strictEqual(result.status, "204");
     } catch (err) {
       assert.fail(err as string);
     }
   });
 
-  it("should spread alias with mixed params", async () => {
+  it("should support required-implicit body", async () => {
     try {
       const result = await client
-        .path("/parameters/spread/alias/request-parameter/{id}", "1")
-        .put({
+        .path("/parameters/body-optionality/required-implicit")
+        .post({
           body: {
             name: "foo"
-          },
-          headers: {
-            "x-ms-test-header": "bar"
-          }
-        });
-      assert.strictEqual(result.status, "204");
-    } catch (err) {
-      assert.fail(err as string);
-    }
-  });
-
-  it("should spread alias with more than 5 params", async () => {
-    try {
-      const result = await client
-        .path("/parameters/spread/alias/multiple-parameters/{id}", "1")
-        .put({
-          body: {
-            prop1: "foo1",
-            prop2: "foo2",
-            prop3: "foo3",
-            prop4: "foo4",
-            prop5: "foo5",
-            prop6: "foo6"
-          },
-          headers: {
-            "x-ms-test-header": "bar"
           }
         });
       assert.strictEqual(result.status, "204");
