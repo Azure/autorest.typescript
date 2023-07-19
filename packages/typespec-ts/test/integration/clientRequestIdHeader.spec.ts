2337091166d2344c5d2569ee33dded2130af488e
Mary Gao <yanmeigao1210@gmail.com>
2023-07-19 13:31:03 +0800

    update the test cases

--- packages/typespec-ts/test/integration/clientRequestIdHeader.spec.ts
+++ packages/typespec-ts/test/integration/clientRequestIdHeader.spec.ts
@@ -25,4 +25,24 @@ describe.only("ClientRequestIdClient", () => {
       assert.fail(err as string);
     }
   });
+
+  it("should override client-request-id headers correctly", async () => {
+    try {
+      const overrideReqId = "86aede1f-96fa-4e7f-b1e1-bf8a947cb804";
+      const result = await client
+        .path("/special-headers/client-request-id")
+        .get({
+          headers: {
+            "client-request-id": overrideReqId
+          }
+        });
+      assert.strictEqual(
+        result.request.headers.get("client-request-id"),
+        overrideReqId
+      );
+      assert.strictEqual(result.status, "204");
+    } catch (err) {
+      assert.fail(err as string);
+    }
+  });
 });
