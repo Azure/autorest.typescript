4ec11f8334258af2b73a0e1c0bfc1d3591ce2abc
Mary Gao <yanmeigao1210@gmail.com>
2023-07-31 10:27:12 +0800

    Update packages/typespec-ts/test/unit/modelsGenerator.spec.ts

--- packages/typespec-ts/test/unit/modelsGenerator.spec.ts
+++ packages/typespec-ts/test/unit/modelsGenerator.spec.ts
@@ -1144,7 +1144,7 @@ describe("Input/output model type", () => {
       await verifyPropertyType(cadlType, inputType, { outputType });
     });
 
-    it("should handle record of record of empty object Record<Record<{}>> -> Record<string, Record<string, any>>", async () => {
+    it("should handle record of record of empty object Record<Record<{}>> -> Record<string, Record<string, Record<string, unknown>>>", async () => {
       const cadlType = "Record<Record<{}>>";
       const inputType = "Record<string, Record<string, Record<string, unknown>>>";
       const outputType = "Record<string, Record<string, Record<string, any>>>";
