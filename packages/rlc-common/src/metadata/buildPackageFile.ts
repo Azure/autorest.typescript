67bdd50bf915de6d3680fb1e713d8a9edad8c91f
qiaozha <qiaozha@microsoft.com>
2023-08-07 20:48:47 +0800

    fix ci

--- packages/rlc-common/src/metadata/buildPackageFile.ts
+++ packages/rlc-common/src/metadata/buildPackageFile.ts
@@ -166,8 +166,8 @@ function restLevelPackage(model: RLCModel, hasSamplesGenerated: boolean) {
         "@azure/abort-controller": "^1.0.0"
       }),
       ...(model.options.isModularLibrary && {
-          "@azure/core-util": "^1.4.0"
-        })
+        "@azure/core-util": "^1.4.0"
+      })
     },
     devDependencies: {
       "@microsoft/api-extractor": "^7.31.1",
