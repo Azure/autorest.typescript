890aab59790321182611c14ae68a8238a4b2ec33
Mary Gao <yanmeigao1210@gmail.com>
2023-08-09 13:12:48 +0800

    Add sources folder to trigger the smoke test

---
+++ packages/typespec-test/test/widget_dpg/generated/typespec-ts/sources/generated/src/index.ts
@@ -0,0 +1,18 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+export {
+  WidgetServiceClient,
+  WidgetServiceClientOptions,
+} from "./WidgetServiceClient.js";
+export {
+  Widget,
+  ColorType,
+  AnalyzeResult,
+  ListWidgetsOptions,
+  GetWidgetOptions,
+  CreateWidgetOptions,
+  UpdateWidgetOptions,
+  DeleteWidgetOptions,
+  AnalyzeWidgetOptions,
+} from "./models/index.js";
