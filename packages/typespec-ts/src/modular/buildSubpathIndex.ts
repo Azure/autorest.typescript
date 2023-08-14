0efc42aede03c31191d89096fad397440047cb28
qiaozha <qiaozha@microsoft.com>
2023-08-14 19:28:10 +0800

    fix format

--- packages/typespec-ts/src/modular/buildSubpathIndex.ts
+++ packages/typespec-ts/src/modular/buildSubpathIndex.ts
@@ -7,7 +7,7 @@ export function buildSubpathIndexFile(
   subpath: string,
   subfolder: string
 ) {
-  const apiFilePattern = join(srcPath, subfolder, subpath, "**")
+  const apiFilePattern = join(srcPath, subfolder, subpath, "**");
   const apiFiles = project.getSourceFiles(apiFilePattern);
   const indexFile = project.createSourceFile(
     `${srcPath}/${subfolder}/${subpath}/index.ts`
