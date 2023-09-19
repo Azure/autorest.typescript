f7a65f3a366f27e05fdb6dc3147ee19e5296c2d2
Mary Gao <yanmeigao1210@gmail.com>
2023-09-19 21:21:54 +0800

    Update the test cases

---
+++ packages/typespec-ts/test/modularUnit/type.spec.ts
@@ -0,0 +1,22 @@
+import { assert } from "chai";
+import { emitModularModelsFromTypeSpec } from "../util/emitUtil.js";
+import { assertEqualContent } from "../util/testUtil.js";
+
+describe("model type", () => {
+  it("string enum", async () => {
+    const modelFile = await emitModularModelsFromTypeSpec(`
+      model Test {
+        color: "red" | "blue";
+      }
+      op read(@body body: Test): void;
+      `);
+    assert.ok(modelFile);
+    assertEqualContent(
+      modelFile!.getFullText()!,
+      `
+      export interface Test {
+        color: "red" | "blue";
+      }`
+    );
+  });
+});
