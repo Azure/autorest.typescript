bc99bd8fb15251fb0759e1a69e8294153dd8e0f6
qiaozha <qiaozha@microsoft.com>
2023-09-21 17:18:24 +0800

    enable cadl ranch test for spread in modular

---
+++ packages/typespec-ts/test/modularIntegration/generated/parameters/spread/src/api/index.ts
@@ -0,0 +1,14 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+export {
+  aliasSpreadAsRequestBody,
+  aliasSpreadAsRequestParameter,
+  aliasSpreadWithMultipleParameters,
+} from "./alias.js";
+export { modelSpreadAsRequestBody } from "./model.js";
+export {
+  createSpread,
+  SpreadClientOptions,
+  SpreadContext,
+} from "./SpreadContext.js";
