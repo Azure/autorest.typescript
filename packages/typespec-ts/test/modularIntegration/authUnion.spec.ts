6dc30498a617665eff5cd484ac2552fb55d77822
Mary Gao <yanmeigao1210@gmail.com>
2023-10-09 15:04:03 +0800

    Update the spec for auth modular testing

---
+++ packages/typespec-ts/test/modularIntegration/authUnion.spec.ts
@@ -0,0 +1,74 @@
+import {
+  bearerTokenAuthenticationPolicyName,
+  PipelinePolicy
+} from "@azure/core-rest-pipeline";
+import {
+  createUnion,
+  UnionContext,
+  validKey,
+  validToken
+} from "./generated/authentication/union/src/api/index.js";
+import { assert } from "chai";
+import { customBearerTokenAuthenticationPolicy } from "../util/customBearerTokenTestingPolicy.js";
+
+describe("UnionContext in API Layer", () => {
+  let context: UnionContext;
+  let policy: PipelinePolicy;
+  const defaultScope = "https://security.microsoft.com/.default";
+
+  function prepareToken() {
+    context = createUnion(
+      {
+        getToken: async () => Promise.resolve(null)
+      },
+      {
+        allowInsecureConnection: true
+      }
+    );
+
+    policy = customBearerTokenAuthenticationPolicy({
+      scopes: defaultScope,
+      credential: {
+        getToken: async () => {
+          return {
+            token: defaultScope,
+            expiresOnTimestamp: Date.now()
+          };
+        }
+      }
+    });
+    context.pipeline.removePolicy({
+      name: bearerTokenAuthenticationPolicyName
+    });
+    context.pipeline.addPolicy(policy);
+  }
+
+  function prepareKey() {
+    context = createUnion(
+      {
+        key: "valid-key"
+      },
+      { allowInsecureConnection: true }
+    );
+  }
+
+  it("should not throw exception if apiKey is valid", async () => {
+    try {
+      prepareKey();
+      const result = await validKey(context);
+      assert.strictEqual(result, undefined);
+    } catch (err) {
+      assert.fail(err as string);
+    }
+  });
+
+  it("should throw exception if the token is valid", async () => {
+    try {
+      prepareToken();
+      const result = await validToken(context);
+      assert.strictEqual(result, undefined);
+    } catch (err: any) {
+      assert.fail(err as string);
+    }
+  });
+});
