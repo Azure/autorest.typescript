53ccb650666758d225328f5b55c22f7dd92ea7cf
Qiaoqiao Zhang <qiaozha@microsoft.com>
2023-10-24 11:20:03 +0800

    remove form data from smoke test as openai generic already covered

--- packages/typespec-test/test/formdata_modular/generated/typespec-ts/src/api/index.ts
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT license.
-
-export {
-  createDemoService,
-  DemoServiceClientOptions,
-  DemoServiceContext,
-} from "./DemoServiceContext.js";
-export { createFile } from "./operations.js";
