8c25ecd705ccb0de23c610c2767e30029879b0ca
Mary Gao <yanmeigao1210@gmail.com>
2023-10-25 14:41:48 +0800

    Update the UT for getModelInlineSigniture

--- packages/typespec-ts/test/unit/utils/modelUtils.spec.ts
+++ packages/typespec-ts/test/unit/utils/modelUtils.spec.ts
@@ -5,7 +5,7 @@ import { emitSchemasFromTypeSpec } from "../../util/emitUtil.js";
 
 describe("utils testing", () => {
   describe("#getModelInlineSigniture", () => {
-    it.skip("anonymous model with null and complext model", async () => {
+    it("anonymous model with null and complex model", async () => {
       const schemaOutput = await emitSchemasFromTypeSpec(`
             model Test {
                 prop: {
@@ -24,8 +24,8 @@ describe("utils testing", () => {
             op getModel(@body input: Test): Test;
           `);
       const first = schemaOutput?.[0] as ObjectSchema;
-      const res = `{"prop": {"index": number;"text": string;"logprobs": null | {"tokens": string[];"token_logprobs": number[];"top_logprobs": Record<string, number>[];"text_offset": number[];};};}`;
-      //   console.log(property, getModelInlineSigniture(first));
+      const res = `{"prop": {"index": number;"text": string;"logprobs": null | {"tokens": string[];"token_logprobs": number[];"top_logprobs": Record<string, number>[];"text_offset": number[];};}[];}`;
+      //   console.log(getModelInlineSigniture(first));
       assert.strictEqual(getModelInlineSigniture(first), res);
     });
   });
