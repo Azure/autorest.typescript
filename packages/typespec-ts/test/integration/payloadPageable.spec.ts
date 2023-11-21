e4fd71130d9f57568fa0f4b6ecbef82756fd5b16
zzhxiaofeng <v-zihz@microsoft.com>
2023-11-21 10:32:50 +0800

    update payloadPageable.spec.ts

--- packages/typespec-ts/test/integration/payloadPageable.spec.ts
+++ packages/typespec-ts/test/integration/payloadPageable.spec.ts
@@ -17,25 +17,31 @@ describe("Pageable Client", () => {
     });
   });
 
-  it("should get pagable list", async () => {
+  it("should get pagable list without skiptoken", async () => {
     try {
-      const initialResponse = await client
+      const result = await client
         .path("/payload/pageable")
         .get({ queryParameters: { maxpagesize: 3 } });
+      assert.strictEqual(result.status, "200");
+      assert.strictEqual(result.body.value[0]?.name, "user5");
+      assert.strictEqual(result.body.value[1]?.name, "user6");
+      assert.strictEqual(result.body.value[2]?.name, "user7");
+      assert.strictEqual(
+        result.body.nextLink,
+        "http://localhost:3000/payload/pageable?skipToken=name-user7&maxpagesize=3"
+      );
+    } catch (err) {
+      assert.fail(err as string);
+    }
+  });
 
-      const iter = paginate(client, initialResponse);
-
-      let result: UserOutput[] = [];
-      for await (const item of iter) {
-        result.push(item);
-      }
-
-      assert.deepEqual(result, [
-        { name: "user5" },
-        { name: "user6" },
-        { name: "user7" },
-        { name: "user8" }
-      ]);
+  it("should get pagable list with skiptoken", async () => {
+    try {
+      const result = await client
+        .path("/payload/pageable")
+        .get({ queryParameters: { maxpagesize: 3, skipToken: "name-user7" } });
+      assert.strictEqual(result.status, "200");
+      assert.strictEqual(result.body.value[0]?.name, "user8");
     } catch (err) {
       assert.fail(err as string);
     }
@@ -56,6 +62,30 @@ describe("Pageable Client", () => {
         `Not provided for first page, "name-user7" for second page`
       );
       assert.strictEqual((result.body as any).actual, "wrong value");
+    } catch (err) {
+      assert.fail(err as string);
+    }
+  });
+
+  it("should get pagable list", async () => {
+    try {
+      const initialResponse = await client
+        .path("/payload/pageable")
+        .get({ queryParameters: { maxpagesize: 3 } });
+
+      const iter = paginate(client, initialResponse);
+
+      let result: UserOutput[] = [];
+      for await (const item of iter) {
+        result.push(item);
+      }
+
+      assert.deepEqual(result, [
+        { name: "user5" },
+        { name: "user6" },
+        { name: "user7" },
+        { name: "user8" }
+      ]);
     } catch (err) {
       assert.fail(err as string);
     }
