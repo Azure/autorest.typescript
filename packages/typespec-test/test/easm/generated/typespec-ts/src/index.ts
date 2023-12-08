82c2263541d2a811082fc3c5ab4f06ac6b0a8f1e
qiaozha <qiaozha@microsoft.com>
2023-12-08 11:18:52 +0800

    using-effective-payload-type

---
+++ packages/typespec-test/test/easm/generated/typespec-ts/src/index.ts
@@ -0,0 +1,15 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import EasmService from "./easmService";
+
+export * from "./easmService";
+export * from "./parameters";
+export * from "./responses";
+export * from "./clientDefinitions";
+export * from "./isUnexpected";
+export * from "./models";
+export * from "./outputModels";
+export * from "./paginateHelper";
+
+export default EasmService;
