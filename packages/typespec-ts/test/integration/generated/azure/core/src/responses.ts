c26642aa5ca8da7613cf7025358bd02ab8d5662f
qiaozha <qiaozha@microsoft.com>
2024-01-17 19:05:07 +0800

    fix rlc integration test order

--- packages/typespec-ts/test/integration/generated/azure/core/src/responses.ts
+++ packages/typespec-ts/test/integration/generated/azure/core/src/responses.ts
@@ -4,46 +4,12 @@
 import { RawHttpHeaders } from "@azure/core-rest-pipeline";
 import { HttpResponse, ErrorResponse } from "@azure-rest/core-client";
 import {
-  PagedFirstItemOutput,
-  PagedSecondItemOutput,
   UserOutput,
   PagedUserOutput,
   UserListResultsOutput,
+  PagedFirstItemOutput,
+  PagedSecondItemOutput,
 } from "./outputModels";
-
-/** The request has succeeded. */
-export interface ListFirstItem200Response extends HttpResponse {
-  status: "200";
-  body: PagedFirstItemOutput;
-}
-
-export interface ListFirstItemDefaultHeaders {
-  /** String error code indicating what went wrong. */
-  "x-ms-error-code"?: string;
-}
-
-export interface ListFirstItemDefaultResponse extends HttpResponse {
-  status: string;
-  body: ErrorResponse;
-  headers: RawHttpHeaders & ListFirstItemDefaultHeaders;
-}
-
-/** The request has succeeded. */
-export interface ListSecondItem200Response extends HttpResponse {
-  status: "200";
-  body: PagedSecondItemOutput;
-}
-
-export interface ListSecondItemDefaultHeaders {
-  /** String error code indicating what went wrong. */
-  "x-ms-error-code"?: string;
-}
-
-export interface ListSecondItemDefaultResponse extends HttpResponse {
-  status: string;
-  body: ErrorResponse;
-  headers: RawHttpHeaders & ListSecondItemDefaultHeaders;
-}
 
 /** The request has succeeded. */
 export interface CreateOrUpdate200Response extends HttpResponse {
@@ -207,4 +173,38 @@ export interface ExportOperationDefaultResponse extends HttpResponse {
   status: string;
   body: ErrorResponse;
   headers: RawHttpHeaders & ExportOperationDefaultHeaders;
+}
+
+/** The request has succeeded. */
+export interface ListFirstItem200Response extends HttpResponse {
+  status: "200";
+  body: PagedFirstItemOutput;
+}
+
+export interface ListFirstItemDefaultHeaders {
+  /** String error code indicating what went wrong. */
+  "x-ms-error-code"?: string;
+}
+
+export interface ListFirstItemDefaultResponse extends HttpResponse {
+  status: string;
+  body: ErrorResponse;
+  headers: RawHttpHeaders & ListFirstItemDefaultHeaders;
+}
+
+/** The request has succeeded. */
+export interface ListSecondItem200Response extends HttpResponse {
+  status: "200";
+  body: PagedSecondItemOutput;
+}
+
+export interface ListSecondItemDefaultHeaders {
+  /** String error code indicating what went wrong. */
+  "x-ms-error-code"?: string;
+}
+
+export interface ListSecondItemDefaultResponse extends HttpResponse {
+  status: string;
+  body: ErrorResponse;
+  headers: RawHttpHeaders & ListSecondItemDefaultHeaders;
 }
--- packages/typespec-ts/test/integration/generated/projection/src/clientDefinitions.ts
+++ packages/typespec-ts/test/integration/generated/projection/src/clientDefinitions.ts
@@ -2,22 +2,30 @@
 // Licensed under the MIT license.
 
 import {
+  OperationParameters,
+  ParameterParameters,
   PropertyJsonParameters,
   PropertyClientParameters,
   PropertyLanguageParameters,
   PropertyJsonAndClientParameters,
-  OperationParameters,
-  ParameterParameters,
 } from "./parameters";
 import {
+  Operation204Response,
+  Parameter204Response,
   PropertyJson204Response,
   PropertyClient204Response,
   PropertyLanguage204Response,
   PropertyJsonAndClient204Response,
-  Operation204Response,
-  Parameter204Response,
 } from "./responses";
 import { Client, StreamableMethod } from "@azure-rest/core-client";
+
+export interface Operation {
+  post(options?: OperationParameters): StreamableMethod<Operation204Response>;
+}
+
+export interface Parameter {
+  post(options: ParameterParameters): StreamableMethod<Parameter204Response>;
+}
 
 export interface Json {
   post(
@@ -43,15 +51,11 @@ export interface JsonAndClient {
   ): StreamableMethod<PropertyJsonAndClient204Response>;
 }
 
-export interface Operation {
-  post(options?: OperationParameters): StreamableMethod<Operation204Response>;
-}
-
-export interface Parameter {
-  post(options: ParameterParameters): StreamableMethod<Parameter204Response>;
-}
-
 export interface Routes {
+  /** Resource for '/projection/projected-name/operation' has methods for the following verbs: post */
+  (path: "/projection/projected-name/operation"): Operation;
+  /** Resource for '/projection/projected-name/parameter' has methods for the following verbs: post */
+  (path: "/projection/projected-name/parameter"): Parameter;
   /** Resource for '/projection/projected-name/property/json' has methods for the following verbs: post */
   (path: "/projection/projected-name/property/json"): Json;
   /** Resource for '/projection/projected-name/property/client' has methods for the following verbs: post */
@@ -60,10 +64,6 @@ export interface Routes {
   (path: "/projection/projected-name/property/language"): Language;
   /** Resource for '/projection/projected-name/property/json-and-client' has methods for the following verbs: post */
   (path: "/projection/projected-name/property/json-and-client"): JsonAndClient;
-  /** Resource for '/projection/projected-name/operation' has methods for the following verbs: post */
-  (path: "/projection/projected-name/operation"): Operation;
-  /** Resource for '/projection/projected-name/parameter' has methods for the following verbs: post */
-  (path: "/projection/projected-name/parameter"): Parameter;
 }
 
 export type ProjectionClient = Client & {
