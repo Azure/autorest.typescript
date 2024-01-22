f4be6ec36b1ab4dcafb78687679bc4afb38a73f8
Di Jiao <v-jiaodi@microsoft.com>
2024-01-22 17:54:39 +0800

    add test case

---
+++ packages/typespec-ts/test/modularIntegration/fixedEnums.spec.ts
@@ -0,0 +1,48 @@
+import { assert } from "chai";
+import {
+  DaysOfWeekEnum,
+  FixedClient
+} from "./generated/enums/fixed/generated/src/index.js";
+
+describe("FixedEnums Rest Client", () => {
+  let client: FixedClient;
+
+  beforeEach(() => {
+    client = new FixedClient({
+      allowInsecureConnection: true,
+      retryOptions: {
+        maxRetries: 0
+      }
+    });
+  });
+
+  it("should get known value", async () => {
+    try {
+      const result = await client.getKnownValue();
+      console.log(result);
+      assert.strictEqual(result, "Monday");
+    } catch (err) {
+      assert.fail(err as string);
+    }
+  });
+
+  it("should put known value", async () => {
+    try {
+      const result = await client.putKnownValue(
+        JSON.stringify("Monday") as DaysOfWeekEnum
+      );
+      assert.isUndefined(result);
+    } catch (err) {
+      assert.fail(err as string);
+    }
+  });
+
+  it("should put unknown value", async () => {
+    try {
+      await client.putUnknownValue(JSON.stringify("Weekend") as DaysOfWeekEnum);
+      assert.fail("Expected an exception to be thrown.");
+    } catch (err) {
+      assert.isOk(err);
+    }
+  });
+});
