c75911ec62a3796a421e49eef66d15e661afeb6b
Mary Gao <yanmeigao1210@gmail.com>
2024-02-02 17:01:32 +0800

    Update the cadl-ranch-list and generate form-data cases

---
+++ packages/typespec-ts/test/integration/generated/payload/multipart/src/clientDefinitions.ts
@@ -0,0 +1,81 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import {
+  FormDataBasicParameters,
+  FormDataComplexParameters,
+  FormDataJsonPartParameters,
+  FormDataBinaryArrayPartsParameters,
+  FormDataJsonArrayPartsParameters,
+  FormDataMultiBinaryPartsParameters,
+} from "./parameters";
+import {
+  FormDataBasic204Response,
+  FormDataComplex204Response,
+  FormDataJsonPart204Response,
+  FormDataBinaryArrayParts204Response,
+  FormDataJsonArrayParts204Response,
+  FormDataMultiBinaryParts204Response,
+} from "./responses";
+import { Client, StreamableMethod } from "@azure-rest/core-client";
+
+export interface Basic {
+  /** Test content-type: multipart/form-data */
+  post(
+    options: FormDataBasicParameters,
+  ): StreamableMethod<FormDataBasic204Response>;
+}
+
+export interface Complex {
+  /** Test content-type: multipart/form-data for mixed scenarios */
+  post(
+    options: FormDataComplexParameters,
+  ): StreamableMethod<FormDataComplex204Response>;
+}
+
+export interface JsonPart {
+  /** Test content-type: multipart/form-data for scenario contains json part and binary part */
+  post(
+    options: FormDataJsonPartParameters,
+  ): StreamableMethod<FormDataJsonPart204Response>;
+}
+
+export interface BinaryArrayParts {
+  /** Test content-type: multipart/form-data for scenario contains multi binary parts */
+  post(
+    options: FormDataBinaryArrayPartsParameters,
+  ): StreamableMethod<FormDataBinaryArrayParts204Response>;
+}
+
+export interface JsonArrayParts {
+  /** Test content-type: multipart/form-data for scenario contains multi json parts */
+  post(
+    options: FormDataJsonArrayPartsParameters,
+  ): StreamableMethod<FormDataJsonArrayParts204Response>;
+}
+
+export interface MultiBinaryParts {
+  /** Test content-type: multipart/form-data for scenario contains multi binary parts */
+  post(
+    options: FormDataMultiBinaryPartsParameters,
+  ): StreamableMethod<FormDataMultiBinaryParts204Response>;
+}
+
+export interface Routes {
+  /** Resource for '/multipart/form-data/mixed-parts' has methods for the following verbs: post */
+  (path: "/multipart/form-data/mixed-parts"): Basic;
+  /** Resource for '/multipart/form-data/complex-parts' has methods for the following verbs: post */
+  (path: "/multipart/form-data/complex-parts"): Complex;
+  /** Resource for '/multipart/form-data/json-part' has methods for the following verbs: post */
+  (path: "/multipart/form-data/json-part"): JsonPart;
+  /** Resource for '/multipart/form-data/binary-array-parts' has methods for the following verbs: post */
+  (path: "/multipart/form-data/binary-array-parts"): BinaryArrayParts;
+  /** Resource for '/multipart/form-data/json-array-parts' has methods for the following verbs: post */
+  (path: "/multipart/form-data/json-array-parts"): JsonArrayParts;
+  /** Resource for '/multipart/form-data/multi-binary-parts' has methods for the following verbs: post */
+  (path: "/multipart/form-data/multi-binary-parts"): MultiBinaryParts;
+}
+
+export type MultiPartClient = Client & {
+  path: Routes;
+};
---
+++ packages/typespec-ts/test/integration/generated/payload/multipart/src/index.ts
@@ -0,0 +1,13 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import MultiPartClient from "./multiPartClient";
+
+export * from "./multiPartClient";
+export * from "./parameters";
+export * from "./responses";
+export * from "./clientDefinitions";
+export * from "./models";
+export { createFile, createFileFromStream } from "@azure/core-rest-pipeline";
+
+export default MultiPartClient;
