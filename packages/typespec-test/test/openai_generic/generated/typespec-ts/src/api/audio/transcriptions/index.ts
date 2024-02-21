f45a749b91779c5a7382f7c830849245f84bfbfc
qiaozha <qiaozha@microsoft.com>
2024-02-21 10:46:06 +0800

    fix ci

--- packages/typespec-test/test/openai_generic/generated/typespec-ts/src/api/audio/transcriptions/index.ts
+++ packages/typespec-test/test/openai_generic/generated/typespec-ts/src/api/audio/transcriptions/index.ts
@@ -16,6 +16,7 @@ import {
   operationOptionsToRequestParameters,
   createRestError,
 } from "@azure-rest/core-client";
+import { uint8ArrayToString } from "@azure/core-util";
 import { AudioTranscriptionsCreateOptions } from "../../../models/options.js";
 
 export function _createSend(
@@ -32,7 +33,7 @@ export function _createSend(
       ...operationOptionsToRequestParameters(options),
       contentType: (options.contentType as any) ?? "multipart/form-data",
       body: {
-        file: audio["file"],
+        file: uint8ArrayToString(audio["file"], "base64"),
         model: audio["model"],
         prompt: audio["prompt"],
         response_format: audio["responseFormat"],
