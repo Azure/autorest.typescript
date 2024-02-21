8c96e331a6874ebcbca8a6d175cf2982fb6a9937
Mary Gao <yanmeigao1210@gmail.com>
2024-02-21 15:54:52 +0800

    Comment the export for non-branding helper

--- packages/rlc-common/src/buildIndexFile.ts
+++ packages/rlc-common/src/buildIndexFile.ts
@@ -340,6 +340,12 @@ function generateRLCIndex(file: SourceFile, model: RLCModel) {
 }
 
 function reExportFileHelperFromCore(file: SourceFile, model: RLCModel) {
+  // TODO: remember to remove this when non-branded core has exported file helper
+  // https://github.com/Azure/autorest.typescript/issues/2299
+  const branded = model.options?.branded ?? true;
+  if (branded === false) {
+    return;
+  }
   if (needsFilePolyfil(model)) {
     file.addExportDeclarations([
       {
