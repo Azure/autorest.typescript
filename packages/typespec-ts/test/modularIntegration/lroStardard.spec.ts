5b0aa8e125a1a27532168fd066005bf46d2fafa8
Mary Gao <yanmeigao1210@gmail.com>
2024-02-25 16:15:47 +0800

    Temp verification

--- packages/typespec-ts/test/modularIntegration/lroStardard.spec.ts
+++ packages/typespec-ts/test/modularIntegration/lroStardard.spec.ts
@@ -2,7 +2,7 @@ import { StandardClient } from "./generated/lro/standard/generated/src/index.js"
 import { assert } from "chai";
 import { restorePoller } from "./generated/lro/standard/generated/src/restorePollerHelpers.js";
 
-describe("LROStandardClient Classical Client", () => {
+describe.only("LROStandardClient Classical Client", () => {
   let client: StandardClient;
 
   beforeEach(() => {
