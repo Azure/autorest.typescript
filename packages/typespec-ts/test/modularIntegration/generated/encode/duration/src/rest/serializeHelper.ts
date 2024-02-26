b982f9e53f619f99973617d331571f23d2a50605
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2024-02-26 17:27:31 +0800

    update

--- packages/typespec-ts/test/modularIntegration/generated/encode/duration/src/rest/serializeHelper.ts
+++ packages/typespec-ts/test/modularIntegration/generated/encode/duration/src/rest/serializeHelper.ts
@@ -1,6 +1,6 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-export function buildCsvCollection(items: string[] | number[]) {
+export function buildCsvCollection(items: string[] | number[]): string {
   return items.join(",");
 }
