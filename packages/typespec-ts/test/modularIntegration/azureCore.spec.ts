f023801c3ba48adc564600caaf3efd40bdca9d20
Qiaoqiao Zhang <55688292+qiaozha@users.noreply.github.com>
2024-03-06 14:43:55 +0800

    Update packages/typespec-ts/test/modularIntegration/azureCore.spec.ts

--- packages/typespec-ts/test/modularIntegration/azureCore.spec.ts
+++ packages/typespec-ts/test/modularIntegration/azureCore.spec.ts
@@ -171,7 +171,7 @@ describe("BasicClient Classical Client", () => {
     }
   });
 
-  it.only("should create or replace a user", async () => {
+  it("should create or replace a user", async () => {
     try {
       const user = await client.createOrReplace(
         1,
