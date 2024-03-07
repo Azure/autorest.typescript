e8c5a0358266e4a200efa62bc909e79988ae94fb
Mary Gao <yanmeigao1210@gmail.com>
2024-03-07 21:18:29 +0800

    [modular] Add the optional body case in cadl-ranch (#2238)
    * Add the optional body case
    
    * Generate the files of body optionality
    
    * Generate classical operations also
    
    * Fix the optional body issue
    
    * add modular test case for optional body
    
    * update test case
    
    * Revert changes in UT
    
    * Revert formats
    
    * Update .vscode/launch.json
    
    * Update .vscode/launch.json
    
    * Update modular code
    
    * Update the apiview
    
    * Update the codegen
    
    * regen code
    
    * Enable the UT
    
    ---------
    
    Co-authored-by: Jiao Di (MSFT) <80496810+v-jiaodi@users.noreply.github.com>

---
+++ packages/typespec-ts/test/modularIntegration/generated/parameters/body-optionality/src/index.ts
@@ -0,0 +1,15 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+export {
+  BodyOptionalityClient,
+  BodyOptionalityClientOptions,
+} from "./BodyOptionalityClient.js";
+export {
+  BodyModel,
+  RequiredExplicitOptions,
+  RequiredImplicitOptions,
+  OptionalExplicitSetOptions,
+  OptionalExplicitOmitOptions,
+} from "./models/index.js";
+export { OptionalExplicitOperations } from "./classic/index.js";
