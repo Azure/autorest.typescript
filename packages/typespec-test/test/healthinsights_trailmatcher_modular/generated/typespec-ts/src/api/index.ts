15f925001f3ab629ca8e02ce5da0380d1c659833
Mary Gao <yanmeigao1210@gmail.com>
2024-03-18 14:36:40 +0800

    Remove the health-insights-clinicalmatching.api

--- packages/typespec-test/test/healthinsights_trailmatcher_modular/generated/typespec-ts/src/api/index.ts
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT license.
-
-export {
-  createClinicalMatching,
-  ClinicalMatchingClientOptions,
-  AzureHealthInsightsContext,
-} from "./ClinicalMatchingContext.js";
-export { matchTrials } from "./operations.js";
