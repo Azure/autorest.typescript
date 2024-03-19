97371c3a124b7824f3cbb2d1f636ee0972803f6c
Jiao Di (MSFT) <80496810+v-jiaodi@users.noreply.github.com>
2024-03-19 14:33:16 +0800

    add modular test case and remove duplicate case

--- packages/typespec-ts/test/modularIntegration/azureClientGeneratorCoreAccess.spec.ts
+++ packages/typespec-ts/test/modularIntegration/azureClientGeneratorCoreAccess.spec.ts
@@ -15,8 +15,81 @@ describe("Azure ClientGeneratorCore Access Client", () => {
   it("should get no decorator in public operation", async () => {
     try {
       const result = await client.noDecoratorInPublic("myname");
-      console.log(result);
       assert.equal(result.name, "myname");
+    } catch (err) {
+      assert.fail(err as string);
+    }
+  });
+
+  it("should get public decorator in public operation", async () => {
+    try {
+      const result = await client.publicDecoratorInPublic("myname");
+      assert.equal(result.name, "myname");
+    } catch (err) {
+      assert.fail(err as string);
+    }
+  });
+
+  it("should get no decorator in internal operation", async () => {
+    try {
+      const result = await client.noDecoratorInInternal("myname");
+      assert.equal(result.name, "myname");
+    } catch (err) {
+      assert.fail(err as string);
+    }
+  });
+
+  it("should get internal decorator in internal operation", async () => {
+    try {
+      const result = await client.internalDecoratorInInternal("myname");
+      assert.equal(result.name, "myname");
+    } catch (err) {
+      assert.fail(err as string);
+    }
+  });
+
+  it("should get public decorator in internal operation", async () => {
+    try {
+      const result = await client.publicDecoratorInInternal("myname");
+      assert.equal(result.name, "myname");
+    } catch (err) {
+      assert.fail(err as string);
+    }
+  });
+
+  it("should get public shared model in operation", async () => {
+    try {
+      const result = await client.publicOperation("myname");
+      assert.equal(result.name, "myname");
+    } catch (err) {
+      assert.fail(err as string);
+    }
+  });
+
+  it("should get internal shared model in operation", async () => {
+    try {
+      const result = await client.internal("myname");
+      assert.equal(result.name, "myname");
+    } catch (err) {
+      assert.fail(err as string);
+    }
+  });
+
+  it("should get relative model in operation", async () => {
+    try {
+      const result = await client.operation("myname");
+      assert.equal(result.name, "Madge");
+      assert.equal(result.inner.name, "Madge");
+    } catch (err) {
+      assert.fail(err as string);
+    }
+  });
+
+  it("should get relative model in discriminator", async () => {
+    try {
+      const result = await client.discriminator("myname");
+      assert.equal(result.name, "Madge");
+      assert.equal(result.kind, "real");
     } catch (err) {
       assert.fail(err as string);
     }
