6b188b3ccafa3b7c8eeddf21dd17284498d5a957
Jiao Di (MSFT) <80496810+v-jiaodi@users.noreply.github.com>
2024-03-19 16:31:51 +0800

    regenerate rlc test

--- packages/typespec-ts/test/integration/generated/azure/clientGeneratorCore/access/src/clientDefinitions.ts
+++ packages/typespec-ts/test/integration/generated/azure/clientGeneratorCore/access/src/clientDefinitions.ts
@@ -18,7 +18,7 @@ import {
   NoDecoratorInInternal200Response,
   InternalDecoratorInInternal200Response,
   PublicDecoratorInInternal200Response,
-  Public200Response,
+  PublicOperation200Response,
   Internal200Response,
   Operation200Response,
   Discriminator200Response,
@@ -56,7 +56,7 @@ export interface PublicDecoratorInInternal {
 }
 
 export interface Public {
-  get(options: PublicParameters): StreamableMethod<Public200Response>;
+  get(options: PublicParameters): StreamableMethod<PublicOperation200Response>;
 }
 
 export interface Internal {
