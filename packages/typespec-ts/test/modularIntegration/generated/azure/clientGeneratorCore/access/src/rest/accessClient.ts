16f6ebafa89dd5fb4c2770352b759d720573f36a
qiaozha <qiaozha@microsoft.com>
2024-03-21 16:16:43 +0800

    fix ci

--- packages/typespec-ts/test/modularIntegration/generated/azure/clientGeneratorCore/access/src/rest/accessClient.ts
+++ packages/typespec-ts/test/modularIntegration/generated/azure/clientGeneratorCore/access/src/rest/accessClient.ts
@@ -12,7 +12,8 @@ import { AccessContext } from "./clientDefinitions.js";
 export default function createClient(
   options: ClientOptions = {},
 ): AccessContext {
-  const baseUrl = options.baseUrl ?? `http://localhost:3000`;
+  const endpointUrl =
+    options.endpoint ?? options.baseUrl ?? `http://localhost:3000`;
   const userAgentInfo = `azsdk-js-clientGeneratorCore-access-rest/1.0.0-beta.1`;
   const userAgentPrefix =
     options.userAgentOptions && options.userAgentOptions.userAgentPrefix
@@ -28,7 +29,7 @@ export default function createClient(
     },
   };
 
-  const client = getClient(baseUrl, options) as AccessContext;
+  const client = getClient(endpointUrl, options) as AccessContext;
 
   client.pipeline.removePolicy({ name: "ApiVersionPolicy" });
   return client;
