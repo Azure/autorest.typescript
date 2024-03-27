9cc9e96cd4aa29b7d6f1ca26fd683f80c666b740
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2024-03-27 16:24:06 +0800

    Update specialWords.spec.ts

--- packages/typespec-ts/test/modularIntegration/specialWords.spec.ts
+++ packages/typespec-ts/test/modularIntegration/specialWords.spec.ts
@@ -2,18 +2,37 @@ import { assert } from "chai";
 import { SpecialWordsClient } from "./generated/specialWords/src/SpecialWordsClient.js";
 describe.only("Type Union Client", () => {
   let client: SpecialWordsClient;
-  let body:{
-    sameAsModel: "string"
-  };
 
   beforeEach(() => {
-    client = new SpecialWordsClient({});
+    client = new SpecialWordsClient({ allowInsecureConnection: true });
   });
 
   it("should send sameAsModel", async () => {
     try {
-      const result = await client.modelProperties.sameAsModel(body);
-      console.log(result);
+      const result = await client.modelProperties.sameAsModel({
+        sameAsModel: "ok"
+      });
+      assert.equal(result , undefined);
+    } catch (err) {
+      assert.fail(err as string);
+    }
+  });
+
+  it("should send models withAnd", async () => {
+    try {
+      const result = await client.models.withAnd({
+        name: "ok"
+      });
+      assert.equal(result , undefined);
+    } catch (err) {
+      assert.fail(err as string);
+    }
+  });
+
+  it.only("should get parameters withConstructor", async () => {
+    try {
+      const result = await client.parameters.withConstructor("ok");
+      assert.equal(result , undefined);
     } catch (err) {
       assert.fail(err as string);
     }
