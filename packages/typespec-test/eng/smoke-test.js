0a54fe1c32efcd686f0dc0ff9782469bf2f44c51
Jose Manuel Heredia Hidalgo <joheredi@microsoft.com>
2024-04-12 13:34:21 -0700

    Use promises instead of workers

--- packages/typespec-test/eng/smoke-test.js
+++ packages/typespec-test/eng/smoke-test.js
@@ -1,11 +1,118 @@
-import { readdir } from "fs/promises";
+import { readdir, access, rm, mkdir, cp } from "fs/promises";
 import { join, dirname } from "path";
+import { spawn } from "child_process";
 import { fileURLToPath } from "url";
-import os from "os";
-import { Worker } from "worker_threads";
+import { createTaskLogger } from "./logger.js";
+
+async function exists(filePath) {
+  try {
+    await access(filePath);
+    return true;
+  } catch (err) {
+    return false;
+  }
+}
+
+function runCommand(command, args = [], workingDirectory, logger) {
+  return new Promise((resolve, reject) => {
+    const env = { ...process.env, FORCE_COLOR: "true" };
+
+    const child = spawn(command, [...args], {
+      cwd: workingDirectory ?? process.cwd(),
+      shell: true,
+      env,
+      stdio: ["inherit", "pipe", "pipe"], // Use 'inherit' to 'pipe' for stdout and stderr
+    });
+
+    child.stdout.on("data", (data) => {
+      logger.captureOutput(data);
+    });
+
+    child.stderr.on("data", (data) => {
+      logger.captureOutput(data);
+    });
+
+    child.on("error", (error) => {
+      logger.error(error.toString());
+      reject(new Error(error));
+    });
+
+    child.on("close", (code) => {
+      if (code === 0) {
+        resolve();
+      } else {
+        reject(new Error(`Command failed with code ${code}`));
+      }
+    });
+  });
+}
+
+async function npxCommand(command, args = [], workingDirectory, logger) {
+  return runCommand("npx", [command, ...args], workingDirectory, logger);
+}
+
+async function generate(path, logger) {
+  let workingDir = path;
+  let tspArgs = ["compile"];
+  try {
+    // Prepare the generate command
+    if (await exists(join(path, "tspconfig.yaml"))) {
+      tspArgs = [...tspArgs, "--config", "./tspconfig.yaml"];
+    }
+
+    if (await exists(join(path, "spec", "client.tsp"))) {
+      tspArgs.push("./spec/client.tsp");
+    } else {
+      tspArgs.push("./spec/main.tsp");
+    }
+
+    // Clean up the folder before generation
+    if (await exists(join(path, "generated", "typespec-ts"))) {
+      const pathToRemove = join(path, "generated", "typespec-ts");
+      await rm(pathToRemove, { recursive: true, force: true });
+    }
+  } catch (e) {
+    // do nothing
+    logger.log("Preparation error:", e);
+  }
+
+  try {
+    await npxCommand("tsp", tspArgs, workingDir, logger);
+    logger.log("Generated output:", result);
+  } catch (e) {
+    logger.error(Error(e.toString("utf8")));
+  }
+}
 
 const failed = [];
-async function generateSmokeTests() {
+
+async function build(path, logger) {
+  const workingDir = join(path, "generated/typespec-ts");
+  try {
+    await runCommand("npm", ["install"], workingDir, logger);
+    await runCommand("npm", ["run", "build"], workingDir, logger);
+  } catch (e) {
+    failed.push(path);
+    logger.error(Error(e.toString("utf8")));
+    process.exitCode = 1;
+  }
+}
+
+async function generateSmokeTest(path) {
+  const logger = createTaskLogger();
+  const contents = await readdir(path);
+  logger.log(`================Start ${path}===============`);
+  if (contents.includes("skip")) {
+    logger.log(`          ##### Skipped #####          `);
+  } else {
+    await generate(path, logger);
+    await build(path, logger);
+  }
+  logger.log(`================End ${path}===============`);
+  logger.flush();
+}
+
+async function main() {
   let folder;
   if (process.argv.includes("--")) {
     folder = process.argv[process.argv.length - 1];
@@ -13,64 +120,20 @@ async function generateSmokeTests() {
     folder = process.argv[4];
   }
 
-  const maxConcurrentWorkers = os.cpus().length;
-  let activeWorkers = 0;
-
-  console.log(`Max concurrent workers: ${maxConcurrentWorkers}`);
-
   const __filename = fileURLToPath(import.meta.url);
   const __dirname = dirname(__filename);
   const root = join(__dirname, "..");
 
   const folders = folder ? [folder] : await readdir(join(root, "test"));
+  const generatePromises = [];
 
   for (const folder of folders) {
-    // Wait for an available worker
-    while (activeWorkers >= maxConcurrentWorkers) {
-      await new Promise((resolve) => {
-        setTimeout(resolve, 1000);
-      });
-    }
-
-    const worker = new Worker("./eng/smoke-test-worker.js");
-    activeWorkers++;
-
-    // Send the worker the data it needs to do the work
-    worker.postMessage({ folder });
-
-    worker.on("message", ({ status, error, outputLog }) => {
-      if (status === "closed") {
-        activeWorkers--;
-      }
-
-      if (error) {
-        outputLog += `Error occurred during execution\n${JSON.stringify(
-          error
-        )} \n`;
-        failed.push(folder);
-      }
-
-      console.log(outputLog);
-    });
-
-    worker.on("error", (error) => {
-      console.error(`Error from worker: ${error}`);
-      activeWorkers--;
-    });
-
-    worker.on("exit", (code) => {
-      if (code !== 0) {
-        console.error(`Worker stopped with exit code ${code}`);
-      }
-      activeWorkers--;
-    });
+    const path = join(root, "test", folder);
+    const generatePromise = generateSmokeTest(path);
+    generatePromises.push(generatePromise);
   }
 
-  while (activeWorkers > 0) {
-    await new Promise((resolve) => {
-      setTimeout(resolve, 1000);
-    });
-  }
+  await Promise.all(generatePromises);
 
   if (failed.length > 0) {
     console.error("\x1b[31m%s\x1b[0m", `Failed folders: ${failed.join(", ")}`);
@@ -79,14 +142,6 @@ async function generateSmokeTests() {
   }
 }
 
-let exitCode = 0;
-try {
-  console.time("generateTypeSpecs");
-  await generateSmokeTests();
-} catch (e) {
-  console.error(`Error occurred during execution\n${e.message}`);
-  exitCode = 1;
-} finally {
-  console.timeEnd("generateTypeSpecs");
-  process.exit(exitCode);
-}
+console.time("generateSmokeTest");
+await main();
+console.timeEnd("generateSmokeTest");
