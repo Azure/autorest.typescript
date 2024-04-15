100cff0ff51b86a48863b255cc29eedb6392921d
Mary Gao <yanmeigao1210@gmail.com>
2024-04-15 22:18:12 +0800

    Update the scripts to install deps

--- packages/autorest.typescript/test/commands/prepare-deps.ts
+++ packages/autorest.typescript/test/commands/prepare-deps.ts
@@ -1,26 +1,83 @@
 import { spawn } from "child_process";
-import { onExit } from "./childProcessOnExit";
+import { existsSync } from "fs";
 import { join } from "path";
 
 async function main() {
-  // install extra dependencies for RLC tests
-  await installDependencies(
-    join(`${__dirname}`, "..", "..", "test", "rlcIntegration")
-  );
+  const isBrowserTest = process.argv.includes("--browser");
+  const isRemoval = process.argv.includes("--removal");
+  if (isBrowserTest) {
+    await copyPackageJson();
+    await installDependencies(join(`${__dirname}`, "..", "..", "test-browser"));
+  } else if (isRemoval) {
+    await removeDependencies([
+      join(`${__dirname}`, "..", "..", "test-browser", "package.json"),
+      join(`${__dirname}`, "..", "..", "test-browser", "node_modules")
+    ]);
+  } else {
+    await installDependencies(
+      join(`${__dirname}`, "..", "..", "test", "rlcIntegration")
+    );
+  }
 }
 
-const installDependencies = async (projectPath?: string) => {
-  if (!projectPath) {
+async function removeDependencies(files: string[]) {
+  const existing = files.filter((file) => existsSync(file));
+  if (existing.length === 0) {
+    console.log("No dependencies to remove");
     return;
   }
-  const npmCommand = `npm${/^win/.test(process.platform) ? ".cmd" : ""}`;
-  const npmInstall = spawn(npmCommand, ["install"], {
-    stdio: [process.stdin, process.stdout, process.stderr],
-    cwd: projectPath
+  runCommand("rm", ["-rf", ...existing]);
+  console.log("Removed dependencies for hlc browser tests", existing);
+}
+
+async function copyPackageJson() {
+  const srcPath = join(
+    `${__dirname}`,
+    "..",
+    "..",
+    "test",
+    "commands",
+    "browser.package.json"
+  );
+  const destPath = join(
+    `${__dirname}`,
+    "..",
+    "..",
+    "test-browser",
+    "package.json"
+  );
+  await runCommand("cp", [srcPath, destPath]);
+}
+
+async function installDependencies(path: string) {
+  await runCommand(
+    `npm${/^win/.test(process.platform) ? ".cmd" : ""}`,
+    ["install"],
+    path
+  );
+  console.log("Installed dependencies for rlc browser tests", path);
+}
+
+async function runCommand(command: string, args: string[] = [], cwd = ".") {
+  return new Promise((resolve, reject) => {
+    const process = spawn(command, args, { cwd, shell: true });
+
+    let stdout = "";
+    let stderr = "";
+
+    process.stdout.on("data", (data) => (stdout += data.toString()));
+    process.stderr.on("data", (data) => (stderr += data.toString()));
+
+    process.on("close", () => {
+      resolve(stdout ?? stderr);
+    });
+
+    process.on("error", (error: any) => {
+      console.log(stdout, stderr, error);
+      reject(new Error(error));
+    });
   });
-  await onExit(npmInstall);
-  console.log("Installed dependencies for path", projectPath);
-};
+}
 
 main().catch((error) => {
   console.error(error);
--- packages/autorest.typescript/webpack.config.test.js
+++ packages/autorest.typescript/webpack.config.test.js
@@ -1,16 +1,9 @@
-const { readdirSync, statSync, existsSync } = require("fs");
-const { join: joinPath, sep, extname } = require("path");
+const { readdirSync, statSync } = require("fs");
+const { join: joinPath, sep, extname, resolve } = require("path");
 const webpack = require("webpack");
-const { spawn } = require("child_process");
 
 function getIntegrationTestFiles(env) {
   const mode = env.mode ?? "hlc";
-  if (mode === "hlc") {
-    removeDependencies();
-  } else {
-    copyPackageJson();
-    installDependencies();
-  }
   const dirPath = joinPath(
     __dirname,
     "test-browser",
@@ -26,53 +19,6 @@ function getIntegrationTestFiles(env) {
 
   return [...files];
 }
-
-async function removeDependencies() {
-  const packageJson = joinPath(__dirname, "test-browser", "package.json");
-  if (!existsSync(packageJson)) {
-    return;
-  }
-  const nodeModules = joinPath(__dirname, "test-browser", "node_modules");
-  const rmCommand = `rm`;
-  const rm = spawn(rmCommand, ["-rf", packageJson, nodeModules], {
-    stdio: [process.stdin, process.stdout, process.stderr]
-  });
-  rm.on("error", (err) => {
-    console.error("Failed to remove dependencies", err);
-  });
-  console.log("Removed dependencies for hlc browser tests", nodeModules);
-}
-
-async function copyPackageJson() {
-  const srcPath = joinPath(
-    __dirname,
-    "test",
-    "commands",
-    "browser.package.json"
-  );
-  const destPath = joinPath(__dirname, "test-browser", "package.json");
-  const cpCommand = `cp`;
-  const cp = spawn(cpCommand, [srcPath, destPath], {
-    stdio: [process.stdin, process.stdout, process.stderr]
-  });
-  cp.on("error", (err) => {
-    console.error("Failed to install dependencies", err);
-  });
-}
-
-async function installDependencies() {
-  let path = joinPath(__dirname, "test-browser");
-  const npmCommand = `npm${/^win/.test(process.platform) ? ".cmd" : ""}`;
-  const npmInstall = spawn(npmCommand, ["install"], {
-    stdio: [process.stdin, process.stdout, process.stderr],
-    cwd: path
-  });
-  npmInstall.on("error", (err) => {
-    console.error("Failed to install dependencies", err);
-  });
-  console.log("Installed dependencies for rlc browser tests", path);
-}
-
 module.exports = (env) => ({
   target: "web",
   entry: getIntegrationTestFiles(env),
