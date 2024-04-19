874cdbc173e40627b7f326af738dd3a9c91fb315
Jiao Di (MSFT) <80496810+v-jiaodi@users.noreply.github.com>
2024-04-19 15:05:51 +0800

    regen integration test

--- packages/typespec-ts/test/modularIntegration/generated/lro/standard/generated/src/api/pollingHelpers.ts
+++ packages/typespec-ts/test/modularIntegration/generated/lro/standard/generated/src/api/pollingHelpers.ts
@@ -7,13 +7,13 @@ import {
   ResourceLocationConfig,
   LongRunningOperation,
   createHttpPoller,
-  OperationResponse
+  OperationResponse,
 } from "@azure/core-lro";
 
 import {
   Client,
   PathUncheckedResponse,
-  createRestError
+  createRestError,
 } from "@azure-rest/core-client";
 import { AbortSignalLike } from "@azure/abort-controller";
 import { isUnexpected } from "../rest/index.js";
@@ -45,16 +45,16 @@ export interface GetLongRunningPollerOptions<TResponse> {
 }
 export function getLongRunningPoller<
   TResponse extends PathUncheckedResponse,
-  TResult = void
+  TResult = void,
 >(
   client: Client,
   processResponseBody: (result: TResponse) => PromiseLike<TResult>,
-  options: GetLongRunningPollerOptions<TResponse>
+  options: GetLongRunningPollerOptions<TResponse>,
 ): PollerLike<OperationState<TResult>, TResult> {
   const { restoreFrom, getInitialResponse } = options;
   if (!restoreFrom && !getInitialResponse) {
     throw new Error(
-      "Either restoreFrom or getInitialResponse must be specified"
+      "Either restoreFrom or getInitialResponse must be specified",
     );
   }
   let initialResponse: TResponse | undefined = undefined;
@@ -63,7 +63,7 @@ export function getLongRunningPoller<
     sendInitialRequest: async () => {
       if (!getInitialResponse) {
         throw new Error(
-          "getInitialResponse is required when initializing a new poller"
+          "getInitialResponse is required when initializing a new poller",
         );
       }
       initialResponse = await getInitialResponse();
@@ -73,7 +73,7 @@ export function getLongRunningPoller<
       path: string,
       pollOptions?: {
         abortSignal?: AbortSignalLike;
-      }
+      },
     ) => {
       // The poll request would both listen to the user provided abort signal and the poller's own abort signal
       function abortListener(): void {
@@ -86,10 +86,10 @@ export function getLongRunningPoller<
         pollAbortController.abort();
       } else if (!abortSignal.aborted) {
         options.abortSignal?.addEventListener("abort", abortListener, {
-          once: true
+          once: true,
         });
         pollOptions?.abortSignal?.addEventListener("abort", abortListener, {
-          once: true
+          once: true,
         });
       }
       let response;
@@ -105,7 +105,7 @@ export function getLongRunningPoller<
       }
 
       return getLroResponse(response as TResponse);
-    }
+    },
   };
   return createHttpPoller(poller, {
     intervalInMs: options?.updateIntervalInMs,
@@ -113,7 +113,7 @@ export function getLongRunningPoller<
     restoreFrom: options?.restoreFrom,
     processResult: (result: unknown) => {
       return processResponseBody(result as TResponse) as TResult;
-    }
+    },
   });
 }
 /**
@@ -123,7 +123,7 @@ export function getLongRunningPoller<
  * @returns - An LRO response that the LRO implementation understands
  */
 function getLroResponse<TResponse extends PathUncheckedResponse>(
-  response: TResponse
+  response: TResponse,
 ): OperationResponse<TResponse> {
   if (isUnexpected(response as PathUncheckedResponse)) {
     throw createRestError(response);
@@ -133,7 +133,7 @@ function getLroResponse<TResponse extends PathUncheckedResponse>(
     rawResponse: {
       ...response,
       statusCode: Number.parseInt(response.status),
-      body: response.body
-    }
+      body: response.body,
+    },
   };
 }
