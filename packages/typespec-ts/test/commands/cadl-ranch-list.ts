4b1c4379088659208c57709ce2be0dc43bf57865
Qiaoqiao Zhang <qiaozha@microsoft.com>
2024-04-23 12:48:49 +0800

    undo cadl ranch cases list

--- packages/typespec-ts/test/commands/cadl-ranch-list.ts
+++ packages/typespec-ts/test/commands/cadl-ranch-list.ts
@@ -247,179 +247,179 @@ export const nonBrandedRlcTsps: TypeSpecRanchConfig[] = [
 ];
 
 export const modularTsps: TypeSpecRanchConfig[] = [
-//   {
-//     outputPath: "azure/clientGeneratorCore/access",
-//     inputPath: "azure/client-generator-core/access"
-//   },
-//   {
-//     outputPath: "azure/clientGeneratorCore/usage",
-//     inputPath: "azure/client-generator-core/usage"
-//   },
-//   {
-//     outputPath: "parameters/body-optionality",
-//     inputPath: "parameters/body-optionality"
-//   },
-//   {
-//     outputPath: "models/usage",
-//     inputPath: "type/model/usage"
-//   },
-//   {
-//     outputPath: "client/structure/default",
-//     inputPath: "client/structure/default"
-//   },
-//   {
-//     outputPath: "client/structure/multi-client",
-//     inputPath: "client/structure/multi-client"
-//   },
-//   {
-//     outputPath: "client/structure/renamed-operation",
-//     inputPath: "client/structure/renamed-operation"
-//   },
-//   {
-//     outputPath: "client/structure/two-operation-group",
-//     inputPath: "client/structure/two-operation-group"
-//   },
-//   {
-//     outputPath: "headers/client-request-id",
-//     inputPath: "special-headers/client-request-id"
-//   },
-//   {
-//     outputPath: "parameters/collection-format",
-//     inputPath: "parameters/collection-format"
-//   },
-//   {
-//     outputPath: "azure/core",
-//     inputPath: "azure/core/basic"
-//   },
-//   {
-//     outputPath: "models/inheritance/nested-discriminator",
-//     inputPath: "type/model/inheritance/nested-discriminator"
-//   },
-//   {
-//     outputPath: "models/inheritance/not-discriminated",
-//     inputPath: "type/model/inheritance/not-discriminated"
-//   },
-//   {
-//     outputPath: "models/inheritance/enum-discriminator",
-//     inputPath: "type/model/inheritance/enum-discriminator"
-//   },
-//   {
-//     outputPath: "models/inheritance/single-discriminator",
-//     inputPath: "type/model/inheritance/single-discriminator"
-//   },
-//   {
-//     outputPath: "payload/pageable",
-//     inputPath: "payload/pageable"
-//   },
-//   {
-//     outputPath: "encode/bytes",
-//     inputPath: "encode/bytes"
-//   },
-//   {
-//     outputPath: "encode/duration",
-//     inputPath: "encode/duration"
-//   },
-//   {
-//     outputPath: "encode/datetime",
-//     inputPath: "encode/datetime"
-//   },
-//   {
-//     outputPath: "parameters/spread",
-//     inputPath: "parameters/spread"
-//   },
-//   {
-//     outputPath: "server/path/single",
-//     inputPath: "server/path/single"
-//   },
-//   {
-//     outputPath: "authentication/api-key",
-//     inputPath: "authentication/api-key"
-//   },
-//   {
-//     outputPath: "authentication/http-custom",
-//     inputPath: "authentication/http/custom"
-//   },
-//   {
-//     outputPath: "server/path/multiple",
-//     inputPath: "server/path/multiple"
-//   },
-//   {
-//     outputPath: "authentication/oauth2",
-//     inputPath: "authentication/oauth2"
-//   },
-//   {
-//     outputPath: "authentication/union",
-//     inputPath: "authentication/union"
-//   },
-//   {
-//     outputPath: "headers/repeatability",
-//     inputPath: "special-headers/repeatability"
-//   },
-//   {
-//     outputPath: "azure/core-traits",
-//     inputPath: "azure/core/traits"
-//   },
-//   {
-//     outputPath: "models/empty",
-//     inputPath: "type/model/empty"
-//   },
-//   {
-//     outputPath: "models/propertyTypes",
-//     inputPath: "type/property/value-types"
-//   },
-//   { outputPath: "arrays/items", inputPath: "type/array" },
-//   {
-//     outputPath: "models/inheritance/recursive",
-//     inputPath: "type/model/inheritance/recursive"
-//   },
-//   {
-//     outputPath: "unions",
-//     inputPath: "type/union"
-//   },
-//   {
-//     outputPath: "payload/media-type",
-//     inputPath: "payload/media-type"
-//   },
-//   {
-//     outputPath: "server/versions/versioned",
-//     inputPath: "server/versions/versioned"
-//   },
-//   {
-//     outputPath: "server/versions/not-versioned",
-//     inputPath: "server/versions/not-versioned"
-//   },
-//   {
-//     outputPath: "scalar",
-//     inputPath: "type/scalar"
-//   },
-//   {
-//     outputPath: "client/naming",
-//     inputPath: "client/naming"
-//   },
-//   {
-//     outputPath: "serialization/encoded-name/json",
-//     inputPath: "serialization/encoded-name/json"
-//   },
-//   {
-//     outputPath: "azure/core-scalar",
-//     inputPath: "azure/core/scalar"
-//   },
-//   {
-//     outputPath: "server/endpoint/not-defined",
-//     inputPath: "server/endpoint/not-defined"
-//   },
-//   {
-//     outputPath: "specialWords",
-//     inputPath: "special-words"
-//   },
-//   {
-//     outputPath: "enums/extensible",
-//     inputPath: "type/enum/extensible"
-//   },
-//   {
-//     outputPath: "enums/fixed",
-//     inputPath: "type/enum/fixed"
-//   },
+  {
+    outputPath: "azure/clientGeneratorCore/access",
+    inputPath: "azure/client-generator-core/access"
+  },
+  {
+    outputPath: "azure/clientGeneratorCore/usage",
+    inputPath: "azure/client-generator-core/usage"
+  },
+  {
+    outputPath: "parameters/body-optionality",
+    inputPath: "parameters/body-optionality"
+  },
+  {
+    outputPath: "models/usage",
+    inputPath: "type/model/usage"
+  },
+  {
+    outputPath: "client/structure/default",
+    inputPath: "client/structure/default"
+  },
+  {
+    outputPath: "client/structure/multi-client",
+    inputPath: "client/structure/multi-client"
+  },
+  {
+    outputPath: "client/structure/renamed-operation",
+    inputPath: "client/structure/renamed-operation"
+  },
+  {
+    outputPath: "client/structure/two-operation-group",
+    inputPath: "client/structure/two-operation-group"
+  },
+  {
+    outputPath: "headers/client-request-id",
+    inputPath: "special-headers/client-request-id"
+  },
+  {
+    outputPath: "parameters/collection-format",
+    inputPath: "parameters/collection-format"
+  },
+  {
+    outputPath: "azure/core",
+    inputPath: "azure/core/basic"
+  },
+  {
+    outputPath: "models/inheritance/nested-discriminator",
+    inputPath: "type/model/inheritance/nested-discriminator"
+  },
+  {
+    outputPath: "models/inheritance/not-discriminated",
+    inputPath: "type/model/inheritance/not-discriminated"
+  },
+  {
+    outputPath: "models/inheritance/enum-discriminator",
+    inputPath: "type/model/inheritance/enum-discriminator"
+  },
+  {
+    outputPath: "models/inheritance/single-discriminator",
+    inputPath: "type/model/inheritance/single-discriminator"
+  },
+  {
+    outputPath: "payload/pageable",
+    inputPath: "payload/pageable"
+  },
+  {
+    outputPath: "encode/bytes",
+    inputPath: "encode/bytes"
+  },
+  {
+    outputPath: "encode/duration",
+    inputPath: "encode/duration"
+  },
+  {
+    outputPath: "encode/datetime",
+    inputPath: "encode/datetime"
+  },
+  {
+    outputPath: "parameters/spread",
+    inputPath: "parameters/spread"
+  },
+  {
+    outputPath: "server/path/single",
+    inputPath: "server/path/single"
+  },
+  {
+    outputPath: "authentication/api-key",
+    inputPath: "authentication/api-key"
+  },
+  {
+    outputPath: "authentication/http-custom",
+    inputPath: "authentication/http/custom"
+  },
+  {
+    outputPath: "server/path/multiple",
+    inputPath: "server/path/multiple"
+  },
+  {
+    outputPath: "authentication/oauth2",
+    inputPath: "authentication/oauth2"
+  },
+  {
+    outputPath: "authentication/union",
+    inputPath: "authentication/union"
+  },
+  {
+    outputPath: "headers/repeatability",
+    inputPath: "special-headers/repeatability"
+  },
+  {
+    outputPath: "azure/core-traits",
+    inputPath: "azure/core/traits"
+  },
+  {
+    outputPath: "models/empty",
+    inputPath: "type/model/empty"
+  },
+  {
+    outputPath: "models/propertyTypes",
+    inputPath: "type/property/value-types"
+  },
+  { outputPath: "arrays/items", inputPath: "type/array" },
+  {
+    outputPath: "models/inheritance/recursive",
+    inputPath: "type/model/inheritance/recursive"
+  },
+  {
+    outputPath: "unions",
+    inputPath: "type/union"
+  },
+  {
+    outputPath: "payload/media-type",
+    inputPath: "payload/media-type"
+  },
+  {
+    outputPath: "server/versions/versioned",
+    inputPath: "server/versions/versioned"
+  },
+  {
+    outputPath: "server/versions/not-versioned",
+    inputPath: "server/versions/not-versioned"
+  },
+  {
+    outputPath: "scalar",
+    inputPath: "type/scalar"
+  },
+  {
+    outputPath: "client/naming",
+    inputPath: "client/naming"
+  },
+  {
+    outputPath: "serialization/encoded-name/json",
+    inputPath: "serialization/encoded-name/json"
+  },
+  {
+    outputPath: "azure/core-scalar",
+    inputPath: "azure/core/scalar"
+  },
+  {
+    outputPath: "server/endpoint/not-defined",
+    inputPath: "server/endpoint/not-defined"
+  },
+  {
+    outputPath: "specialWords",
+    inputPath: "special-words"
+  },
+  {
+    outputPath: "enums/extensible",
+    inputPath: "type/enum/extensible"
+  },
+  {
+    outputPath: "enums/fixed",
+    inputPath: "type/enum/fixed"
+  },
   {
     outputPath: "models/propertyAdditional",
     inputPath: "type/property/additional-properties"
