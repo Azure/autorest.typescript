0cd4056b733582be042101229fc1b5db4ffa8854
Jiao Di (MSFT) <80496810+v-jiaodi@users.noreply.github.com>
2024-04-29 16:49:41 +0800

    update test case without try catch

--- packages/typespec-ts/test/integration/payloadContentNegotiation.spec.ts
+++ packages/typespec-ts/test/integration/payloadContentNegotiation.spec.ts
@@ -21,97 +21,73 @@ describe("Content Negotiation Client", () => {
   });
 
   it("should get image/png for same body in content negotiation", async () => {
-    try {
-      const result = await client.path("/content-negotiation/same-body").get({
-        headers: { accept: "image/png" }
-      });
-      assert.strictEqual(result.status, "200");
-      assert.strictEqual(
-        uint8ArrayToString(result.body, "utf-8"),
-        pngFile.toString()
-      );
-    } catch (err) {
-      assert.fail(err as string);
-    }
+    const result = await client.path("/content-negotiation/same-body").get({
+      headers: { accept: "image/png" }
+    });
+    assert.strictEqual(result.status, "200");
+    assert.strictEqual(
+      uint8ArrayToString(result.body, "utf-8"),
+      pngFile.toString()
+    );
   });
 
   it("should get image/jpeg for same body in content negotiation", async () => {
-    try {
-      const result = await client
-        .path("/content-negotiation/same-body")
-        .get({ headers: { accept: "image/jpeg" } });
-      assert.strictEqual(result.status, "200");
-      assert.strictEqual(
-        uint8ArrayToString(result.body, "utf-8"),
-        jpegImage.toString()
-      );
-    } catch (err) {
-      assert.fail(err as string);
-    }
+    const result = await client
+      .path("/content-negotiation/same-body")
+      .get({ headers: { accept: "image/jpeg" } });
+    assert.strictEqual(result.status, "200");
+    assert.strictEqual(
+      uint8ArrayToString(result.body, "utf-8"),
+      jpegImage.toString()
+    );
   });
 
   it("should return error if put wrong accept for same body in content negotiation", async () => {
-    try {
-      const result = await client
-        .path("/content-negotiation/same-body")
-        .get({ headers: { accept: "wrongAccept" } as any });
-      assert.strictEqual(
-        (result.body as any).message,
-        "Unsupported Accept header"
-      );
-      assert.strictEqual(
-        (result.body as any).expected,
-        `"image/png" | "image/jpeg"`
-      );
-      assert.strictEqual((result.body as any).actual, "wrongAccept");
-    } catch (err) {
-      assert.fail(err as string);
-    }
+    const result = await client
+      .path("/content-negotiation/same-body")
+      .get({ headers: { accept: "wrongAccept" } as any });
+    assert.strictEqual(
+      (result.body as any).message,
+      "Unsupported Accept header"
+    );
+    assert.strictEqual(
+      (result.body as any).expected,
+      `"image/png" | "image/jpeg"`
+    );
+    assert.strictEqual((result.body as any).actual, "wrongAccept");
   });
 
   it("should get image/png for different body in content negotiation", async () => {
-    try {
-      const result = await client
-        .path("/content-negotiation/different-body")
-        .get({ headers: { accept: "image/png" } });
-      assert.strictEqual(result.status, "200");
-      assert.strictEqual(
-        uint8ArrayToString(result.body, "utf-8"),
-        pngFile.toString()
-      );
-    } catch (err) {
-      assert.fail(err as string);
-    }
+    const result = await client
+      .path("/content-negotiation/different-body")
+      .get({ headers: { accept: "image/png" } });
+    assert.strictEqual(result.status, "200");
+    assert.strictEqual(
+      uint8ArrayToString(result.body, "utf-8"),
+      pngFile.toString()
+    );
   });
 
   it("should get application/json for different body in content negotiation", async () => {
-    try {
-      const result = await client
-        .path("/content-negotiation/different-body")
-        .get({ headers: { accept: "application/json" } });
-      assert.strictEqual(result.status, "200");
-      assert.strictEqual(result.body.content, pngFile.toString("base64"));
-    } catch (err) {
-      assert.fail(err as string);
-    }
+    const result = await client
+      .path("/content-negotiation/different-body")
+      .get({ headers: { accept: "application/json" } });
+    assert.strictEqual(result.status, "200");
+    assert.strictEqual(result.body.content, pngFile.toString("base64"));
   });
 
   it("should return error if put wrong accept for different body in content negotiation", async () => {
-    try {
-      const result = await client
-        .path("/content-negotiation/different-body")
-        .get({ headers: { accept: "wrongAccept" } as any });
-      assert.strictEqual(
-        (result.body as any).message,
-        "Unsupported Accept header"
-      );
-      assert.strictEqual(
-        (result.body as any).expected,
-        `"image/png" | "application/json"`
-      );
-      assert.strictEqual((result.body as any).actual, "wrongAccept");
-    } catch (err) {
-      assert.fail(err as string);
-    }
+    const result = await client
+      .path("/content-negotiation/different-body")
+      .get({ headers: { accept: "wrongAccept" } as any });
+    assert.strictEqual(
+      (result.body as any).message,
+      "Unsupported Accept header"
+    );
+    assert.strictEqual(
+      (result.body as any).expected,
+      `"image/png" | "application/json"`
+    );
+    assert.strictEqual((result.body as any).actual, "wrongAccept");
   });
 });
