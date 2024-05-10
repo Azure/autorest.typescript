1f6d0bef36f1d68190d9489e1f9a7dab991810ce
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2024-05-10 10:09:13 +0800

    add rules

--- packages/rlc-common/src/metadata/buildESLintConfig.ts
+++ packages/rlc-common/src/metadata/buildESLintConfig.ts
@@ -12,6 +12,9 @@ const esLintConfig = {
     "@azure/azure-sdk/ts-apiextractor-json-types": "warn",
     "@azure/azure-sdk/ts-package-json-types": "warn",
     "@azure/azure-sdk/ts-package-json-engine-is-present": "warn",
+    "@azure/azure-sdk/ts-package-json-module": "off",
+    "@azure/azure-sdk/ts-package-json-files-required": "off",
+    "@azure/azure-sdk/ts-package-json-main-is-cjs": "off",
     "tsdoc/syntax": "warn"
   }
 };
