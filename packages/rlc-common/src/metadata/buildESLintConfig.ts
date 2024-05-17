48367df139944980c92dc8d255e94d1725535bdb
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2024-05-17 17:26:48 +0800

    update
    This reverts commit 1f6d0bef36f1d68190d9489e1f9a7dab991810ce.

--- packages/rlc-common/src/metadata/buildESLintConfig.ts
+++ packages/rlc-common/src/metadata/buildESLintConfig.ts
@@ -12,10 +12,7 @@ const esLintConfig = {
     "@azure/azure-sdk/ts-apiextractor-json-types": "warn",
     "@azure/azure-sdk/ts-package-json-types": "warn",
     "@azure/azure-sdk/ts-package-json-engine-is-present": "warn",
-    "@azure/azure-sdk/ts-package-json-module": "off",
-    "@azure/azure-sdk/ts-package-json-files-required": "off",
-    "@azure/azure-sdk/ts-package-json-main-is-cjs": "off",
-    "tsdoc/syntax": "warn"
+    "tsdoc/syntax": "warn",
   }
 };
 
@@ -25,6 +22,11 @@ export function buildEsLintConfig(model: RLCModel) {
   }
   const project = new Project();
   const filePath = ".eslintrc.json";
+  if(model.options?.moduleKind == "esm"){
+   (esLintConfig.rules as any)["@azure/azure-sdk/ts-package-json-module"] ="off";
+   (esLintConfig.rules as any)["@azure/azure-sdk/ts-package-json-files-required"] ="off";
+   (esLintConfig.rules as any)["@azure/azure-sdk/ts-package-json-main-is-cjs"] ="off";
+  }
   const configFile = project.createSourceFile(
     ".eslintrc.json",
     JSON.stringify(esLintConfig),
