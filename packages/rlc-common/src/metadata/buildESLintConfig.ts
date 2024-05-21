28a8e38aedd83f12a3316a281a84dae1a090fb24
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2024-05-21 09:55:54 +0800

    update

--- packages/rlc-common/src/metadata/buildESLintConfig.ts
+++ packages/rlc-common/src/metadata/buildESLintConfig.ts
@@ -4,7 +4,7 @@
 import { Project } from "ts-morph";
 import { RLCModel } from "../interfaces.js";
 
-const esLintConfig = {
+const esLintConfig: Record<string, any>= {
   plugins: ["@azure/azure-sdk"],
   extends: ["plugin:@azure/azure-sdk/azure-sdk-base"],
   rules: {
@@ -23,12 +23,12 @@ export function buildEsLintConfig(model: RLCModel) {
   const project = new Project();
   const filePath = ".eslintrc.json";
   if (model.options?.moduleKind == "esm") {
-    (esLintConfig.rules as any)["@azure/azure-sdk/ts-package-json-module"] =
+    esLintConfig.rules["@azure/azure-sdk/ts-package-json-module"] =
       "off";
-    (esLintConfig.rules as any)[
+    esLintConfig.rules[
       "@azure/azure-sdk/ts-package-json-files-required"
     ] = "off";
-    (esLintConfig.rules as any)[
+    esLintConfig.rules[
       "@azure/azure-sdk/ts-package-json-main-is-cjs"
     ] = "off";
   }
