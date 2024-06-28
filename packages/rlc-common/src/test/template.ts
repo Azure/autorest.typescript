dbd64d248686824728718223f7fa56d50bd0ae96
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2024-06-28 16:50:44 +0800

    Update template.ts

--- packages/rlc-common/src/test/template.ts
+++ packages/rlc-common/src/test/template.ts
@@ -127,28 +127,28 @@ module.exports = function (config) {
 `;
 
 export const recordedClientContent = `
-import { Context } from "mocha";
-import { Recorder, RecorderStartOptions } from "@azure-tools/test-recorder";
+import {
+  Recorder,
+  RecorderStartOptions,
+  VitestTestContext,
+} from "@azure-tools/test-recorder";
 
-const envSetupForPlayback: Record<string, string> = {
-  ENDPOINT: "https://endpoint",
-  AZURE_CLIENT_ID: "azure_client_id",
-  AZURE_CLIENT_SECRET: "azure_client_secret",
-  AZURE_TENANT_ID: "88888888-8888-8888-8888-888888888888",
+
+const replaceableVariables: Record<string, string> = {
   SUBSCRIPTION_ID: "azure_subscription_id"
 };
 
 const recorderEnvSetup: RecorderStartOptions = {
-  envSetupForPlayback
+  envSetupForPlayback: replaceableVariables,
 };
 
 /**
-* creates the recorder and reads the environment variables from the \`.env\` file.
-* Should be called first in the test suite to make sure environment variables are
-* read before they are being used.
-*/
-export async function createRecorder(context: Context): Promise<Recorder> {
-  const recorder = new Recorder(context.currentTest);
+ * creates the recorder and reads the environment variables from the \`.env\` file.
+ * Should be called first in the test suite to make sure environment variables are
+ * read before they are being used.
+ */
+export async function createRecorder(context: VitestTestContext): Promise<Recorder> {
+  const recorder = new Recorder(context);
   await recorder.start(recorderEnvSetup);
   return recorder;
 }
