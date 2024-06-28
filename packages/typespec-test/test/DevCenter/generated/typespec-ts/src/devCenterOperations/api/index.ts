22a2a84806df21cdcd080e31ae445202b6ce9b92
Jiao Di (MSFT) <80496810+v-jiaodi@users.noreply.github.com>
2024-06-28 13:57:21 +0800

    update config and regen

--- packages/typespec-test/test/DevCenter/generated/typespec-ts/src/devCenterOperations/api/index.ts
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT license.
-
-export {
-  createDevCenterOperations,
-  DevCenterOperationsClientOptions,
-  DevCenterServiceContext,
-} from "./devCenterOperationsContext.js";
-export { listProjects, getProject } from "./operations.js";
