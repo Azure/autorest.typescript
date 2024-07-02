169003c45d7ab0e482416d49d4150e9fbf75b72b
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2024-07-02 11:32:23 +0800

    update

--- packages/autorest.typescript/test/rlcIntegration/generated/bodyStringRest/test/public/utils/recordedClient.ts
+++ packages/autorest.typescript/test/rlcIntegration/generated/bodyStringRest/test/public/utils/recordedClient.ts
@@ -4,16 +4,12 @@
 import { Context } from "mocha";
 import { Recorder, RecorderStartOptions } from "@azure-tools/test-recorder";
 
-const envSetupForPlayback: Record<string, string> = {
-  ENDPOINT: "https://endpoint",
-  AZURE_CLIENT_ID: "azure_client_id",
-  AZURE_CLIENT_SECRET: "azure_client_secret",
-  AZURE_TENANT_ID: "88888888-8888-8888-8888-888888888888",
+const replaceableVariables: Record<string, string> = {
   SUBSCRIPTION_ID: "azure_subscription_id",
 };
 
 const recorderEnvSetup: RecorderStartOptions = {
-  envSetupForPlayback,
+  envSetupForPlayback: replaceableVariables,
 };
 
 /**
@@ -21,6 +17,7 @@ const recorderEnvSetup: RecorderStartOptions = {
  * Should be called first in the test suite to make sure environment variables are
  * read before they are being used.
  */
+
 export async function createRecorder(context: Context): Promise<Recorder> {
   const recorder = new Recorder(context.currentTest);
   await recorder.start(recorderEnvSetup);
