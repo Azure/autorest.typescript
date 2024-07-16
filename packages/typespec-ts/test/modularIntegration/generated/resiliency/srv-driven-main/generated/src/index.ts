cc84aa68e80aec76b389ba9e8cdc9955048229e7
Jiao Di (MSFT) <80496810+v-jiaodi@users.noreply.github.com>
2024-07-16 18:35:49 +0800

    Supplement test case for resiliency (#2674)
    * add resiliency test case for modular
    
    * remove repeated case

---
+++ packages/typespec-ts/test/modularIntegration/generated/resiliency/srv-driven-main/generated/src/index.ts
@@ -0,0 +1,14 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+export {
+  ServiceDrivenClient,
+  ServiceDrivenClientOptions,
+} from "./serviceDrivenClient.js";
+export {
+  Versions,
+  AddOperationOptionalParams,
+  FromNoneOptionalParams,
+  FromOneRequiredOptionalParams,
+  FromOneOptionalOptionalParams,
+} from "./models/index.js";
