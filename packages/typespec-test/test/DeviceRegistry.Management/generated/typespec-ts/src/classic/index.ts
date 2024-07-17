928b2192e8bcb11722b275f34f5650951b69bdbf
Jiao Di (MSFT) <80496810+v-jiaodi@users.noreply.github.com>
2024-07-17 14:29:21 +0800

    remove device case

--- packages/typespec-test/test/DeviceRegistry.Management/generated/typespec-ts/src/classic/index.ts
+++
@@ -1,7 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT license.
-
-export { AssetEndpointProfilesOperations } from "./assetEndpointProfiles/index.js";
-export { AssetsOperations } from "./assets/index.js";
-export { OperationsOperations } from "./operations/index.js";
-export { OperationStatusOperations } from "./operationStatus/index.js";
