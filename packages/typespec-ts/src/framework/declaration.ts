7b54c895e8b3192c2d7faef77ccfedb2928e55a2
Jose Manuel Heredia Hidalgo <joheredi@microsoft.com>
2024-07-17 10:47:56 -0700

    Handle declarations and references

---
+++ packages/typespec-ts/src/framework/declaration.ts
@@ -0,0 +1,98 @@
+import {
+  SourceFile,
+  ClassDeclarationStructure,
+  EnumDeclarationStructure,
+  FunctionDeclarationStructure,
+  InterfaceDeclarationStructure,
+  TypeAliasDeclarationStructure,
+  StructureKind,
+  ClassDeclaration,
+  EnumDeclaration,
+  FunctionDeclaration,
+  InterfaceDeclaration,
+  TypeAliasDeclaration
+} from "ts-morph";
+import { useBinder } from "./hooks/binder.js";
+
+export type DeclarationStructures =
+  | ClassDeclarationStructure
+  | EnumDeclarationStructure
+  | FunctionDeclarationStructure
+  | InterfaceDeclarationStructure
+  | TypeAliasDeclarationStructure;
+
+export type Declarations =
+  | ClassDeclaration
+  | EnumDeclaration
+  | FunctionDeclaration
+  | InterfaceDeclaration
+  | TypeAliasDeclaration;
+
+export function addDeclaration(
+  sourceFile: SourceFile,
+  declaration: ClassDeclarationStructure,
+  refkey: unknown
+): ClassDeclaration;
+
+export function addDeclaration(
+  sourceFile: SourceFile,
+  declaration: EnumDeclarationStructure,
+  refkey: unknown
+): EnumDeclaration;
+
+export function addDeclaration(
+  sourceFile: SourceFile,
+  declaration: FunctionDeclarationStructure,
+  refkey: unknown
+): FunctionDeclaration;
+
+export function addDeclaration(
+  sourceFile: SourceFile,
+  declaration: InterfaceDeclarationStructure,
+  refkey: unknown
+): InterfaceDeclaration;
+
+export function addDeclaration(
+  sourceFile: SourceFile,
+  declaration: TypeAliasDeclarationStructure,
+  refkey: unknown
+): TypeAliasDeclaration;
+export function addDeclaration(
+  sourceFile: SourceFile,
+  declaration: DeclarationStructures,
+  refkey: unknown
+): Declarations {
+  const binder = useBinder();
+
+  if (!declaration.name) {
+    throw new Error(
+      `Declaration must have a name ${JSON.stringify(declaration)}`
+    );
+  }
+
+  const declarationInfo = binder.trackDeclaration(
+    refkey,
+    declaration.name,
+    sourceFile
+  );
+
+  // Update the declaration name to be unique
+  declaration.name = declarationInfo.name;
+
+  switch (declaration.kind) {
+    case StructureKind.Class:
+      return sourceFile.addClass(declaration);
+    case StructureKind.Enum:
+      return sourceFile.addEnum(declaration);
+    case StructureKind.Function:
+      return sourceFile.addFunction(declaration);
+    case StructureKind.Interface:
+      return sourceFile.addInterface(declaration);
+    case StructureKind.TypeAlias:
+      return sourceFile.addTypeAlias(declaration);
+    default:
+      throw new Error(
+        `Unsupported declaration kind ${(declaration as any).kind}`
+      );
+  }
+}
---
+++ packages/typespec-ts/src/framework/reference.ts
@@ -0,0 +1,12 @@
+import { SourceFile } from "ts-morph";
+import { useBinder } from "./hooks/binder.js";
+
+export function resolveReference(
+  refkey: unknown,
+  currentSourceFile: SourceFile
+): string | undefined {
+  const binder = useBinder();
+
+  const declarationInfo = binder.resolveReference(refkey, currentSourceFile);
+  return declarationInfo!.name;
+}
