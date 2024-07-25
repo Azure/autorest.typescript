f380fcdd3dbbbc897f23c7b8173c60d398da6e72
Jiao Di (MSFT) <80496810+v-jiaodi@users.noreply.github.com>
2024-07-25 13:32:03 +0800

    Add test case for Azure Managed Identity (#2689)
    * Add test case for Azure Managed Identity
    
    * update

---
+++ packages/typespec-ts/test/modularIntegration/generated/azure/resource-manager/models/common-types/managed-identity/src/index.ts
@@ -0,0 +1,27 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+export {
+  ManagedIdentityClient,
+  ManagedIdentityClientOptionalParams,
+} from "./managedIdentityClient.js";
+export {
+  Resource,
+  SystemData,
+  KnownCreatedByType,
+  CreatedByType,
+  TrackedResource,
+  ManagedIdentityTrackedResource,
+  ManagedIdentityTrackedResourceProperties,
+  ManagedServiceIdentity,
+  KnownManagedServiceIdentityType,
+  ManagedServiceIdentityType,
+  UserAssignedIdentity,
+  ErrorResponse,
+  ErrorDetail,
+  ErrorAdditionalInfo,
+  Versions,
+  GetOptionalParams,
+  CreateWithSystemAssignedOptionalParams,
+  UpdateWithUserAssignedAndSystemAssignedOptionalParams,
+} from "./models/index.js";
