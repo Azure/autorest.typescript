1cc2b90d5feafbfa8091370f6e7799d289b46952
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2024-07-31 13:39:50 +0800

    Update buildCodeModel.ts

--- packages/typespec-ts/src/modular/buildCodeModel.ts
+++ packages/typespec-ts/src/modular/buildCodeModel.ts
@@ -1561,7 +1561,7 @@ function emitOperationGroups(
     context.rlcOptions?.hierarchyClient === false &&
     context.rlcOptions?.enableOperationGroup
   ) {
-    resolveConflict(operationGroups);
+    appendOperationGroupPrefix (operationGroups);
   }
   return operationGroups;
 }
@@ -1589,7 +1589,7 @@ function addHierarchyOperationGroup(
   return [];
 }
 
-function resolveConflict(operationGroups: OperationGroup[]) {
+function appendOperationGroupPrefix (operationGroups: OperationGroup[]) {
   if (operationGroups.length < 2) {
     return;
   }
