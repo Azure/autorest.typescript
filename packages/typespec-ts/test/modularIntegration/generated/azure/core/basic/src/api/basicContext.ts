f9351cb5a03241fa6d2112a8c60b49cd2e12bb9c
Timo van Veenendaal <me@timo.nz>
2024-08-08 16:33:21 +0000

    Update tests

--- packages/typespec-ts/test/modularIntegration/generated/azure/core/basic/src/api/basicContext.ts
+++ packages/typespec-ts/test/modularIntegration/generated/azure/core/basic/src/api/basicContext.ts
@@ -23,7 +23,7 @@ export function createBasic(
   const userAgentPrefix = prefixFromOptions
     ? `${prefixFromOptions} azsdk-js-api`
     : "azsdk-js-api";
-  const updatedOptions = {
+  const { apiVersion: _, ...updatedOptions } = {
     ...options,
     userAgentOptions: { userAgentPrefix },
     loggingOptions: { logger: options.loggingOptions?.logger ?? logger.info },
--- packages/typespec-ts/test/modularIntegration/generated/resiliency/srv-driven-main/generated/src/api/serviceDrivenContext.ts
+++ packages/typespec-ts/test/modularIntegration/generated/resiliency/srv-driven-main/generated/src/api/serviceDrivenContext.ts
@@ -39,7 +39,7 @@ export function createServiceDriven(
   const userAgentPrefix = prefixFromOptions
     ? `${prefixFromOptions} azsdk-js-api`
     : "azsdk-js-api";
-  const updatedOptions = {
+  const { apiVersion: _, ...updatedOptions } = {
     ...options,
     userAgentOptions: { userAgentPrefix },
     loggingOptions: { logger: options.loggingOptions?.logger ?? logger.info },
