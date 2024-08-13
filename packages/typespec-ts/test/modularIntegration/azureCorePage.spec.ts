10081596eac4b5e75a5c10eea1a6d55acb2feed6
Jiao Di (MSFT) <80496810+v-jiaodi@users.noreply.github.com>
2024-08-13 17:17:47 +0800

    update test case

--- packages/typespec-ts/test/modularIntegration/azureCorePage.spec.ts
+++ packages/typespec-ts/test/modularIntegration/azureCorePage.spec.ts
@@ -1,4 +1,4 @@
-import { PageClient } from "./generated/azure/core/page/src/index.js";
+import { PageClient, User } from "./generated/azure/core/page/src/index.js";
 import { assert } from "chai";
 
 describe("Page Client", () => {
@@ -15,38 +15,55 @@ describe("Page Client", () => {
     name: "Madge",
     etag: "11bdc430-65e8-45ad-81d9-8ffa60d55b59"
   };
-  // const responseBody = {
-  //   value: [validUser]
-  // };
-  it.only("should list core page withPage", async () => {
-    const result = (await client.listWithPage().next()).value;
 
-    assert.deepStrictEqual(result, validUser);
+  it("should list core page withPage", async () => {
+    const result = client.listWithPage();
+    const items: User[] = [];
+    for await (const page of result) {
+      items.push(page);
+    }
+    assert.strictEqual(items[0]?.name, validUser.name);
+    assert.strictEqual(items[0]?.id, validUser.id);
+    assert.strictEqual(items[0]?.etag, validUser.etag);
   });
 
-  // it("should list core page withParameters", async () => {
-  //   const validBody = { inputName: "Madge" };
-  //   const result = await client.listWithParameters(validBody, {
-  //     another: "Second"
-  //   });
-  //   assert.deepStrictEqual(result, responseBody);
-  // });
+  it("should list core page withParameters", async () => {
+    const validBody = { inputName: "Madge" };
+    const result = client.listWithParameters(validBody, {
+      another: "Second"
+    });
+    const items: User[] = [];
+    for await (const parameter of result) {
+      items.push(parameter);
+    }
+    assert.strictEqual(items[0]?.name, validUser.name);
+    assert.strictEqual(items[0]?.id, validUser.id);
+    assert.strictEqual(items[0]?.etag, validUser.etag);
+  });
 
-  // it("should get core page TwoModelsAsPageItem", async () => {
-  //   const result1 = await client.listFirstItem();
-  //   assert.deepStrictEqual(result1, {
-  //     value: [{ id: 1 }]
-  //   });
-  //   const result2 = await client.listSecondItem();
-  //   assert.deepStrictEqual(result2, {
-  //     value: [{ name: "Madge" }]
-  //   });
-  // });
+  it("should get core page TwoModelsAsPageItem", async () => {
+    const result1 = await client.listFirstItem();
+    const firstItems = [];
+    for await (const user of result1) {
+      firstItems.push(user);
+    }
+    assert.strictEqual(firstItems[0]?.id, 1);
+    const result2 = await client.listSecondItem();
+    const secondItems = [];
+    for await (const user of result2) {
+      secondItems.push(user);
+    }
+    assert.deepStrictEqual(secondItems[0]?.name, "Madge");
+  });
 
-  // it("should list core page withCustomPageModel", async () => {
-  //   const result = await client.listWithCustomPageModel();
-  //   assert.deepStrictEqual(result, {
-  //     items: [validUser]
-  //   });
-  // });
+  it("should list core page withCustomPageModel", async () => {
+    const result = client.listWithCustomPageModel();
+    const items: User[] = [];
+    for await (const model of result) {
+      items.push(model);
+    }
+    assert.strictEqual(items[0]?.name, validUser.name);
+    assert.strictEqual(items[0]?.id, validUser.id);
+    assert.strictEqual(items[0]?.etag, validUser.etag);
+  });
 });
