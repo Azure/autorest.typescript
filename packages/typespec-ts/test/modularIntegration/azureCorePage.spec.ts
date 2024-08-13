5161c08b5baf9d1a05be1b33a1a2eacf3686235d
Jiao Di (MSFT) <80496810+v-jiaodi@users.noreply.github.com>
2024-08-13 16:33:49 +0800

    update case

--- packages/typespec-ts/test/modularIntegration/azureCorePage.spec.ts
+++ packages/typespec-ts/test/modularIntegration/azureCorePage.spec.ts
@@ -15,13 +15,13 @@ describe("Page Client", () => {
     name: "Madge",
     etag: "11bdc430-65e8-45ad-81d9-8ffa60d55b59"
   };
-  const responseBody = {
-    value: [validUser]
-  };
+  // const responseBody = {
+  //   value: [validUser]
+  // };
   it.only("should list core page withPage", async () => {
-    const result = await client.listWithPage();
+    const result = (await client.listWithPage().next()).value;
 
-    assert.deepStrictEqual(result, responseBody);
+    assert.deepStrictEqual(result, validUser);
   });
 
   // it("should list core page withParameters", async () => {
