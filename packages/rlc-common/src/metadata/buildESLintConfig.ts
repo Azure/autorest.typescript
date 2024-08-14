ce6a98fc290ab2da2e3cc037a2e6a883ee7a050c
Jeremy Meng <yumeng@microsoft.com>
2024-08-14 10:38:15 -0700

    simplify building of eslint config by just using strings

--- packages/rlc-common/src/metadata/buildESLintConfig.ts
+++ packages/rlc-common/src/metadata/buildESLintConfig.ts
@@ -4,34 +4,38 @@
 import { Project } from "ts-morph";
 import { RLCModel } from "../interfaces.js";
 
-const rules: Record<string, any> = {
-  rules: {
-    "@azure/azure-sdk/ts-modules-only-named": "warn",
-    "@azure/azure-sdk/ts-apiextractor-json-types": "warn",
-    "@azure/azure-sdk/ts-package-json-types": "warn",
-    "@azure/azure-sdk/ts-package-json-engine-is-present": "warn",
-    "tsdoc/syntax": "warn"
-  }
-};
-
-const esmRules: Record<string, any> = {
-  rules: {
-    "@azure/azure-sdk/ts-modules-only-named": "warn",
-    "@azure/azure-sdk/ts-apiextractor-json-types": "warn",
-    "@azure/azure-sdk/ts-package-json-types": "warn",
-    "@azure/azure-sdk/ts-package-json-engine-is-present": "warn",
-    "@azure/azure-sdk/ts-package-json-module": "off",
-    "@azure/azure-sdk/ts-package-json-files-required": "off",
-    "@azure/azure-sdk/ts-package-json-main-is-cjs": "off",
-    "tsdoc/syntax": "warn"
-  }
-};
-
-const esLintConfigTemplate = `import azsdkEslint from "@azure/eslint-plugin-azure-sdk";
+const eslintConfig = `import azsdkEslint from "@azure/eslint-plugin-azure-sdk";
 
 export default [
   ...azsdkEslint.configs.recommended,
-  <rulesPlaceHolder>
+  {
+    rules: {
+      "@azure/azure-sdk/ts-modules-only-named": "warn",
+      "@azure/azure-sdk/ts-apiextractor-json-types": "warn",
+      "@azure/azure-sdk/ts-package-json-types": "warn",
+      "@azure/azure-sdk/ts-package-json-engine-is-present": "warn",
+      "tsdoc/syntax": "warn"
+    }
+  }
+];
+`;
+
+const esLintConfigEsm = `import azsdkEslint from "@azure/eslint-plugin-azure-sdk";
+
+export default [
+  ...azsdkEslint.configs.recommended,
+  {
+    rules: {
+      "@azure/azure-sdk/ts-modules-only-named": "warn",
+      "@azure/azure-sdk/ts-apiextractor-json-types": "warn",
+      "@azure/azure-sdk/ts-package-json-types": "warn",
+      "@azure/azure-sdk/ts-package-json-engine-is-present": "warn",
+      "@azure/azure-sdk/ts-package-json-module": "off",
+      "@azure/azure-sdk/ts-package-json-files-required": "off",
+      "@azure/azure-sdk/ts-package-json-main-is-cjs": "off",
+      "tsdoc/syntax": "warn"
+    }
+  }
 ];
 `;
 
@@ -42,14 +46,9 @@ export function buildEsLintConfig(model: RLCModel) {
   const project = new Project();
   const filePath = "eslint.config.mjs";
 
-  const contentText = esLintConfigTemplate.replace(
-    "<rulesPlaceHolder>",
-    JSON.stringify(model.options?.moduleKind === "esm" ? esmRules : rules, null, 2)
-  );
-
   const configFile = project.createSourceFile(
     "eslint.config.mjs",
-    contentText,
+    model.options?.moduleKind === "esm" ? esLintConfigEsm : eslintConfig,
     {
       overwrite: true
     }
