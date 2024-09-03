72472f93a1b5b3c26204a4d0c2de0a681f499d87
Jiao Di (MSFT) <80496810+v-jiaodi@users.noreply.github.com>
2024-09-03 17:39:25 +0800

    update tspconfig

--- packages/typespec-test/test/hierarchy_generic/generated/typespec-ts/src/models/index.ts
+++ packages/typespec-test/test/hierarchy_generic/generated/typespec-ts/src/models/index.ts
@@ -5,7 +5,6 @@ export { A, BA, BEA } from "./models.js";
 export {
   Op1OptionalParams,
   BOp1OptionalParams,
-  BECOp1OptionalParams,
-  BCOp1OptionalParams,
+  COp1OptionalParams,
   DOp1OptionalParams,
 } from "./options.js";
