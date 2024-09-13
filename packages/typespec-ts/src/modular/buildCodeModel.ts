74251f8e0fc376213372adcd16d655a86121750f
Jeremy Meng <yumeng@microsoft.com>
2024-09-13 11:13:05 -0700

    Fix build error
    - now Iterator's `TReturn` defaults to `undefined` instead of `any` so we need
    to ensure that there are values.
    
    - the type of values element is `EnumMember`. It builds fine because previously
    TReturn is any, so `EnumMember | any` ends up being `any`

--- packages/typespec-ts/src/modular/buildCodeModel.ts
+++ packages/typespec-ts/src/modular/buildCodeModel.ts
@@ -1,5 +1,6 @@
 import {
   Enum,
+  EnumMember,
   IntrinsicScalarName,
   Model,
   ModelProperty,
@@ -67,7 +68,6 @@ import {
 import {
   SdkBuiltInType,
   SdkClient,
-  SdkEnumValueType,
   SdkType,
   getAllModels,
   getClientNamespaceString,
@@ -1087,6 +1087,9 @@ function emitEnum(context: SdkContext, type: Enum): Record<string, any> {
     });
   }
 
+  if (enumValues.length === 0) {
+    throw new Error(`Expecting enum values but got none`);
+  }
   const name = normalizeName(
     getLibraryName(context, type) ? getLibraryName(context, type) : type.name,
     NameType.Interface
@@ -1098,14 +1101,14 @@ function emitEnum(context: SdkContext, type: Enum): Record<string, any> {
       getDocStr(program, type) === ""
         ? `Type of ${name}`
         : getDocStr(program, type),
-    valueType: { type: enumMemberType(type.members.values().next().value) },
+    valueType: { type: enumMemberType(type.members.values().next().value!) },
     values: enumValues,
     isFixed: true,
     coreTypeInfo: buildCoreTypeInfo(program, type)
   };
 }
 
-function enumMemberType(member: SdkEnumValueType) {
+function enumMemberType(member: EnumMember) {
   if (typeof member.value === "number") {
     return "number";
   }
