fd31197af5fdc9d0eec6e73d76d9c1aff0f178c9
Jiao Di (MSFT) <80496810+v-jiaodi@users.noreply.github.com>
2024-09-20 11:26:23 +0800

    fix ci

--- packages/typespec-test/test/anomalyDetector/generated/typespec-ts/src/models/index.ts
+++ packages/typespec-test/test/anomalyDetector/generated/typespec-ts/src/models/index.ts
@@ -35,6 +35,7 @@ export {
   UnivariateUnivariateChangePointDetectionOptions,
   UnivariateUnivariateChangePointDetectionResult,
   Versions,
+  MultivariateResponseError,
 } from "./models.js";
 export {
   UnivariateDetectUnivariateEntireSeriesOptionalParams,
