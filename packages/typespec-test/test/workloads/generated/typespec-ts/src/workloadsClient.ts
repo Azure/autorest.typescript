3dcbd1213f9f52b36a87eb597bfe2da93572dcc2
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2024-10-17 16:19:00 +0800

    smoke test for workloads

---
+++ packages/typespec-test/test/workloads/generated/typespec-ts/src/workloadsClient.ts
@@ -0,0 +1,81 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import { TokenCredential } from "@azure/core-auth";
+import { Pipeline } from "@azure/core-rest-pipeline";
+import {
+  getSAPVirtualInstancesOperations,
+  SAPVirtualInstancesOperations,
+} from "./classic/sAPVirtualInstances/index.js";
+import {
+  getSAPCentralServerInstancesOperations,
+  SAPCentralServerInstancesOperations,
+} from "./classic/sAPCentralServerInstances/index.js";
+import {
+  getSAPDatabaseInstancesOperations,
+  SAPDatabaseInstancesOperations,
+} from "./classic/sAPDatabaseInstances/index.js";
+import {
+  getSAPApplicationServerInstancesOperations,
+  SAPApplicationServerInstancesOperations,
+} from "./classic/sAPApplicationServerInstances/index.js";
+import {
+  getOperationsOperations,
+  OperationsOperations,
+} from "./classic/operations/index.js";
+import {
+  createWorkloads,
+  WorkloadsContext,
+  WorkloadsClientOptionalParams,
+} from "./api/index.js";
+
+export { WorkloadsClientOptionalParams } from "./api/workloadsContext.js";
+
+export class WorkloadsClient {
+  private _client: WorkloadsContext;
+  /** The pipeline used by this client to make requests */
+  public readonly pipeline: Pipeline;
+
+  /** Workloads client provides access to various workload operations. */
+  constructor(
+    credential: TokenCredential,
+    subscriptionId: string,
+    options: WorkloadsClientOptionalParams = {},
+  ) {
+    const prefixFromOptions = options?.userAgentOptions?.userAgentPrefix;
+    const userAgentPrefix = prefixFromOptions
+      ? `${prefixFromOptions} azsdk-js-client`
+      : "azsdk-js-client";
+    this._client = createWorkloads(credential, {
+      ...options,
+      userAgentOptions: { userAgentPrefix },
+    });
+    this.pipeline = this._client.pipeline;
+    this.sAPVirtualInstances = getSAPVirtualInstancesOperations(
+      this._client,
+      subscriptionId,
+    );
+    this.sAPCentralServerInstances = getSAPCentralServerInstancesOperations(
+      this._client,
+      subscriptionId,
+    );
+    this.sAPDatabaseInstances = getSAPDatabaseInstancesOperations(
+      this._client,
+      subscriptionId,
+    );
+    this.sAPApplicationServerInstances =
+      getSAPApplicationServerInstancesOperations(this._client, subscriptionId);
+    this.operations = getOperationsOperations(this._client);
+  }
+
+  /** The operation groups for SAPVirtualInstances */
+  public readonly sAPVirtualInstances: SAPVirtualInstancesOperations;
+  /** The operation groups for SAPCentralServerInstances */
+  public readonly sAPCentralServerInstances: SAPCentralServerInstancesOperations;
+  /** The operation groups for SAPDatabaseInstances */
+  public readonly sAPDatabaseInstances: SAPDatabaseInstancesOperations;
+  /** The operation groups for SAPApplicationServerInstances */
+  public readonly sAPApplicationServerInstances: SAPApplicationServerInstancesOperations;
+  /** The operation groups for Operations */
+  public readonly operations: OperationsOperations;
+}
