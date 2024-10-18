7a2983d1f77fda8faf154a6faa81838aa3096e6a
Jiao Di (MSFT) <80496810+v-jiaodi@users.noreply.github.com>
2024-10-18 11:33:42 +0800

    update test case

--- packages/typespec-ts/test/modularIntegration/routes.spec.ts
+++ packages/typespec-ts/test/modularIntegration/routes.spec.ts
@@ -1,18 +1,31 @@
 import { assert } from "chai";
 import { RoutesClient } from "./generated/routes/src/index.js";
 
-describe.only("Routes Client", () => {
+describe("Routes Client", () => {
   let client: RoutesClient;
 
   beforeEach(() => {
     client = new RoutesClient({
-      allowInsecureConnection: true
+      allowInsecureConnection: true,
+      endpoint: "http://localhost:3002"
     });
   });
 
-  it("should work with Routes_InInterface", async () => {
+  it("should work with Routes_PathParameters_ReservedExpansion_annotation", async () => {
     try {
-      const result = await client.inInterface.fixed();
+      const result =
+        await client.pathParameters.reservedExpansion.annotation(
+          "foo/bar%20baz"
+        );
+      assert.isUndefined(result);
+    } catch (err) {
+      assert.fail(err as string);
+    }
+  });
+  it("should work with Routes_PathParameters_ReservedExpansion_template", async () => {
+    try {
+      const result =
+        await client.pathParameters.reservedExpansion.template("foo/bar%20baz");
       assert.isUndefined(result);
     } catch (err) {
       assert.fail(err as string);
