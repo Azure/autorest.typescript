7a4e7e1f3d39a7ace661c01bb187e8247cff6c94
Jiao Di (MSFT) <80496810+v-jiaodi@users.noreply.github.com>
2024-10-22 14:48:59 +0800

    update

--- packages/typespec-ts/test/modularIntegration/generated/azure/resource-manager/models/resources/src/classic/index.ts
+++
@@ -1,6 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
-
-export { NestedProxyResourcesOperations } from "./nestedProxyResources/index.js";
-export { SingletonTrackedResourcesOperations } from "./singletonTrackedResources/index.js";
-export { TopLevelTrackedResourcesOperations } from "./topLevelTrackedResources/index.js";
