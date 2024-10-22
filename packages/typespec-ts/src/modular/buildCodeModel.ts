44dcd92c20dc3352584421666e11703e8a3f87ab
Mary Gao <yanmeigao1210@gmail.com>
2024-10-22 13:34:53 +0800

    Update the encode int as string for modular support

--- packages/typespec-ts/src/modular/buildCodeModel.ts
+++ packages/typespec-ts/src/modular/buildCodeModel.ts
@@ -5,6 +5,7 @@ import {
   Model,
   ModelProperty,
   Namespace,
+  NoTarget,
   Operation,
   Program,
   Scalar,
@@ -228,8 +229,8 @@ function handleDiscriminator(
       description:
         discriminatorValues.length > 0
           ? `the discriminator possible values: ${discriminatorValues.join(
-            ", "
-          )}`
+              ", "
+            )}`
           : "discriminator property",
       type: { type: "string", tcgcType: discriminatorTcgcType },
       restApiName: discriminator.propertyName,
@@ -324,7 +325,7 @@ export function getType(
   const enableCache = !isSimpleType(context.program, type);
   const effectiveModel =
     !options.disableEffectiveModel &&
-      (type.kind === "Model" || type.kind === "Union")
+    (type.kind === "Model" || type.kind === "Union")
       ? getEffectiveSchemaType(context.program, type)
       : type;
   if (enableCache) {
@@ -644,8 +645,8 @@ function emitResponse(
       type = isVoidType(innerResponse.body.type)
         ? undefined
         : getType(context, innerResponse.body.type, {
-          usage: UsageFlags.Output
-        });
+            usage: UsageFlags.Output
+          });
     }
   }
   const statusCodes: (number | "default")[] = [];
@@ -662,10 +663,10 @@ function emitResponse(
     type: type,
     isBinaryPayload: innerResponse.body?.type
       ? isBinaryPayload(
-        context,
-        innerResponse.body?.type,
-        innerResponse.body?.contentTypes![0] ?? "application/json"
-      )
+          context,
+          innerResponse.body?.type,
+          innerResponse.body?.contentTypes![0] ?? "application/json"
+        )
       : false
   };
 
@@ -812,7 +813,7 @@ function emitBasicOperation(
   const httpOperation = getHttpOperationWithCache(context, operation);
   const sourceOperation =
     operation.sourceOperation &&
-      !isTemplateDeclarationOrInstance(operation.sourceOperation)
+    !isTemplateDeclarationOrInstance(operation.sourceOperation)
       ? operation.sourceOperation
       : operation;
   const sourceOperationGroupName = getOperationGroupName(
@@ -897,7 +898,7 @@ function emitBasicOperation(
           (operation.parameters.properties.get(k) ===
             (originalBodyType as Model).properties.get(k) ||
             operation.parameters.properties.get(k) ===
-            (originalBodyType as Model).properties.get(k)?.sourceProperty)
+              (originalBodyType as Model).properties.get(k)?.sourceProperty)
       )
     ) {
       for (const param of bodyParameter.type.properties) {
@@ -1260,7 +1261,7 @@ function emitStdScalar(
         format: {
           propertyName: newScalar?.name ?? ""
         },
-        target: newScalar ?? scalar
+        target: NoTarget
       });
       return { type: "integer", format: newScalar.format };
     default:
@@ -1592,12 +1593,12 @@ function emitOperationGroups(
     const overrideName = getLibraryName(context, operationGroup.type);
     const name =
       context.rlcOptions?.hierarchyClient ||
-        context.rlcOptions?.enableOperationGroup
+      context.rlcOptions?.enableOperationGroup
         ? (overrideName ?? operationGroup.type.name)
         : "";
     const hierarchies =
       context.rlcOptions?.hierarchyClient ||
-        context.rlcOptions?.enableOperationGroup
+      context.rlcOptions?.enableOperationGroup
         ? operationGroup.groupPath.split(".")
         : [];
     if (hierarchies[0]?.endsWith("Client")) {
---
+++ packages/typespec-ts/test/modularUnit/scenarios/models/serialization/encodeIntAsString.md
@@ -0,0 +1,28 @@
+# Should generate serializer for encode int as string
+
+## TypeSpec
+
+This is tsp definition.
+
+```tsp
+model Foo {
+    @encode("string")
+    prop1: int32;
+}
+op post(@body body: Foo): void;
+```
+
+## Provide generated models and its serializer
+
+Generated Models.
+
+```ts models
+/** model interface Foo */
+export interface Foo {
+  prop1: string;
+}
+
+export function fooSerializer(item: Foo): any {
+  return { prop1: item["prop1"] };
+}
+```
