0ba2bf89a7c5cc70dd4c4e1b5a5781478ae3f7f7
Qiaoqiao Zhang <55688292+qiaozha@users.noreply.github.com>
2024-10-28 17:15:20 +0800

    Update packages/typespec-ts/src/framework/sample.ts

--- packages/typespec-ts/src/framework/sample.ts
+++ packages/typespec-ts/src/framework/sample.ts
@@ -67,7 +67,7 @@ sourceFile2.addStatements(`${functionReference}();`);
 sourceFile2.addStatements(`let obj: ${modelReference} = { id: 1 };`);
 
 // Apply imports to ensure correct references
-binder.resolveAllReferences("");
+binder.resolveAllReferences("/modularPackageFolder/src");
 
 // Output the generated files
 console.log("// test.ts");
