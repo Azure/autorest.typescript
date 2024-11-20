cfd75a0ed0bb180356574c859585a8c96d7da4da
Jiao Di (MSFT) <80496810+v-jiaodi@users.noreply.github.com>
2024-11-20 11:46:05 +0800

    update dpgName

--- packages/autorest.typescript/test/version-tolerance/generated/rlc-updated/src/index.ts
+++ packages/autorest.typescript/test/version-tolerance/generated/rlc-updated/src/index.ts
@@ -1,9 +1,9 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-import DPGClient from "./dpgClient2";
+import DPGClient from "./dpgClient";
 
-export * from "./dpgClient2";
+export * from "./dpgClient";
 export * from "./parameters";
 export * from "./responses";
 export * from "./clientDefinitions";
