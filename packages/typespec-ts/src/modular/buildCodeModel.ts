930f8c8a7b32e1ff725eaf3d33d9fad52eee0eed
Jiao Di (MSFT) <80496810+v-jiaodi@users.noreply.github.com>
2024-11-22 09:51:37 +0800

    update

--- packages/typespec-ts/src/modular/buildCodeModel.ts
+++ packages/typespec-ts/src/modular/buildCodeModel.ts
@@ -44,7 +44,6 @@ import {
   ModularCodeModel,
   OperationGroup,
   Parameter,
-  ParameterLocation,
   Property,
   Response
 } from "./modularCodeModel.js";
@@ -231,8 +230,8 @@ function handleDiscriminator(
       description:
         discriminatorValues.length > 0
           ? `the discriminator possible values: ${discriminatorValues.join(
-              ", "
-            )}`
+            ", "
+          )}`
           : "discriminator property",
       type: { type: "string", tcgcType: discriminatorTcgcType },
       restApiName: discriminator.propertyName,
@@ -327,7 +326,7 @@ export function getType(
   const enableCache = !isSimpleType(context.program, type);
   const effectiveModel =
     !options.disableEffectiveModel &&
-    (type.kind === "Model" || type.kind === "Union")
+      (type.kind === "Model" || type.kind === "Union")
       ? getEffectiveSchemaType(context.program, type)
       : type;
   if (enableCache) {
@@ -505,14 +504,7 @@ function emitBodyParameter(
   }
   return undefined;
 }
-function notSupportCookie(
-  parameter: HttpOperationParameter | HttpServerParameter
-): ParameterLocation {
-  if (parameter.type === "cookie") {
-    return "header";
-  }
-  return parameter.type;
-}
+
 function emitParameter(
   context: SdkContext,
   parameter: HttpOperationParameter | HttpServerParameter,
@@ -550,13 +542,9 @@ function emitParameter(
           : base.tcgcType
     };
   }
-
   const paramMap = {
     restApiName: parameter.name,
-    location:
-      parameter.type !== "cookie"
-        ? parameter.type
-        : notSupportCookie(parameter),
+    location: parameter.type,
     type: base.format ? { ...type, format: base.format } : type,
     implementation: implementation,
     skipUrlEncoding:
@@ -679,8 +667,8 @@ function emitResponse(
       type = isVoidType(innerResponse.body.type)
         ? undefined
         : getType(context, innerResponse.body.type, {
-            usage: UsageFlags.Output
-          });
+          usage: UsageFlags.Output
+        });
     }
   }
   const statusCodes: (number | "default")[] = [];
@@ -697,10 +685,10 @@ function emitResponse(
     type: type,
     isBinaryPayload: innerResponse.body?.type
       ? isBinaryPayload(
-          context,
-          innerResponse.body?.type,
-          innerResponse.body?.contentTypes![0] ?? "application/json"
-        )
+        context,
+        innerResponse.body?.type,
+        innerResponse.body?.contentTypes![0] ?? "application/json"
+      )
       : false
   };
 
@@ -847,7 +835,7 @@ function emitBasicOperation(
   const httpOperation = getHttpOperationWithCache(context, operation);
   const sourceOperation =
     operation.sourceOperation &&
-    !isTemplateDeclarationOrInstance(operation.sourceOperation)
+      !isTemplateDeclarationOrInstance(operation.sourceOperation)
       ? operation.sourceOperation
       : operation;
   const sourceOperationGroupName = getOperationGroupName(
@@ -935,7 +923,7 @@ function emitBasicOperation(
           (operation.parameters.properties.get(k) ===
             (originalBodyType as Model).properties.get(k) ||
             operation.parameters.properties.get(k) ===
-              (originalBodyType as Model).properties.get(k)?.sourceProperty)
+            (originalBodyType as Model).properties.get(k)?.sourceProperty)
       )
     ) {
       for (const param of bodyParameter.type.properties) {
@@ -1630,12 +1618,12 @@ function emitOperationGroups(
     const overrideName = getLibraryName(context, operationGroup.type);
     const name =
       context.rlcOptions?.hierarchyClient ||
-      context.rlcOptions?.enableOperationGroup
+        context.rlcOptions?.enableOperationGroup
         ? (overrideName ?? operationGroup.type.name)
         : "";
     const hierarchies =
       context.rlcOptions?.hierarchyClient ||
-      context.rlcOptions?.enableOperationGroup
+        context.rlcOptions?.enableOperationGroup
         ? operationGroup.groupPath.split(".")
         : [];
     if (hierarchies[0]?.endsWith("Client")) {
