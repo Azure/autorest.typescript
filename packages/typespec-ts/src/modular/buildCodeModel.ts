b9448066d85c3012a8a0cb4227440a7cc2d4e9bf
Mary Gao <yanmeigao1210@gmail.com>
2024-12-20 11:21:40 +0800

    Skip the multipart parameter generation

--- packages/typespec-ts/src/modular/buildCodeModel.ts
+++ packages/typespec-ts/src/modular/buildCodeModel.ts
@@ -230,8 +230,8 @@ function handleDiscriminator(
       description:
         discriminatorValues.length > 0
           ? `the discriminator possible values: ${discriminatorValues.join(
-              ", "
-            )}`
+            ", "
+          )}`
           : "discriminator property",
       type: { type: "string", tcgcType: discriminatorTcgcType },
       restApiName: discriminator.propertyName,
@@ -326,7 +326,7 @@ export function getType(
   const enableCache = !isSimpleType(context.program, type);
   const effectiveModel =
     !options.disableEffectiveModel &&
-    (type.kind === "Model" || type.kind === "Union")
+      (type.kind === "Model" || type.kind === "Union")
       ? getEffectiveSchemaType(context.program, type)
       : type;
   if (enableCache) {
@@ -501,6 +501,10 @@ function emitBodyParameter(
       isBinaryPayload: isBinaryPayload(context, body.type, contentTypes),
       optional: !isBodyRequired(httpOperation.parameters)
     };
+  } else if (body.bodyKind === "multipart") {
+    // TODO: support multipart body parameters
+    // https://github.com/Azure/autorest.typescript/issues/2422
+    return undefined;
   }
   return undefined;
 }
@@ -667,8 +671,8 @@ function emitResponse(
       type = isVoidType(innerResponse.body.type)
         ? undefined
         : getType(context, innerResponse.body.type, {
-            usage: UsageFlags.Output
-          });
+          usage: UsageFlags.Output
+        });
     }
   }
   const statusCodes: (number | "default")[] = [];
@@ -685,10 +689,10 @@ function emitResponse(
     type: type,
     isBinaryPayload: innerResponse.body?.type
       ? isBinaryPayload(
-          context,
-          innerResponse.body?.type,
-          innerResponse.body?.contentTypes![0] ?? "application/json"
-        )
+        context,
+        innerResponse.body?.type,
+        innerResponse.body?.contentTypes![0] ?? "application/json"
+      )
       : false
   };
 
@@ -835,7 +839,7 @@ function emitBasicOperation(
   const httpOperation = getHttpOperationWithCache(context, operation);
   const sourceOperation =
     operation.sourceOperation &&
-    !isTemplateDeclarationOrInstance(operation.sourceOperation)
+      !isTemplateDeclarationOrInstance(operation.sourceOperation)
       ? operation.sourceOperation
       : operation;
   const sourceOperationGroupName = getOperationGroupName(
@@ -902,7 +906,8 @@ function emitBasicOperation(
     }
   }
 
-  let bodyParameter: any | undefined;
+  // any would skip type checking and setting the type to `any | undefined` doesn't make sense
+  let bodyParameter: any;
   if (
     httpOperation.parameters.body === undefined ||
     isVoidType(httpOperation.parameters.body.type)
@@ -913,7 +918,10 @@ function emitBasicOperation(
     // Flatten the body parameter if it is an anonymous model
     const originalBodyType = httpOperation.parameters.body.type;
 
-    if (
+    // skip body parameter if it is not a body parameter
+    if (!bodyParameter) {
+      bodyParameter = undefined;
+    } else if (
       bodyParameter.type.type === "model" &&
       originalBodyType.kind === "Model" &&
       originalBodyType.name === "" &&
@@ -923,7 +931,7 @@ function emitBasicOperation(
           (operation.parameters.properties.get(k) ===
             (originalBodyType as Model).properties.get(k) ||
             operation.parameters.properties.get(k) ===
-              (originalBodyType as Model).properties.get(k)?.sourceProperty)
+            (originalBodyType as Model).properties.get(k)?.sourceProperty)
       )
     ) {
       for (const param of bodyParameter.type.properties) {
@@ -1620,12 +1628,12 @@ function emitOperationGroups(
     const overrideName = getLibraryName(context, operationGroup.type);
     const name =
       context.rlcOptions?.hierarchyClient ||
-      context.rlcOptions?.enableOperationGroup
+        context.rlcOptions?.enableOperationGroup
         ? (overrideName ?? operationGroup.type.name)
         : "";
     const hierarchies =
       context.rlcOptions?.hierarchyClient ||
-      context.rlcOptions?.enableOperationGroup
+        context.rlcOptions?.enableOperationGroup
         ? operationGroup.groupPath.split(".")
         : [];
     if (hierarchies[0]?.endsWith("Client")) {
