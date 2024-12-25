983395cfa5985082e9fd3f22ab0e7cd3f9840dc0
Jiao Di (MSFT) <80496810+v-jiaodi@users.noreply.github.com>
2024-12-25 15:19:44 +0800

    regen code

--- packages/typespec-ts/test/unit/uriTemplate.spec.ts
+++ packages/typespec-ts/test/unit/uriTemplate.spec.ts
@@ -35,9 +35,9 @@ describe("Client definition generation", () => {
     await assertEqualContent(
       clientDef!.content,
       `
-        import { TemplateParameters, TemplateParamPathParam } from "./parameters.js";
-        import { Template204Response } from "./responses.js";
-        import { Client, StreamableMethod } from "@azure-rest/core-client";
+        import type { TemplateParameters, TemplateParamPathParam } from "./parameters.js";
+        import type { Template204Response } from "./responses.js";
+        import type { Client, StreamableMethod } from "@azure-rest/core-client";
 
         export interface Template {
             get(options?: TemplateParameters): StreamableMethod<Template204Response>;
