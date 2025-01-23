81397bc720c32c984ddf8df210720af029cb971b
Jiao Di (MSFT) <80496810+v-jiaodi@users.noreply.github.com>
2025-01-23 12:28:19 +0800

    disable rlc payload pageable case

--- packages/typespec-ts/test/azureIntegration/payloadPageable.spec.ts
+++ packages/typespec-ts/test/azureIntegration/payloadPageable.spec.ts
@@ -1,36 +1,36 @@
-import { assert } from "chai";
-import PageableClientFactory, {
-    PageableClient,
-    paginate,
-    PetOutput
-} from "./generated/payload/pageable/src/index.js";
+// import { assert } from "chai";
+// import PageableClientFactory, {
+//     PageableClient,
+//     paginate,
+//     PetOutput
+// } from "./generated/payload/pageable/src/index.js";
 
-describe("Pageable Client", () => {
-    let client: PageableClient;
+// describe("Pageable Client", () => {
+//     let client: PageableClient;
 
-    beforeEach(() => {
-        client = PageableClientFactory({
-            allowInsecureConnection: true
-        });
-    });
-    const pets: PetOutput[] = [
-        { id: "1", name: "dog" },
-        { id: "2", name: "cat" },
-        { id: "3", name: "bird" },
-        { id: "4", name: "fish" }
-    ]
-    it("should get pagable Server Driven Pagination link", async () => {
-        const initialResponse = await client
-            .path("/payload/pageable/server-driven-pagination/link")
-            .get({ queryParameters: { maxpagesize: 3 } });
+//     beforeEach(() => {
+//         client = PageableClientFactory({
+//             allowInsecureConnection: true
+//         });
+//     });
+//     const pets: PetOutput[] = [
+//         { id: "1", name: "dog" },
+//         { id: "2", name: "cat" },
+//         { id: "3", name: "bird" },
+//         { id: "4", name: "fish" }
+//     ]
+//     it("should get pagable Server Driven Pagination link", async () => {
+//         const initialResponse = await client
+//             .path("/payload/pageable/server-driven-pagination/link")
+//             .get({ queryParameters: { maxpagesize: 3 } });
 
-        const iter = paginate(client, initialResponse);
+//         const iter = paginate(client, initialResponse);
 
-        let result: PetOutput[] = [];
-        for await (const item of iter) {
-            result.push(item);
-        }
-        assert.equal(result.length, 4)
-        assert.deepEqual(result, pets);
-    });
-});
+//         let result: PetOutput[] = [];
+//         for await (const item of iter) {
+//             result.push(item);
+//         }
+//         assert.equal(result.length, 4)
+//         assert.deepEqual(result, pets);
+//     });
+// });
