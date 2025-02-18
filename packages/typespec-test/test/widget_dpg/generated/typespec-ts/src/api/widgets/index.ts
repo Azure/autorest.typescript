ca64801c82157a9eb3765dfad755480791aefa35
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2025-02-18 17:42:26 +0800

    fix ci

--- packages/typespec-test/test/widget_dpg/generated/typespec-ts/src/api/widgets/index.ts
+++ packages/typespec-test/test/widget_dpg/generated/typespec-ts/src/api/widgets/index.ts
@@ -26,11 +26,11 @@ import {
   AnalyzeResult,
   analyzeResultDeserializer,
 } from "../../models/models.js";
+import { getLongRunningPoller } from "../../static-helpers/pollingHelpers.js";
 import {
   PagedAsyncIterableIterator,
   buildPagedAsyncIterator,
 } from "../../static-helpers/pagingHelpers.js";
-import { getLongRunningPoller } from "../../static-helpers/pollingHelpers.js";
 import { buildCsvCollection } from "../../static-helpers/serialization/build-csv-collection.js";
 import {
   StreamableMethod,
