24980251ec8c4dde94bef243e130245ae890e47c
Mary Gao <yanmeigao1210@gmail.com>
2025-03-03 18:06:12 +0800

    Remove wrongly added files

--- packages/typespec-ts/test/modularIntegration/generated/payload/multipart/src/classic/index.ts
+++
@@ -1,6 +0,0 @@
-// Licensed under the MIT License.
-
-export { FormDataOperations } from "./formData/index.js";
-export { FormDataHttpPartsOperations } from "./formData/httpParts/index.js";
-export { FormDataHttpPartsContentTypeOperations } from "./formData/httpParts/contentType/index.js";
-export { FormDataHttpPartsNonStringOperations } from "./formData/httpParts/nonString/index.js";
