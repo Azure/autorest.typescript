44356945b1465497bc92f683db9fc78db44932ff
Jiao Di (MSFT) <80496810+v-jiaodi@users.noreply.github.com>
2025-03-05 15:10:34 +0800

    update config options

--- packages/typespec-ts/test/azureModularIntegration/generated/client/enableModeNamespace/src/index.d.ts
+++ packages/typespec-ts/test/azureModularIntegration/generated/client/enableModeNamespace/src/index.d.ts
@@ -2,34 +2,34 @@ import { ClientOptions } from '@azure-rest/core-client';
 import { OperationOptions } from '@azure-rest/core-client';
 import { Pipeline } from '@azure/core-rest-pipeline';
 
+export declare interface ClientclientnamespacefirstFirstClientResult {
+    name: string;
+}
+
+export declare interface ClientclientnamespacesecondSecondClientResult {
+    type: ClientclientnamespacesecondsubSecondClientEnumType;
+}
+
+export declare type ClientclientnamespacesecondsubSecondClientEnumType = "second";
+
 export declare class ClientNamespaceFirstClient {
     private _client;
     readonly pipeline: Pipeline;
     constructor(options?: ClientNamespaceFirstClientOptionalParams);
-    getFirst(options?: GetFirstOptionalParams): Promise<ClientNamespaceFirstModelFirstClientResult>;
+    getFirst(options?: GetFirstOptionalParams): Promise<ClientclientnamespacefirstFirstClientResult>;
 }
 
 export declare interface ClientNamespaceFirstClientOptionalParams extends ClientOptions {
-}
-
-export declare interface ClientNamespaceFirstModelFirstClientResult {
-    name: string;
 }
 
 export declare class ClientNamespaceSecondClient {
     private _client;
     readonly pipeline: Pipeline;
     constructor(options?: ClientNamespaceSecondClientOptionalParams);
-    getSecond(options?: GetSecondOptionalParams): Promise<ClientNamespaceSecondModelSecondClientResult>;
+    getSecond(options?: GetSecondOptionalParams): Promise<ClientclientnamespacesecondSecondClientResult>;
 }
 
 export declare interface ClientNamespaceSecondClientOptionalParams extends ClientOptions {
-}
-
-export declare type ClientNamespaceSecondModelSecondClientEnumType = "second";
-
-export declare interface ClientNamespaceSecondModelSecondClientResult {
-    type: ClientNamespaceSecondModelSecondClientEnumType;
 }
 
 export declare interface GetFirstOptionalParams extends OperationOptions {
