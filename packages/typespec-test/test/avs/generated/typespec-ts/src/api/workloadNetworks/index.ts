1e2bd436cd707ee7cb822b4044c53cbb18fba64b
Jiao Di (MSFT) <80496810+v-jiaodi@users.noreply.github.com>
2025-03-13 14:39:47 +0800

    remove avs case

--- packages/typespec-test/test/avs/generated/typespec-ts/src/api/workloadNetworks/index.ts
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
-
-export { workloadNetworksList, workloadNetworksGet } from "./operations.js";
-export {
-  WorkloadNetworksListOptionalParams,
-  WorkloadNetworksGetOptionalParams,
-} from "./options.js";
--- packages/typespec-test/test/avs/generated/typespec-ts/src/classic/index.ts
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
-
-export { ClustersOperations } from "./clusters/index.js";
-export { OperationsOperations } from "./operations/index.js";
-export { PrivateCloudsOperations } from "./privateClouds/index.js";
-export { ScriptExecutionsOperations } from "./scriptExecutions/index.js";
-export { WorkloadNetworkDnsZonesOperations } from "./workloadNetworkDnsZones/index.js";
-export { WorkloadNetworksOperations } from "./workloadNetworks/index.js";
