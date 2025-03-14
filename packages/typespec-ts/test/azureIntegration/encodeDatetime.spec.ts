f711cd60c3d7bb8b96a79760264f3ee5816403c8
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2025-03-14 11:12:36 +0800

    update

--- packages/typespec-ts/test/azureIntegration/encodeDatetime.spec.ts
+++ packages/typespec-ts/test/azureIntegration/encodeDatetime.spec.ts
@@ -205,7 +205,7 @@ describe("EncodeDatetimeClient Rest Client", () => {
       assert.strictEqual(result.headers.value, "Fri, 26 Aug 2022 14:38:00 GMT");
     });
 
-    //skip this case as there's issue in servier side
+    //TODO revert this skipped case after merging https://github.com/microsoft/typespec/pull/6423 
     it.skip(`should get unix-timestamp header`, async () => {
       const result = await client
         .path(`/encode/datetime/responseheader/unix-timestamp`)
