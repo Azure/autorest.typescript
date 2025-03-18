91072b6dded777a6a8144bacbaa203846ed5d081
Jiao Di (MSFT) <80496810+v-jiaodi@users.noreply.github.com>
2025-03-18 10:30:07 +0800

    remove test case

--- packages/typespec-test/test/workloads/generated/typespec-ts/src/api/operations/index.ts
+++
@@ -1,5 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
-
-export { operationsList } from "./operations.js";
-export { OperationsListOptionalParams } from "./options.js";
