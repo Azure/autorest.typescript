1391e77a7ebfa722080bf1cd68397b96e2c2fd44
Jiao Di (MSFT) <80496810+v-jiaodi@users.noreply.github.com>
2025-04-07 17:04:02 +0800

    add api-version config

---
+++ packages/typespec-ts/test/azureModularIntegration/versioningRemovedV1.spec.ts
@@ -0,0 +1,21 @@
+import { assert } from "chai";
+import { RemovedClient } from "./generated/versioning/removedV1/src/index.js";
+
+describe.only("VersioningRemoved Rest Client", () => {
+    let client1: RemovedClient;
+
+    beforeEach(() => {
+        client1 = new RemovedClient("http://localhost:3002", "v1", {
+            allowInsecureConnection: true
+        });
+    });
+
+    it("versioning removed test v1/v3", async () => {
+        const result = await client1.modelV3({
+            id: "123",
+            enumProp: "enumMemberV1"
+        });
+        assert.strictEqual(result.id, "123");
+        assert.strictEqual(result.enumProp, "enumMemberV1");
+    });
+});
