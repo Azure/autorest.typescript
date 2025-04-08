166badcd3624ce1f2b1255aeae2ed127b45b3b74
Jiao Di (MSFT) <80496810+v-jiaodi@users.noreply.github.com>
2025-04-08 15:09:50 +0800

    format

--- packages/typespec-ts/test/azureModularIntegration/versioningRemovedV1.spec.ts
+++ packages/typespec-ts/test/azureModularIntegration/versioningRemovedV1.spec.ts
@@ -2,20 +2,20 @@ import { assert } from "chai";
 import { RemovedClient } from "./generated/versioning/removed/v1/src/index.js";
 
 describe("VersioningRemoved Rest Client", () => {
-    let client1: RemovedClient;
+  let client1: RemovedClient;
 
-    beforeEach(() => {
-        client1 = new RemovedClient("http://localhost:3002", "v1", {
-            allowInsecureConnection: true
-        });
+  beforeEach(() => {
+    client1 = new RemovedClient("http://localhost:3002", "v1", {
+      allowInsecureConnection: true
     });
+  });
 
-    it("versioning removed test v1/v3", async () => {
-        const result = await client1.modelV3({
-            id: "123",
-            enumProp: "enumMemberV1"
-        });
-        assert.strictEqual(result.id, "123");
-        assert.strictEqual(result.enumProp, "enumMemberV1");
+  it("versioning removed test v1/v3", async () => {
+    const result = await client1.modelV3({
+      id: "123",
+      enumProp: "enumMemberV1"
     });
+    assert.strictEqual(result.id, "123");
+    assert.strictEqual(result.enumProp, "enumMemberV1");
+  });
 });
