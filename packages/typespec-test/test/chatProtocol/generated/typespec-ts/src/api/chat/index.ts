c6596d0339d795d1a9a1060428140f7bd8ca07f8
Jiao Di (MSFT) <80496810+v-jiaodi@users.noreply.github.com>
2025-04-09 11:36:32 +0800

    remove case

--- packages/typespec-test/test/chatProtocol/generated/typespec-ts/src/api/chat/index.ts
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
-
-export { create, createStreaming } from "./operations.js";
-export {
-  ChatCreateOptionalParams,
-  ChatCreateStreamingOptionalParams,
-} from "./options.js";
