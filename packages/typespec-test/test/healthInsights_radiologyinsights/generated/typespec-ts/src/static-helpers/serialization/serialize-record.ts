0ddabbe7fe5fd1976ea79a2f72772eb53a6288f5
Mary Gao <yanmeigao1210@gmail.com>
2025-04-10 13:10:33 +0800

    Refresh the smoke testing

--- packages/typespec-test/test/healthInsights_radiologyinsights/generated/typespec-ts/src/static-helpers/serialization/serialize-record.ts
+++ packages/typespec-test/test/healthInsights_radiologyinsights/generated/typespec-ts/src/static-helpers/serialization/serialize-record.ts
@@ -1,12 +1,22 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-export function serializeRecord(item: any, excludeProperties: string[] = []) {
+export function serializeRecord(
+  item: any,
+  excludes?: string[],
+  serializer?: (item: any) => any,
+) {
+  excludes = excludes ?? [];
   let res: any = {};
   for (let key of Object.keys(item)) {
-    if (!excludeProperties.includes(key)) {
+    if (excludes.includes(key) || item[key] === undefined) {
+      continue;
+    }
+    if (serializer) {
+      res[key] = serializer(item[key]);
+    } else {
       res[key] = item[key] as any;
     }
   }
-  return Object.keys(res).length === 0 ? undefined : res;
+  return res;
 }
