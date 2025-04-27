b2189b42f660af743b67a6e61685fe3b93fbeee4
Jiao Di (MSFT) <80496810+v-jiaodi@users.noreply.github.com>
2025-04-27 16:24:09 +0800

    add test case for path

---
+++ packages/typespec-ts/test/azureModularIntegration/pathParameters.spec.ts
@@ -0,0 +1,22 @@
+import { PathClient } from "./generated/parameters/path/src/index.js";
+import { assert } from "chai";
+describe("PathClient Client", () => {
+    let client: PathClient;
+
+    beforeEach(() => {
+        client = new PathClient({
+            allowInsecureConnection: true,
+            endpoint: "http://localhost:3002"
+        });
+    });
+
+    it("path parameters normal", async () => {
+        const result = await client.normal("foo");
+        assert.isUndefined(result);
+    });
+
+    it("path parameters optional", async () => {
+        const result = await client.optional();
+        assert.isUndefined(result);
+    });
+});
