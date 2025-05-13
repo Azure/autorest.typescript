330277955954774382b203df12d3c4da4db3bdd6
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2025-05-13 10:06:03 +0800

    fix missing thsy configs

--- packages/autorest.typescript/src/generators/static/packageFileGenerator.ts
+++ packages/autorest.typescript/src/generators/static/packageFileGenerator.ts
@@ -162,14 +162,8 @@ function regularAutorestPackage(
     },
     autoPublish: true,
     browser: "./dist/browser/index.js",
-    "react-native": "./dist/react-native/index.js"
-  };
-  if (azureOutputDirectory) {
-    packageInfo.homepage = `https://github.com/Azure/azure-sdk-for-js/tree/main/${azureOutputDirectory}`;
-  }
-  if (azureSdkForJs) {
-    packageInfo["tshy"] = {
-      project: "./tsconfig.src.json",
+    "react-native": "./dist/react-native/index.js",
+    tshy: {
       exports: {
         "./package.json": "./package.json",
         ".": "./src/index.ts",
@@ -177,7 +171,13 @@ function regularAutorestPackage(
       dialects: ["esm", "commonjs"],
       esmDialects: ["browser", "react-native"],
       selfLink: false,
-    };
+    }
+  };
+  if (azureOutputDirectory) {
+    packageInfo.homepage = `https://github.com/Azure/azure-sdk-for-js/tree/main/${azureOutputDirectory}`;
+  }
+  if (azureSdkForJs) {
+    packageInfo.tshy["project"] = "./tsconfig.src.json";
     packageInfo.devDependencies["@azure/dev-tool"] = "^1.0.0";
     delete packageInfo.devDependencies["@microsoft/api-extractor"];
     delete packageInfo.devDependencies["rimraf"];
