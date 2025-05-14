d91bbde63ce3b6f5c1f31b557c30f6b3dd797722
Jiao Di (MSFT) <80496810+v-jiaodi@users.noreply.github.com>
2025-05-14 10:24:12 +0800

    remove search smoke

--- packages/typespec-test/test/Azure.Search/generated/typespec-ts/src/searchIndex/api/index.ts
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
-
-export {
-  createSearchIndex,
-  SearchIndexContext,
-  SearchIndexClientOptionalParams,
-} from "./searchIndexContext.js";
--- packages/typespec-test/test/Azure.Search/generated/typespec-ts/src/searchService/classic/index.ts
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
-
-export { AliasesOperations } from "./aliases/index.js";
-export { DataSourcesOperations } from "./dataSources/index.js";
-export { IndexersOperations } from "./indexers/index.js";
-export { IndexesOperations } from "./indexes/index.js";
-export { SkillsetsOperations } from "./skillsets/index.js";
-export { SynonymMapsOperations } from "./synonymMaps/index.js";
