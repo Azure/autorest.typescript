cd7f62ad4e63e3c237de0da4d79380c7377b3a6f
Jiao Di (MSFT) <80496810+v-jiaodi@users.noreply.github.com>
2025-05-21 17:25:37 +0800

    Fix the reserved word issue in Modular for API layer (#3191)
    * add test
    
    * update
    
    * fix
    
    * Add the UTs for the reserved word
    
    * Format codes
    
    * Fix the issue in sample gen
    
    * Fix the UTs
    
    * remove help test
    
    * add test case for reserved word operation
    
    * fix ci
    
    * fix ci
    
    * update reserved words list
    
    ---------
    
    Co-authored-by: Mary Gao <yanmeigao1210@gmail.com>

--- packages/typespec-test/test/widget_dpg/generated/typespec-ts/src/classic/budgets/index.ts
+++ packages/typespec-test/test/widget_dpg/generated/typespec-ts/src/classic/budgets/index.ts
@@ -4,14 +4,25 @@
 import { SAPWidgetServiceContext } from "../../api/sapWidgetServiceContext.js";
 import { Widget, SAPUser } from "../../models/models.js";
 import {
+  BudgetsContinueOptionalParams,
   BudgetsGetBudgetsOptionalParams,
   BudgetsCreateOrReplaceOptionalParams,
 } from "../../api/budgets/options.js";
-import { getBudgets, createOrReplace } from "../../api/budgets/operations.js";
+import {
+  $continue,
+  getBudgets,
+  createOrReplace,
+} from "../../api/budgets/operations.js";
 import { PollerLike, OperationState } from "@azure/core-lro";
 
 /** Interface representing a Budgets operations. */
 export interface BudgetsOperations {
+  /**
+   *  @fixme continue is a reserved word that cannot be used as an operation name.
+   *         Please add @clientName("clientName") or @clientName("<JS-Specific-Name>", "javascript")
+   *         to the operation to override the generated name.
+   */
+  continue: (options?: BudgetsContinueOptionalParams) => Promise<void>;
   getBudgets: (
     name: string,
     options?: BudgetsGetBudgetsOptionalParams,
@@ -26,6 +37,8 @@ export interface BudgetsOperations {
 
 function _getBudgets(context: SAPWidgetServiceContext) {
   return {
+    continue: (options?: BudgetsContinueOptionalParams) =>
+      $continue(context, options),
     getBudgets: (name: string, options?: BudgetsGetBudgetsOptionalParams) =>
       getBudgets(context, name, options),
     createOrReplace: (
