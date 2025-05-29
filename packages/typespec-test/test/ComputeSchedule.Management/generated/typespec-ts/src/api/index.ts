3c1b59abd8d60d1f81217a54a5bcbf83811ddf6c
Jiao Di (MSFT) <80496810+v-jiaodi@users.noreply.github.com>
2025-05-29 15:36:55 +0800

    add ut for unknown type

--- packages/typespec-test/test/ComputeSchedule.Management/generated/typespec-ts/src/api/index.ts
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
-
-export {
-  createComputeSchedule,
-  ComputeScheduleContext,
-  ComputeScheduleClientOptionalParams,
-} from "./computeScheduleContext.js";
