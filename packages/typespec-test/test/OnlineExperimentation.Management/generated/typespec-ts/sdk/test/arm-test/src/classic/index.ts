0a903aca82f39bd3c3f4f391007e46331ebfe8cf
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2025-05-29 11:39:18 +0800

    fix snippets generation issue

---
+++ packages/typespec-test/test/OnlineExperimentation.Management/generated/typespec-ts/sdk/test/arm-test/src/classic/index.ts
@@ -0,0 +1,7 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+export { OnlineExperimentationWorkspacesOperations } from "./onlineExperimentationWorkspaces/index.js";
+export { OperationsOperations } from "./operations/index.js";
+export { PrivateEndpointConnectionsOperations } from "./privateEndpointConnections/index.js";
+export { PrivateLinkResourcesOperations } from "./privateLinkResources/index.js";
