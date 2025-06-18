75f2f33773072f892ae2b2233555927696fd3604
Jiao Di (MSFT) <80496810+v-jiaodi@users.noreply.github.com>
2025-06-18 17:54:10 +0800

    add case for apiVersion header/path/query

--- packages/typespec-ts/test/commands/cadl-ranch-list.js
+++ packages/typespec-ts/test/commands/cadl-ranch-list.js
@@ -311,6 +311,18 @@ export const azureRlcTsps = [
   {
     outputPath: "azure/resource-manager/non-resource",
     inputPath: "azure/resource-manager/non-resource"
+  },
+  {
+    outputPath: "azure/client-generator-core/api-version/header",
+    inputPath: "azure/client-generator-core/api-version/header"
+  },
+  {
+    outputPath: "azure/client-generator-core/api-version/path",
+    inputPath: "azure/client-generator-core/api-version/path"
+  },
+  {
+    outputPath: "azure/client-generator-core/api-version/query",
+    inputPath: "azure/client-generator-core/api-version/query"
   }
 ];
 
@@ -815,6 +827,18 @@ export const azureModularTsps = [
   {
     outputPath: "azure/resource-manager/non-resource",
     inputPath: "azure/resource-manager/non-resource"
+  },
+  {
+    outputPath: "azure/client-generator-core/api-version/header",
+    inputPath: "azure/client-generator-core/api-version/header"
+  },
+  {
+    outputPath: "azure/client-generator-core/api-version/path",
+    inputPath: "azure/client-generator-core/api-version/path"
+  },
+  {
+    outputPath: "azure/client-generator-core/api-version/query",
+    inputPath: "azure/client-generator-core/api-version/query"
   }
 ];
 
