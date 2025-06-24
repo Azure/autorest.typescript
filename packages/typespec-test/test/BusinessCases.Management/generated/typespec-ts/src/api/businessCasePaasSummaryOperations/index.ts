8286bf8c9b80e29f0177ca62a1fc59a680590217
Jiao Di (MSFT) <80496810+v-jiaodi@users.noreply.github.com>
2025-06-24 16:19:43 +0800

    remove smoke case

--- packages/typespec-test/test/BusinessCases.Management/generated/typespec-ts/src/api/businessCasePaasSummaryOperations/index.ts
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
-
-export { listByParent, get } from "./operations.js";
-export {
-  BusinessCasePaasSummaryOperationsListByParentOptionalParams,
-  BusinessCasePaasSummaryOperationsGetOptionalParams,
-} from "./options.js";
--- packages/typespec-test/test/BusinessCases.Management/generated/typespec-ts/src/api/evaluatedSqlEntitiesOperations/index.ts
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
-
-export { listByParent, get } from "./operations.js";
-export {
-  EvaluatedSqlEntitiesOperationsListByParentOptionalParams,
-  EvaluatedSqlEntitiesOperationsGetOptionalParams,
-} from "./options.js";
