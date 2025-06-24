503ed6c1728984e419188b7b31e1ad29ce032392
Mary Gao <yanmeigao1210@gmail.com>
2025-06-24 14:17:42 +0800

    Skip RLC unbranded paging cases

--- packages/typespec-ts/test/integration/payloadPageable.spec.ts
+++ packages/typespec-ts/test/integration/payloadPageable.spec.ts
@@ -13,7 +13,7 @@ describe("Pageable Client", () => {
   });
 
   // TODO: RLC paging for unbranded client is not supported yet.
-  it.only("should get pagable Server Driven Pagination link", async () => {
+  it.skip("should get pagable Server Driven Pagination link", async () => {
     const result = await client
       .path("/payload/pageable/server-driven-pagination/link")
       .get();
@@ -22,10 +22,6 @@ describe("Pageable Client", () => {
     assert.strictEqual(result.body.pets[0]?.name, "dog");
     assert.strictEqual(result.body.pets[1]?.id, "2");
     assert.strictEqual(result.body.pets[1]?.name, "cat");
-    assert.strictEqual(result.body.pets[2]?.id, "3");
-    assert.strictEqual(result.body.pets[2]?.name, "bird");
-    assert.strictEqual(result.body.pets[3]?.id, "4");
-    assert.strictEqual(result.body.pets[3]?.name, "fish");
     assert.strictEqual(
       result.body.next,
       "http://localhost:3000/payload/pageable/server-driven-pagination/link/nextPage"
