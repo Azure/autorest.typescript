9365c96b717af5355432d7b8fb3f603a897d21b2
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2025-07-04 13:42:32 +0800

    update

--- packages/typespec-test/test/NetworkAnalytics.Management/generated/typespec-ts/sdk/test/arm-test/src/api/dataTypes/operations.ts
+++ packages/typespec-test/test/NetworkAnalytics.Management/generated/typespec-ts/sdk/test/arm-test/src/api/dataTypes/operations.ts
@@ -26,11 +26,11 @@ import {
   DataTypesGetOptionalParams,
   DataTypesCreateOptionalParams,
 } from "./options.js";
+import { getLongRunningPoller } from "../../static-helpers/pollingHelpers.js";
 import {
   PagedAsyncIterableIterator,
   buildPagedAsyncIterator,
 } from "../../static-helpers/pagingHelpers.js";
-import { getLongRunningPoller } from "../../static-helpers/pollingHelpers.js";
 import { expandUrlTemplate } from "../../static-helpers/urlTemplate.js";
 import {
   StreamableMethod,
