c81d60a46b04e564b7ea4517e0963aeee2537f1a
Jiao Di (MSFT) <80496810+v-jiaodi@users.noreply.github.com>
2025-07-07 15:53:56 +0800

    add ut

--- packages/typespec-test/test/EdgeOrder.Management/generated/typespec-ts/src/api/orders/index.ts
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
-
-export { listByResourceGroup, listBySubscription, get } from "./operations.js";
-export {
-  OrdersListByResourceGroupOptionalParams,
-  OrdersListBySubscriptionOptionalParams,
-  OrdersGetOptionalParams,
-} from "./options.js";
--- packages/typespec-test/test/EdgeOrder.Management/generated/typespec-ts/src/classic/index.ts
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
-
-export { AddressesOperations } from "./addresses/index.js";
-export { OperationsOperations } from "./operations/index.js";
-export { OrderItemsOperations } from "./orderItems/index.js";
-export { OrdersOperations } from "./orders/index.js";
-export { ProductsAndConfigurationsOperations } from "./productsAndConfigurations/index.js";
