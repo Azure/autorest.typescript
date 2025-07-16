8be85f0cd0e1311a429e11f9d8bc4e3855793c16
ZiWei Chen <98569699+kazrael2119@users.noreply.github.com>
2025-07-16 17:43:18 +0800

    Update packages/typespec-ts/src/lib.ts
    Co-authored-by: Renhe Li <renhe.li@hotmail.com>

--- packages/typespec-ts/src/lib.ts
+++ packages/typespec-ts/src/lib.ts
@@ -249,7 +249,7 @@ export const RLCOptionsSchema: JSONSchemaType<EmitterOptions> = {
       type: "boolean",
       nullable: true,
       description:
-        "An option to add model namespace as prefix if there's any model name conflicts within different namespaces. This is not recommended unless specifically told so"
+        "Provides an option to add the model namespace to model names in case of conflicts across different namespaces. This approach is generally discouraged unless explicitly required."
     },
     "hierarchy-client": {
       type: "boolean",
