dcd69dd8ddc040e906a6cc7f8a7c944c1d0d3e5d
Jiao Di (MSFT) <80496810+v-jiaodi@users.noreply.github.com>
2025-08-01 14:49:41 +0800

    fix modular ut and azure modular integration test

--- packages/typespec-ts/test/azureIntegration/generated/azure/payload/pageable/src/index.d.ts
+++ packages/typespec-ts/test/azureIntegration/generated/azure/payload/pageable/src/index.d.ts
@@ -1,19 +1,11 @@
 import type { Client } from '@azure-rest/core-client';
 import type { ClientOptions } from '@azure-rest/core-client';
 import type { HttpResponse } from '@azure-rest/core-client';
-import type { PathUncheckedResponse } from '@azure-rest/core-client';
 import type { RequestParameters } from '@azure-rest/core-client';
 import type { StreamableMethod } from '@azure-rest/core-client';
 
 declare function createClient(options?: PageableClientOptions): PageableClient;
 export default createClient;
-
-export declare type GetArrayType<T> = T extends Array<infer TData> ? TData : never;
-
-export declare type GetPage<TPage> = (pageLink: string) => Promise<{
-    page: TPage;
-    nextPageLink?: string;
-}>;
 
 export declare interface List {
     get(options?: ListParameters): StreamableMethod<List200Response>;
@@ -41,31 +33,9 @@ export declare type PageableClient = Client & {
 export declare interface PageableClientOptions extends ClientOptions {
 }
 
-export declare interface PagedAsyncIterableIterator<TElement, TPage = TElement[], TPageSettings = PageSettings> {
-    next(): Promise<IteratorResult<TElement>>;
-    [Symbol.asyncIterator](): PagedAsyncIterableIterator<TElement, TPage, TPageSettings>;
-    byPage: (settings?: TPageSettings) => AsyncIterableIterator<TPage>;
-}
-
 export declare interface PagedUserOutput {
     value: Array<UserOutput>;
     nextLink?: string;
-}
-
-export declare interface PageSettings {
-    continuationToken?: string;
-}
-
-export declare function paginate<TResponse extends PathUncheckedResponse>(client: Client, initialResponse: TResponse, options?: PagingOptions<TResponse>): PagedAsyncIterableIterator<PaginateReturn<TResponse>>;
-
-export declare type PaginateReturn<TResult> = TResult extends {
-    body: {
-        value?: infer TPage;
-    };
-} ? GetArrayType<TPage> : Array<unknown>;
-
-export declare interface PagingOptions<TResponse> {
-    customGetPage?: GetPage<PaginateReturn<TResponse>[]>;
 }
 
 export declare interface Routes {
