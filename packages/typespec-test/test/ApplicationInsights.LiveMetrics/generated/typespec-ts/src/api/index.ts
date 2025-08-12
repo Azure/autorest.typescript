8d7f2c634c3c23dc6d1062945bbf80769864d8cc
Jiao Di (MSFT) <80496810+v-jiaodi@users.noreply.github.com>
2025-08-12 16:02:53 +0800

    add smoke case for @azure-rest/livemetrics

---
+++ packages/typespec-test/test/ApplicationInsights.LiveMetrics/generated/typespec-ts/src/api/index.ts
@@ -0,0 +1,13 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+export {
+  createLiveMetrics,
+  LiveMetricsContext,
+  LiveMetricsClientOptionalParams,
+} from "./liveMetricsContext.js";
+export { publish, isSubscribed } from "./operations.js";
+export {
+  PublishOptionalParams,
+  IsSubscribedOptionalParams,
+} from "./options.js";
---
+++ packages/typespec-test/test/ApplicationInsights.LiveMetrics/generated/typespec-ts/src/models/index.ts
@@ -0,0 +1,31 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+export {
+  MonitoringDataPoint,
+  MetricPoint,
+  DocumentIngress,
+  DocumentIngressUnion,
+  DocumentType,
+  KeyValuePairStringString,
+  Request,
+  RemoteDependency,
+  Exception,
+  Event,
+  Trace,
+  ProcessCpuData,
+  CollectionConfigurationError,
+  CollectionConfigurationErrorType,
+  CollectionConfigurationInfo,
+  DerivedMetricInfo,
+  FilterConjunctionGroupInfo,
+  FilterInfo,
+  PredicateType,
+  AggregationType,
+  DocumentStreamInfo,
+  DocumentFilterConjunctionGroupInfo,
+  TelemetryType,
+  QuotaConfigurationInfo,
+  ServiceError,
+  KnownVersions,
+} from "./models.js";
