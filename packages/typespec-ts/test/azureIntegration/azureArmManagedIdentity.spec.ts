09f6add9580629f9e9424847dcff9c1cc5063d64
Jiao Di (MSFT) <80496810+v-jiaodi@users.noreply.github.com>
2025-09-12 12:22:58 +0800

    update case

--- packages/typespec-ts/test/azureIntegration/azureArmManagedIdentity.spec.ts
+++ packages/typespec-ts/test/azureIntegration/azureArmManagedIdentity.spec.ts
@@ -1,186 +1,186 @@
-// import { assert } from "chai";
-// import AzureArmModelsCommonTypesManagedIdentityClientFactory, {
-//   AzureArmModelsCommonTypesManagedIdentityClient
-// } from "./generated/azure/resource-manager/common-properties/src/index.js";
-// describe("Azure Arm Resources Rest Client", () => {
-//   let client: AzureArmModelsCommonTypesManagedIdentityClient;
+import { assert } from "chai";
+import AzureArmModelsCommonTypesManagedIdentityClientFactory, {
+    AzureArmModelsCommonTypesManagedIdentityClient
+} from "./generated/azure/resource-manager/common-properties/src/index.js";
+describe("Azure Arm Resources Rest Client", () => {
+    let client: AzureArmModelsCommonTypesManagedIdentityClient;
 
-//   beforeEach(() => {
-//     client = AzureArmModelsCommonTypesManagedIdentityClientFactory({
-//       endpoint: "http://localhost:3000",
-//       allowInsecureConnection: true
-//     });
-//   });
-//   const SUBSCRIPTION_ID_EXPECTED = "00000000-0000-0000-0000-000000000000";
-//   const PRINCIPAL_ID_EXPECTED = "00000000-0000-0000-0000-000000000000";
-//   const TENANT_ID_EXPECTED = "00000000-0000-0000-0000-000000000000";
-//   const CLIENT_ID_EXPECTED = "00000000-0000-0000-0000-000000000000";
-//   const LOCATION_REGION_EXPECTED = "eastus";
-//   const RESOURCE_GROUP_EXPECTED = "test-rg";
-//   const IDENTITY_TYPE_SYSTEM_ASSIGNED_EXPECTED = "SystemAssigned";
-//   const IDENTITY_TYPE_SYSTEM_USER_ASSIGNED_EXPECTED =
-//     "SystemAssigned,UserAssigned";
-//   const validSystemAssignedManagedIdentityResource = {
-//     id: `/subscriptions/${SUBSCRIPTION_ID_EXPECTED}/resourceGroups/${RESOURCE_GROUP_EXPECTED}/providers/Azure.ResourceManager.CommonProperties/managedIdentityTrackedResources/identity`,
-//     location: `${LOCATION_REGION_EXPECTED}`,
-//     tags: {
-//       tagKey1: "tagValue1"
-//     },
-//     identity: {
-//       type: `${IDENTITY_TYPE_SYSTEM_ASSIGNED_EXPECTED}`,
-//       principalId: `${PRINCIPAL_ID_EXPECTED}`,
-//       tenantId: `${TENANT_ID_EXPECTED}`
-//     },
-//     properties: {
-//       provisioningState: "Succeeded"
-//     }
-//   };
+    beforeEach(() => {
+        client = AzureArmModelsCommonTypesManagedIdentityClientFactory({
+            endpoint: "http://localhost:3000",
+            allowInsecureConnection: true
+        });
+    });
+    const SUBSCRIPTION_ID_EXPECTED = "00000000-0000-0000-0000-000000000000";
+    const PRINCIPAL_ID_EXPECTED = "00000000-0000-0000-0000-000000000000";
+    const TENANT_ID_EXPECTED = "00000000-0000-0000-0000-000000000000";
+    const CLIENT_ID_EXPECTED = "00000000-0000-0000-0000-000000000000";
+    const LOCATION_REGION_EXPECTED = "eastus";
+    const RESOURCE_GROUP_EXPECTED = "test-rg";
+    const IDENTITY_TYPE_SYSTEM_ASSIGNED_EXPECTED = "SystemAssigned";
+    const IDENTITY_TYPE_SYSTEM_USER_ASSIGNED_EXPECTED =
+        "SystemAssigned,UserAssigned";
+    const validSystemAssignedManagedIdentityResource = {
+        id: `/subscriptions/${SUBSCRIPTION_ID_EXPECTED}/resourceGroups/${RESOURCE_GROUP_EXPECTED}/providers/Azure.ResourceManager.CommonProperties/managedIdentityTrackedResources/identity`,
+        location: `${LOCATION_REGION_EXPECTED}`,
+        tags: {
+            tagKey1: "tagValue1"
+        },
+        identity: {
+            type: `${IDENTITY_TYPE_SYSTEM_ASSIGNED_EXPECTED}`,
+            principalId: `${PRINCIPAL_ID_EXPECTED}`,
+            tenantId: `${TENANT_ID_EXPECTED}`
+        },
+        properties: {
+            provisioningState: "Succeeded"
+        }
+    };
 
-//   const validUserAssignedAndSystemAssignedManagedIdentityResource = {
-//     id: `/subscriptions/${SUBSCRIPTION_ID_EXPECTED}/resourceGroups/${RESOURCE_GROUP_EXPECTED}/providers/Azure.ResourceManager.CommonProperties/managedIdentityTrackedResources/identity`,
-//     location: `${LOCATION_REGION_EXPECTED}`,
-//     tags: {
-//       tagKey1: "tagValue1"
-//     },
-//     identity: {
-//       type: `${IDENTITY_TYPE_SYSTEM_USER_ASSIGNED_EXPECTED}`,
-//       userAssignedIdentities: {
-//         "/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/test-rg/providers/Microsoft.ManagedIdentity/userAssignedIdentities/id1":
-//           {
-//             principalId: `${PRINCIPAL_ID_EXPECTED}`,
-//             clientId: `${CLIENT_ID_EXPECTED}`
-//           }
-//       },
-//       principalId: `${PRINCIPAL_ID_EXPECTED}`,
-//       tenantId: `${TENANT_ID_EXPECTED}`
-//     },
-//     properties: {
-//       provisioningState: "Succeeded"
-//     }
-//   };
+    const validUserAssignedAndSystemAssignedManagedIdentityResource = {
+        id: `/subscriptions/${SUBSCRIPTION_ID_EXPECTED}/resourceGroups/${RESOURCE_GROUP_EXPECTED}/providers/Azure.ResourceManager.CommonProperties/managedIdentityTrackedResources/identity`,
+        location: `${LOCATION_REGION_EXPECTED}`,
+        tags: {
+            tagKey1: "tagValue1"
+        },
+        identity: {
+            type: `${IDENTITY_TYPE_SYSTEM_USER_ASSIGNED_EXPECTED}`,
+            userAssignedIdentities: {
+                "/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/test-rg/providers/Microsoft.ManagedIdentity/userAssignedIdentities/id1":
+                {
+                    principalId: `${PRINCIPAL_ID_EXPECTED}`,
+                    clientId: `${CLIENT_ID_EXPECTED}`
+                }
+            },
+            principalId: `${PRINCIPAL_ID_EXPECTED}`,
+            tenantId: `${TENANT_ID_EXPECTED}`
+        },
+        properties: {
+            provisioningState: "Succeeded"
+        }
+    };
 
-//   const createExpectedIdentity = {
-//     type: `${IDENTITY_TYPE_SYSTEM_ASSIGNED_EXPECTED}`
-//   };
+    const createExpectedIdentity = {
+        type: `${IDENTITY_TYPE_SYSTEM_ASSIGNED_EXPECTED}`
+    };
 
-//   const updateExpectedIdentity = {
-//     type: `${IDENTITY_TYPE_SYSTEM_USER_ASSIGNED_EXPECTED}`,
-//     userAssignedIdentities: {
-//       "/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/test-rg/providers/Microsoft.ManagedIdentity/userAssignedIdentities/id1":
-//         {}
-//     }
-//   };
+    const updateExpectedIdentity = {
+        type: `${IDENTITY_TYPE_SYSTEM_USER_ASSIGNED_EXPECTED}`,
+        userAssignedIdentities: {
+            "/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/test-rg/providers/Microsoft.ManagedIdentity/userAssignedIdentities/id1":
+                {}
+        }
+    };
 
-//   // managed identity tracked resource
-//   it("should get models commonTypes managedIdentityTrackedResources", async () => {
-//     const result = await client
-//       .path(
-//         "/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Azure.ResourceManager.CommonProperties/managedIdentityTrackedResources/{managedIdentityTrackedResourceName}",
-//         SUBSCRIPTION_ID_EXPECTED,
-//         RESOURCE_GROUP_EXPECTED,
-//         "identity"
-//       )
-//       .get();
-//     assert.strictEqual(result.status, "200");
-//     assert.deepEqual(result.body, validSystemAssignedManagedIdentityResource);
-//   });
+    // managed identity tracked resource
+    it("should get models commonTypes managedIdentityTrackedResources", async () => {
+        const result = await client
+            .path(
+                "/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Azure.ResourceManager.CommonProperties/managedIdentityTrackedResources/{managedIdentityTrackedResourceName}",
+                SUBSCRIPTION_ID_EXPECTED,
+                RESOURCE_GROUP_EXPECTED,
+                "identity"
+            )
+            .get();
+        assert.strictEqual(result.status, "200");
+        assert.deepEqual(result.body, validSystemAssignedManagedIdentityResource);
+    });
 
-//   it("should put models commonTypes managedIdentityTrackedResources", async () => {
-//     const result = await client
-//       .path(
-//         "/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Azure.ResourceManager.CommonProperties/managedIdentityTrackedResources/{managedIdentityTrackedResourceName}",
-//         SUBSCRIPTION_ID_EXPECTED,
-//         RESOURCE_GROUP_EXPECTED,
-//         "identity"
-//       )
-//       .put({
-//         body: {
-//           identity: createExpectedIdentity,
-//           location: LOCATION_REGION_EXPECTED,
-//           tags: { tagKey1: "tagValue1" }
-//         }
-//       });
-//     assert.strictEqual(result.status, "200");
-//     assert.deepEqual(result.body, validSystemAssignedManagedIdentityResource);
-//   });
+    it("should put models commonTypes managedIdentityTrackedResources", async () => {
+        const result = await client
+            .path(
+                "/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Azure.ResourceManager.CommonProperties/managedIdentityTrackedResources/{managedIdentityTrackedResourceName}",
+                SUBSCRIPTION_ID_EXPECTED,
+                RESOURCE_GROUP_EXPECTED,
+                "identity"
+            )
+            .put({
+                body: {
+                    identity: createExpectedIdentity,
+                    location: LOCATION_REGION_EXPECTED,
+                    tags: { tagKey1: "tagValue1" }
+                }
+            });
+        assert.strictEqual(result.status, "200");
+        assert.deepEqual(result.body, validSystemAssignedManagedIdentityResource);
+    });
 
-//   it("should patch models commonTypes managedIdentityTrackedResources", async () => {
-//     const result = await client
-//       .path(
-//         "/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Azure.ResourceManager.CommonProperties/managedIdentityTrackedResources/{managedIdentityTrackedResourceName}",
-//         SUBSCRIPTION_ID_EXPECTED,
-//         RESOURCE_GROUP_EXPECTED,
-//         "identity"
-//       )
-//       .patch({
-//         body: {
-//           identity: updateExpectedIdentity,
-//           location: LOCATION_REGION_EXPECTED,
-//           tags: { tagKey1: "tagValue1" }
-//         }
-//       });
-//     assert.strictEqual(result.status, "200");
-//     assert.deepEqual(
-//       result.body,
-//       validUserAssignedAndSystemAssignedManagedIdentityResource
-//     );
-//   });
+    it("should patch models commonTypes managedIdentityTrackedResources", async () => {
+        const result = await client
+            .path(
+                "/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Azure.ResourceManager.CommonProperties/managedIdentityTrackedResources/{managedIdentityTrackedResourceName}",
+                SUBSCRIPTION_ID_EXPECTED,
+                RESOURCE_GROUP_EXPECTED,
+                "identity"
+            )
+            .patch({
+                body: {
+                    identity: updateExpectedIdentity,
+                    location: LOCATION_REGION_EXPECTED,
+                    tags: { tagKey1: "tagValue1" }
+                }
+            });
+        assert.strictEqual(result.status, "200");
+        assert.deepEqual(
+            result.body,
+            validUserAssignedAndSystemAssignedManagedIdentityResource
+        );
+    });
 
-//   describe("Error Handling", () => {
-//     it("should handle predefined error for resource not found (404)", async () => {
-//       const result = await client
-//         .path(
-//           "/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Azure.ResourceManager.CommonProperties/confidentialResources/{confidentialResourceName}",
-//           SUBSCRIPTION_ID_EXPECTED,
-//           RESOURCE_GROUP_EXPECTED,
-//           "confidential"
-//         )
-//         .get();
+    describe("Error Handling", () => {
+        it("should handle predefined error for resource not found (404)", async () => {
+            const result = await client
+                .path(
+                    "/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Azure.ResourceManager.CommonProperties/confidentialResources/{confidentialResourceName}",
+                    SUBSCRIPTION_ID_EXPECTED,
+                    RESOURCE_GROUP_EXPECTED,
+                    "confidential"
+                )
+                .get();
 
-//       assert.strictEqual(result.status, "404");
-//       assert.isObject(result.body);
+            assert.strictEqual(result.status, "404");
+            assert.isObject(result.body);
 
-//       if (result.status === "404" && "error" in result.body) {
-//         const errorBody = result.body as any;
-//         assert.strictEqual(errorBody.error.code, "ResourceNotFound");
-//         assert.strictEqual(
-//           errorBody.error.message,
-//           "The Resource 'Azure.ResourceManager.CommonProperties/confidentialResources/confidential' under resource group 'test-rg' was not found."
-//         );
-//       }
-//     });
+            if (result.status === "404" && "error" in result.body) {
+                const errorBody = result.body as any;
+                assert.strictEqual(errorBody.error.code, "ResourceNotFound");
+                assert.strictEqual(
+                    errorBody.error.message,
+                    "The Resource 'Azure.ResourceManager.CommonProperties/confidentialResources/confidential' under resource group 'test-rg' was not found."
+                );
+            }
+        });
 
-//     it("should handle user-defined error for bad request (400)", async () => {
-//       const result = await client
-//         .path(
-//           "/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Azure.ResourceManager.CommonProperties/confidentialResources/{confidentialResourceName}",
-//           SUBSCRIPTION_ID_EXPECTED,
-//           RESOURCE_GROUP_EXPECTED,
-//           "confidential"
-//         )
-//         .put({
-//           body: {
-//             location: "eastus",
-//             properties: {
-//               username: "00"
-//             }
-//           }
-//         });
+        it("should handle user-defined error for bad request (400)", async () => {
+            const result = await client
+                .path(
+                    "/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Azure.ResourceManager.CommonProperties/confidentialResources/{confidentialResourceName}",
+                    SUBSCRIPTION_ID_EXPECTED,
+                    RESOURCE_GROUP_EXPECTED,
+                    "confidential"
+                )
+                .put({
+                    body: {
+                        location: "eastus",
+                        properties: {
+                            username: "00"
+                        }
+                    }
+                });
 
-//       assert.strictEqual(result.status, "400");
+            assert.strictEqual(result.status, "400");
 
-//       if (result.status === "400") {
-//         assert.strictEqual(result.body.error?.code, "BadRequest");
-//         assert.strictEqual(
-//           result.body.error?.message,
-//           "Username should not contain only numbers."
-//         );
-//         assert.isObject(result.body.error?.innererror);
-//         assert.strictEqual(
-//           result.body.error?.innererror?.exceptiontype,
-//           "general"
-//         );
-//       }
-//     });
-//   });
-// });
+            if (result.status === "400") {
+                assert.strictEqual(result.body.error?.code, "BadRequest");
+                assert.strictEqual(
+                    result.body.error?.message,
+                    "Username should not contain only numbers."
+                );
+                assert.isObject(result.body.error?.innererror);
+                assert.strictEqual(
+                    result.body.error?.innererror?.exceptiontype,
+                    "general"
+                );
+            }
+        });
+    });
+});
