e19afb19b28fd8d02bde780a65894aadd2bdc973
Jeremy Meng <jeremy.ymeng@gmail.com>
2025-09-12 14:06:05 -0700

    fix build and format

--- packages/typespec-ts/src/utils/crossLanguageDef.ts
+++ packages/typespec-ts/src/utils/crossLanguageDef.ts
@@ -2,38 +2,37 @@
 // Licensed under the MIT License.
 
 import { SdkContext } from "@azure-tools/typespec-client-generator-core";
-import { getFriendlyName } from "@typespec/compiler";
 import { writeFile } from "fs/promises";
 import { join as pathJoin } from "path";
 
 export async function generateCrossLanguageDefinitionFile(
-    outputDir: string,
-    dpgContext: SdkContext
+  outputDir: string,
+  dpgContext: SdkContext
 ) {
-    const CrossLanguageDefinitionId: Record<string, string> = {};
+  const CrossLanguageDefinitionId: Record<string, string> = {};
 
-    for (const model of dpgContext.sdkPackage.models) {
-        CrossLanguageDefinitionId[`${model.name}:interface`] =
-            model.crossLanguageDefinitionId;
-    }
-    for (const enm of dpgContext.sdkPackage.enums) {
-        CrossLanguageDefinitionId[`Known${enm.name}:enum`] =
-            enm.crossLanguageDefinitionId;
-    }
-    for (const union of dpgContext.sdkPackage.unions) {
-        CrossLanguageDefinitionId[`${union.name}:type`] =
-            union.crossLanguageDefinitionId;
-    }
+  for (const model of dpgContext.sdkPackage.models) {
+    CrossLanguageDefinitionId[`${model.name}:interface`] =
+      model.crossLanguageDefinitionId;
+  }
+  for (const enm of dpgContext.sdkPackage.enums) {
+    CrossLanguageDefinitionId[`Known${enm.name}:enum`] =
+      enm.crossLanguageDefinitionId;
+  }
+  for (const union of dpgContext.sdkPackage.unions) {
+    CrossLanguageDefinitionId[`${union.name}:type`] =
+      union.crossLanguageDefinitionId;
+  }
 
-    // TODO: operation groups and operations
+  // TODO: operation groups and operations
 
-    const json = {
-        CrossLanguagePackageId: dpgContext.sdkPackage.crossLanguagePackageId,
-        CrossLanguageDefinitionId
-    };
+  const json = {
+    CrossLanguagePackageId: dpgContext.sdkPackage.crossLanguagePackageId,
+    CrossLanguageDefinitionId
+  };
 
-    await writeFile(
-        pathJoin(outputDir, "apiview-properties.json"),
-        JSON.stringify(json)
-    );
+  await writeFile(
+    pathJoin(outputDir, "apiview-properties.json"),
+    JSON.stringify(json)
+  );
 }
