4d59c63af988b1c1cad344502c8d2ae409f52dd8
Jiao Di (MSFT) <80496810+v-jiaodi@users.noreply.github.com>
2025-10-29 13:51:14 +0800

    skip case

--- packages/typespec-ts/test/commands/cadl-ranch-list.js
+++ packages/typespec-ts/test/commands/cadl-ranch-list.js
@@ -884,10 +884,11 @@ export const azureModularTsps = [
     outputPath: "azure/versioning/previewVersion",
     inputPath: "azure/versioning/previewVersion"
   },
-  {
-    outputPath: "azure/resource-manager/method-subscription-id",
-    inputPath: "azure/resource-manager/method-subscription-id"
-  }
+  // skip due to issue https://github.com/Azure/autorest.typescript/issues/3468
+  // {
+  //   outputPath: "azure/resource-manager/method-subscription-id",
+  //   inputPath: "azure/resource-manager/method-subscription-id"
+  // }
   // skip due to issue https://github.com/Azure/autorest.typescript/issues/3494
   // {
   //   outputPath: "azure/client-generator-core/client-location",
