c7a4025ae2c5d3c51ef12cf26e2b4e4eda888026
Jiao Di (MSFT) <v-jiaodi@microsoft.com>
2025-11-05 17:17:27 +0800

    remove smoke case

--- packages/typespec-test/test/Azure.Analytics.Purview.DataMap/generated/typespec-ts/src/api/discovery/index.ts
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
-
-export { autoComplete, suggest, query } from "./operations.js";
-export {
-  DiscoveryAutoCompleteOptionalParams,
-  DiscoverySuggestOptionalParams,
-  DiscoveryQueryOptionalParams,
-} from "./options.js";
--- packages/typespec-test/test/Azure.Analytics.Purview.DataMap/generated/typespec-ts/src/api/lineage/index.ts
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
-
-export { getByUniqueAttribute, getNextPage, get } from "./operations.js";
-export {
-  LineageGetByUniqueAttributeOptionalParams,
-  LineageGetNextPageOptionalParams,
-  LineageGetOptionalParams,
-} from "./options.js";
