1d33402e6e2c20857c2056012767bd03002d0aa1
Jiao Di (MSFT) <v-jiaodi@microsoft.com>
2025-12-03 17:48:26 +0800

    remove smoke case

--- packages/typespec-test/test/Azure.Search/generated/typespec-ts/src/knowledgeBaseRetrieval/index.ts
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
-
-export { KnowledgeBaseRetrievalClient } from "./knowledgeBaseRetrievalClient.js";
-export {
-  KnowledgeBaseRetrievalContext,
-  KnowledgeBaseRetrievalClientOptionalParams,
-  RetrieveOptionalParams,
-} from "./api/index.js";
