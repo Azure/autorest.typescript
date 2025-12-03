34c748b0b7c57a0a0d31feaa72a13dc8e2a32fe9
Jiao Di (MSFT) <v-jiaodi@microsoft.com>
2025-12-03 17:44:38 +0800

    add smoke case

--- packages/typespec-test/test/hierarchy_generic/generated/typespec-ts/src/index.ts
+++ packages/typespec-test/test/hierarchy_generic/generated/typespec-ts/src/index.ts
@@ -4,16 +4,35 @@
 export { FooClient } from "./fooClient.js";
 export { A } from "./models/index.js";
 export { BA } from "./models/b/index.js";
+export {
+  YDataSourceProperties,
+  YDataSourcePropertiesUnion,
+  YConnectorType,
+} from "./models/y/index.js";
 export { BEA } from "./models/b/e/index.js";
+export {
+  YDataverseDataverseSourceConnectorProperties,
+  YDataverseDataverseEventPosition,
+} from "./models/y/dataverse/index.js";
+export {
+  YDataverseV2DataverseSourceConnectorV2Properties,
+  YDataverseV2DataverseEventPosition,
+} from "./models/y/dataverseV2/index.js";
 export { FooClientOptionalParams, Op1OptionalParams } from "./api/index.js";
 export { BOp1OptionalParams } from "./api/b/index.js";
 export { DOp1OptionalParams } from "./api/d/index.js";
 export { BCOp1OptionalParams } from "./api/b/c/index.js";
+export {
+  YTestOperationsTestDataverseV2OptionalParams,
+  YTestOperationsTestDataverseOptionalParams,
+} from "./api/y/testOperations/index.js";
 export { BECOp1OptionalParams } from "./api/b/e/c/index.js";
 export {
   BOperations,
   DOperations,
+  YOperations,
   BCOperations,
   BEOperations,
+  YTestOperationsOperations,
   BECOperations,
 } from "./classic/index.js";
--- packages/typespec-ts/src/utils/namespaceUtils.ts
+++ packages/typespec-ts/src/utils/namespaceUtils.ts
@@ -70,11 +70,13 @@ export function getOperationNamespaceInterfaceName(
 export function detectModelConflicts(dpgContext: SdkContext) {
   const allModels = getAllModels(dpgContext);
   const nameSet = new Set<string>();
+  const reported = new Set<string>();
+  let hasConflict = false;
   for (const model of allModels) {
     if (model.name === "") {
       continue;
     }
-    if (nameSet.has(model.name)) {
+    if (nameSet.has(model.name) && !reported.has(model.name)) {
       reportDiagnostic(dpgContext.program, {
         code: "detected-model-name-conflict",
         format: {
@@ -86,9 +88,10 @@ export function detectModelConflicts(dpgContext: SdkContext) {
         },
         target: NoTarget
       });
-      return true;
+      reported.add(model.name);
+      hasConflict = true;
     }
     nameSet.add(model.name);
   }
-  return false;
+  return hasConflict;
 }
