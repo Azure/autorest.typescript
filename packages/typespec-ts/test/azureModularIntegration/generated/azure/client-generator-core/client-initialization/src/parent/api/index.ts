37125f0be08c3abb520f7ae5459995c7feec946d
ZiWei Chen <v-ziweichen@microsoft.com>
2025-12-19 11:15:59 +0800

    update

--- packages/typespec-ts/test/azureModularIntegration/generated/azure/client-generator-core/client-initialization/src/parent/api/index.ts
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
-
-export {
-  createParent,
-  ParentContext,
-  ParentClientOptionalParams,
-} from "./parentContext.js";
--- packages/typespec-ts/test/azureModularIntegration/generated/azure/client-generator-core/client-initialization/src/parent/index.ts
+++
@@ -1,5 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
-
-export { ParentClient } from "./parentClient.js";
-export { ParentContext, ParentClientOptionalParams } from "./api/index.js";
