00ffc5711294278bde44c5e94149c7cdfbe1abcd
Jiao Di (MSFT) <v-jiaodi@microsoft.com>
2026-01-22 10:50:41 +0800

    remove smoke case

--- packages/typespec-test/test/Face/generated/typespec-ts/src/faceAdministration/classic/index.ts
+++
@@ -1,5 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
-
-export { LargeFaceListOperations } from "./largeFaceList/index.js";
-export { LargePersonGroupOperations } from "./largePersonGroup/index.js";
--- packages/typespec-test/test/Face/generated/typespec-ts/src/models/typeSpec/http/index.ts
+++
@@ -1,4 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
-
-export { HttpPart, HttpPart1, HttpPart2 } from "./models.js";
