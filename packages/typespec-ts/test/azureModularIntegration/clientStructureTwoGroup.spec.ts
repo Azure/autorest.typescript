6346d53fc20fd278ea4f67e1da8ee662e10d72c8
Jiao Di (MSFT) <v-jiaodi@microsoft.com>
2026-04-09 10:07:58 +0800

    Revert "revert skip case"
    This reverts commit ff0cb5981b306ea904737e7e661ae8d368ce89c3.

--- packages/typespec-ts/test/azureModularIntegration/clientStructureTwoGroup.spec.ts
+++ packages/typespec-ts/test/azureModularIntegration/clientStructureTwoGroup.spec.ts
@@ -1,45 +1,45 @@
 import { assert } from "vitest";
-import { TwoOperationGroupClient } from "./generated/client/structure/two-operation-group/src/index.js";
-describe("Client Structure Two-Operation-Group Rest Client", () => {
-  let client: TwoOperationGroupClient;
+// import { TwoOperationGroupClient } from "./generated/client/structure/two-operation-group/src/index.js";
+// // describe("Client Structure Two-Operation-Group Rest Client", () => {
+//   let client: TwoOperationGroupClient;
 
-  beforeEach(() => {
-    client = new TwoOperationGroupClient(
-      "http://localhost:3002",
-      "two-operation-group",
-      {
-        allowInsecureConnection: true
-      }
-    );
-  });
+//   beforeEach(() => {
+//     client = new TwoOperationGroupClient(
+//       "http://localhost:3002",
+//       "two-operation-group",
+//       {
+//         allowInsecureConnection: true
+//       }
+//     );
+//   });
 
-  it("should call operation one correctly", async () => {
-    const result = await client.one();
-    assert.strictEqual(result, undefined);
-  });
+//   it("should call operation one correctly", async () => {
+//     const result = await client.one();
+//     assert.strictEqual(result, undefined);
+//   });
 
-  it("should call operation two correctly", async () => {
-    const result = await client.two();
-    assert.strictEqual(result, undefined);
-  });
+//   it("should call operation two correctly", async () => {
+//     const result = await client.two();
+//     assert.strictEqual(result, undefined);
+//   });
 
-  it("should call operation three correctly", async () => {
-    const result = await client.three();
-    assert.strictEqual(result, undefined);
-  });
+//   it("should call operation three correctly", async () => {
+//     const result = await client.three();
+//     assert.strictEqual(result, undefined);
+//   });
 
-  it("should call operation four correctly", async () => {
-    const result = await client.four();
-    assert.strictEqual(result, undefined);
-  });
+//   it("should call operation four correctly", async () => {
+//     const result = await client.four();
+//     assert.strictEqual(result, undefined);
+//   });
 
-  it("should call operation five correctly", async () => {
-    const result = await client.five();
-    assert.strictEqual(result, undefined);
-  });
+//   it("should call operation five correctly", async () => {
+//     const result = await client.five();
+//     assert.strictEqual(result, undefined);
+//   });
 
-  it("should call operation six correctly", async () => {
-    const result = await client.six();
-    assert.strictEqual(result, undefined);
-  });
-});
+//   it("should call operation six correctly", async () => {
+//     const result = await client.six();
+//     assert.strictEqual(result, undefined);
+//   });
+// });
