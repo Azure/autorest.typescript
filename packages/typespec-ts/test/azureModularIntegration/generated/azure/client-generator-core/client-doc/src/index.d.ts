081eb612a8c373d6a320401100e808e38deecb69
Jiao Di (MSFT) <v-jiaodi@microsoft.com>
2026-05-12 10:04:10 +0800

    regen code

--- packages/typespec-ts/test/azureModularIntegration/generated/azure/client-generator-core/client-doc/src/index.d.ts
+++ packages/typespec-ts/test/azureModularIntegration/generated/azure/client-generator-core/client-doc/src/index.d.ts
@@ -1,6 +1,8 @@
 import { ClientOptions } from '@azure-rest/core-client';
+import { isRestError } from '@azure/core-rest-pipeline';
 import { OperationOptions } from '@azure-rest/core-client';
 import { Pipeline } from '@azure/core-rest-pipeline';
+import { RestError } from '@azure/core-rest-pipeline';
 
 export declare class ClientDocClient {
     private _client;
@@ -19,9 +21,13 @@ export declare interface DocumentationOperations {
     harvest: (body: Plant, options?: DocumentationHarvestOptionalParams) => Promise<Plant>;
 }
 
+export { isRestError }
+
 export declare interface Plant {
     name: string;
     species: string;
 }
+
+export { RestError }
 
 export { }
--- packages/typespec-ts/test/azureModularIntegration/generated/azure/client-generator-core/response-as-bool/src/index.d.ts
+++ packages/typespec-ts/test/azureModularIntegration/generated/azure/client-generator-core/response-as-bool/src/index.d.ts
@@ -1,6 +1,8 @@
 import { ClientOptions } from '@azure-rest/core-client';
+import { isRestError } from '@azure/core-rest-pipeline';
 import { OperationOptions } from '@azure-rest/core-client';
 import { Pipeline } from '@azure/core-rest-pipeline';
+import { RestError } from '@azure/core-rest-pipeline';
 
 export declare interface HeadAsBooleanExistsOptionalParams extends OperationOptions {
 }
@@ -21,6 +23,8 @@ export declare interface HeadAsBooleanOperations {
     exists: (options?: HeadAsBooleanExistsOptionalParams) => Promise<HeadAsBooleanExistsResponse>;
 }
 
+export { isRestError }
+
 export declare class ResponseAsBoolClient {
     private _client;
     readonly pipeline: Pipeline;
@@ -30,5 +34,7 @@ export declare class ResponseAsBoolClient {
 
 export declare interface ResponseAsBoolClientOptionalParams extends ClientOptions {
 }
+
+export { RestError }
 
 export { }
